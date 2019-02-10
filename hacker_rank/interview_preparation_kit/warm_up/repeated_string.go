bd58c10175a055dddda84dd2bd075a24d80e7e9a
zrma <bulbitain@gmail.com>
2019-02-10 14:57:22 +0900

    solve Repeated String
    https://www.hackerrank.com/challenges/repeated-string/problem

---
+++ hacker_rank/interview_preparation_kit/warm_up/repeated_string.go
@@ -0,0 +1,38 @@
+package warm_up
+
+import "strings"
+
+const findTarget = "a"
+
+func repeatedString(s string, n int64) int64 {
+	if !isValid(s, n) {
+		return 0
+	}
+
+	l := int64(len(s))
+	cnt := int64(strings.Count(s, findTarget))
+	repeatCnt := n / l
+	mod := n % l
+
+	return cnt*repeatCnt + partialCnt(s, mod)
+}
+
+func partialCnt(s string, n int64) int64 {
+	if !isValid(s, n) {
+		return 0
+	}
+
+	return int64(strings.Count(s[:n], findTarget))
+}
+
+func isValid(s string, n int64) bool {
+	if n == 0 {
+		return false
+	}
+
+	if s == "" {
+		return false
+	}
+
+	return true
+}
