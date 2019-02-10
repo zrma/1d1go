55846274ca740d8a7c542af054ca2b01ca0bc6f2
zrma <bulbitain@gmail.com>
2019-02-10 15:29:44 +0900

    solve 2D Array - DS
    https://www.hackerrank.com/challenges/2d-array/problem

---
+++ hacker_rank/interview_preparation_kit/arrays/ds_2d_array_test.go
@@ -0,0 +1,32 @@
+package arrays
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("https://www.hackerrank.com/challenges/2d-array/problem", func() {
+	It("문제를 풀었다", func() {
+		arr := [][]int32{
+			{-9, -9, -9, 1, 1, 1},
+			{0, -9, 0, 4, 3, 2},
+			{-9, -9, -9, 1, 2, 3},
+			{0, 0, 8, 6, 6, 0},
+			{0, 0, 0, -2, 0, 0},
+			{0, 0, 1, 2, 4, 0},
+		}
+		actual := hourglassSum(arr)
+		Expect(actual).Should(BeNumerically("==", 28))
+
+		arr = [][]int32{
+			{1, 1, 1, 0, 0, 0},
+			{0, 1, 0, 0, 0, 0},
+			{1, 1, 1, 0, 0, 0},
+			{0, 0, 2, 4, 4, 0},
+			{0, 0, 0, 2, 0, 0},
+			{0, 0, 1, 2, 4, 0},
+		}
+		actual = hourglassSum(arr)
+		Expect(actual).Should(BeNumerically("==", 19))
+	})
+})
