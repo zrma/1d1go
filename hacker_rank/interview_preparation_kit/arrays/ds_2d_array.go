b4233f8f16155832ec953458d4c6df430b1a7161
zrma <bulbitain@gmail.com>
2019-02-10 15:29:44 +0900

    solve 2D Array - DS
    https://www.hackerrank.com/challenges/2d-array/problem

---
+++ hacker_rank/interview_preparation_kit/arrays/ds_2d_array.go
@@ -0,0 +1,36 @@
+package arrays
+
+import "math"
+
+func hourglassSum(arr [][]int32) int32 {
+
+	rowSize := len(arr)
+	if rowSize == 0 || rowSize < 3 {
+		return 0
+	}
+
+	colSize := len(arr[0])
+	if colSize < 3 {
+		return 0
+	}
+
+	getValue := func(x, y int) int32 {
+		sum := arr[y-1][x-1] + arr[y-1][x] + arr[y-1][x+1]
+		sum += arr[y][x]
+		sum += arr[y+1][x-1] + arr[y+1][x] + arr[y+1][x+1]
+
+		return sum
+	}
+
+	var max int32 = math.MinInt32
+	for y := 1; y < rowSize-1; y++ {
+		for x := 1; x < colSize-1; x++ {
+			val := getValue(x, y)
+			if max < val {
+				max = val
+			}
+		}
+	}
+
+	return max
+}
