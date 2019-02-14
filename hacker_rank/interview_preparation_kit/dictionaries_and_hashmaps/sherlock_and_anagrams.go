ec981f5140c4bb3aca2ae9e1cb448f44cac459de
zrma <bulbitain@gmail.com>
2019-02-14 23:39:39 +0900

    add test code of Sherlock and Anagrams
    https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem

---
+++ hacker_rank/interview_preparation_kit/dictionaries_and_hashmaps/sherlock_and_anagrams.go
@@ -0,0 +1,22 @@
+package dictionaries_and_hashmaps
+
+func sherlockAndAnagrams(s string) int32 {
+	//m := make(map[uint8]int)
+	//for i := 0; i < len(s); i++ {
+	//	if _, ok := m[s[i]]; !ok {
+	//		m[s[i]] = 0
+	//	}
+	//	m[s[i]]++
+	//}
+	//
+	//var seed []string
+	//for k, v := range m {
+	//	if v > 0 {
+	//		idx := strings.IndexAny(s, string(k))
+	//	}
+	//}
+	//
+	//strings.Index(s, seed)
+
+	panic("implement me")
+}
