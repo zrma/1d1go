fb592688305f6ef072331fcd7704c9bc71b63bc3
zrma <bulbitain@gmail.com>
2019-02-17 00:41:09 +0900

    solve Exceeding the Speed Limit
    https://www.hackerrank.com/contests/university-codesprint-5/challenges/exceeding-speed-limit

---
+++ hacker_rank/contests/university_codesprint_5/university_codesprint_5_suite_test.go
@@ -0,0 +1,13 @@
+package university_codesprint_5_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestUniversityCodesprint5(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "UniversityCodesprint5 Suite")
+}
