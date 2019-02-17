c9ac84a52a8fb6ed2fef75a799a3141004710de0
zrma <bulbitain@gmail.com>
2019-02-18 00:08:54 +0900

    solve Count Triplets
    https://www.hackerrank.com/challenges/count-triplets-1/problem

---
+++ hacker_rank/interview_preparation_kit/dictionaries_and_hashmaps/count_triplets.go
@@ -0,0 +1,31 @@
+package dictionaries_and_hashmaps
+
+func countTriplets(arr []int64, r int64) int64 {
+	if r == 1 {
+		map1 := make(map[int64]int64)
+		for _, num := range arr {
+			map1[num]++
+		}
+
+		var total int64
+		for _, num := range map1 {
+			// length Combinations of 3
+			// lC3
+			// lC2 = l * (l - 1) * (l - 2) / (3 * 2 * 1)
+			total += num * (num - 1) * (num - 2) / 6
+		}
+		return total
+	}
+
+	map2 := make(map[int64]int64)
+	map3 := make(map[int64]int64)
+
+	var total int64
+	for _, num := range arr {
+		total += map3[num]
+		map3[num*r] += map2[num]
+		map2[num*r]++
+	}
+
+	return total
+}
