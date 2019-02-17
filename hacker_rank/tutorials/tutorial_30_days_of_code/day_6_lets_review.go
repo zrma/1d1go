99a766a363436bd15fe9d38e4f9a478ad2fb5c07
zrma <bulbitain@gmail.com>
2019-02-17 23:07:56 +0900

    solve Day 6: Let's Review
    https://www.hackerrank.com/challenges/30-review-loop/problem

---
+++ hacker_rank/tutorials/tutorial_30_days_of_code/day_6_lets_review.go
@@ -0,0 +1,21 @@
+package tutorial_30_days_of_code
+
+import "fmt"
+
+func letsReview(s string) {
+	var even, odd string
+	for i := 0; i < len(s); i++ {
+		if i % 2 == 0 {
+			even += string(s[i])
+		} else {
+			odd += string(s[i])
+		}
+	}
+	fmt.Println(even, odd)
+}
+
+func LetsReview(ss []string) {
+	for _, s := range ss {
+		letsReview(s)
+	}
+}
