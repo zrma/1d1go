12ac7e02cedff7b6da796513fd313516bbec1466
zrma <bulbitain@gmail.com>
2019-02-17 23:07:56 +0900

    solve Day 6: Let's Review
    https://www.hackerrank.com/challenges/30-review-loop/problem

---
+++ hacker_rank/tutorials/tutorial_30_days_of_code/day_6_lets_review_test.go
@@ -0,0 +1,24 @@
+package tutorial_30_days_of_code
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("https://www.hackerrank.com/challenges/30-loops/problem", func() {
+	It("문제를 풀었다", func(done Done) {
+		go func() {
+			defer GinkgoRecover()
+
+			ExampleLetsReview()
+			
+			close(done)
+		}()
+	})
+})
+
+func ExampleLetsReview() {
+	LetsReview([]string{"Hacker", "Rank"})
+	// Output:
+	// Hce akr
+	// Rn ak
+}
