e8b79949c5ab1d285dfc72823be5f8fd15c33429
zrma <bulbitain@gmail.com>
2019-02-18 23:30:29 +0900

    solve Day 7: Arrays
    https://www.hackerrank.com/challenges/30-arrays/problem

--- hacker_rank/interview_preparation_kit/dictionaries_and_hashmaps/count_triplets.go
+++ hacker_rank/interview_preparation_kit/dictionaries_and_hashmaps/count_triplets.go
@@ -22,8 +22,15 @@ func countTriplets(arr []int64, r int64) int64 {
 
 	var total int64
 	for _, num := range arr {
+		// 등비수열 3짝의 첫 수가 등장할 경우
+
+		// 다음 이터레이션 언젠가 두 번째 수가 등장할 경우를 위해 그 쪽에 비축. increase 1
 		map2[num*r]++
+
+		// 이전 이터레이션 언젠가 등장했던 두 번째 수를 세 번째로 합산 이관(비축).
 		map3[num*r] += map2[num]
+
+		// 세 번째 이터레이션이 마무리 되었다면 이전의 누적이 모두 이관 완료 된 것. 정산.
 		total += map3[num]
 	}
 
