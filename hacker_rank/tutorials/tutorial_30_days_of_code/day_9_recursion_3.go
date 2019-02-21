d102d3976f6a1efa4839b810705a50dff4def293
zrma <bulbitain@gmail.com>
2019-02-22 00:38:28 +0900

    solve Day 9: Recursion 3
    https://www.hackerrank.com/challenges/30-recursion/problem

---
+++ hacker_rank/tutorials/tutorial_30_days_of_code/day_9_recursion_3.go
@@ -0,0 +1,21 @@
+package tutorial_30_days_of_code
+
+var m map[int32]int32
+
+func factorial(n int32) int32 {
+	if n <= 1 {
+		return 1
+	}
+
+	if m == nil {
+		m = make(map[int32]int32)
+	}
+
+	if v, ok := m[n]; ok {
+		return v
+	}
+
+	result := n * factorial(n-1)
+	m[n] = result
+	return result
+}
