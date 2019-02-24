fe70c34fb46fc48b90d54525193652a83bd005a4
zrma <bulbitain@gmail.com>
2019-02-24 21:32:36 +0900

    solve Day 12: Inheritance
    https://www.hackerrank.com/challenges/30-inheritance/problem

---
+++ hacker_rank/tutorials/tutorial_30_days_of_code/day_12_inheritance_test.go
@@ -0,0 +1,26 @@
+package tutorial_30_days_of_code
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("https://www.hackerrank.com/challenges/30-inheritance/problem", func() {
+	//noinspection SpellCheckingInspection
+	It("문제를 풀었다", func() {
+		s := NewStudent("Heraldo", "Memelli", 8135627, []int{100, 80})
+		Expect(s.printPerson()).Should(Equal("Name: Memelli, Heraldo\nID: 8135627"))
+		Expect(s.calculate()).Should(Equal("O"))
+
+		s.testScores = []int{80}
+		Expect(s.calculate()).Should(Equal("E"))
+		s.testScores = []int{70}
+		Expect(s.calculate()).Should(Equal("A"))
+		s.testScores = []int{55}
+		Expect(s.calculate()).Should(Equal("P"))
+		s.testScores = []int{40}
+		Expect(s.calculate()).Should(Equal("D"))
+		s.testScores = []int{30}
+		Expect(s.calculate()).Should(Equal("T"))
+	})
+})
