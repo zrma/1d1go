f2aa9323b1e38a1892c8c4af6b90986157ef39c5
zrma <bulbitain@gmail.com>
2019-02-24 21:03:51 +0900

    solve Day 11: 2D Arrays
    https://www.hackerrank.com/challenges/30-2d-arrays/problem

---
+++ hacker_rank/tutorials/tutorial_30_days_of_code/day_11_2d_arrays.go
@@ -0,0 +1,11 @@
+package tutorial_30_days_of_code
+
+import (
+	"fmt"
+	"github.com/zrma/1d1c/hacker_rank/interview_preparation_kit/arrays"
+)
+
+func HourGlassSum(arr [][]int32) {
+	result := arrays.HourGlassSum(arr)
+	fmt.Println(result)
+}
