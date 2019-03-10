65ea9c5b6c249bc85626f8fe8b5ce665603559a8
zrma <bulbitain@gmail.com>
2019-03-10 20:06:09 +0900

    Solve Day 21: Generics
    https://www.hackerrank.com/challenges/30-generics/problem

---
+++ hacker_rank/tutorials/tutorial_30_days_of_code/day_21_generics_test.go
@@ -0,0 +1,33 @@
+package tutorial_30_days_of_code
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/zrma/1d1c/hacker_rank/common/utils"
+)
+
+var _ = Describe("https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem", func() {
+	It("문제를 풀었다", func() {
+		err := utils.PrintTest(func() error {
+			var arr []interface{}
+			for _, data := range []int{1, 2, 3} {
+				arr = append(arr, data)
+			}
+			printArray(arr...)
+
+			arr = arr[:0]
+			for _, data := range []string{"Hello", "World"} {
+				arr = append(arr, data)
+			}
+			printArray(arr...)
+			return nil
+		}, []string{
+			"1",
+			"2",
+			"3",
+			"Hello",
+			"World",
+		})
+		Expect(err).ShouldNot(HaveOccurred())
+	})
+})
