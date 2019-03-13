65c8fc56e7b1f4ee9212a54cf11c28e81d4206fe
zrma <bulbitain@gmail.com>
2019-03-14 01:29:17 +0900

    Solve Day 23: BST Level-Order Traversal
    https://www.hackerrank.com/challenges/30-binary-trees/problem

---
+++ hacker_rank/tutorials/tutorial_30_days_of_code/day_23_bst_level_order_traversal_test.go
@@ -0,0 +1,18 @@
+package tutorial_30_days_of_code
+
+import (
+	"github.com/go-test/deep"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("https://stackoverflow.com/questions/2818852/is-there-a-queue-implementation", func() {
+	It("문제를 풀었다", func() {
+		var root *treeNode
+		for _, num := range []int{3, 5, 4, 7, 2, 1} {
+			root = insert(root, num)
+		}
+		diff := deep.Equal(levelOrder(root), []int{3, 2, 5, 1, 4, 7})
+		Expect(diff).Should(BeNil())
+	})
+})
