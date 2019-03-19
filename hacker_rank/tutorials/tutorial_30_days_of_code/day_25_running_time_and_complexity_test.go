f6689050849c4acdcedb0e2256c7447ed7a845a3
zrma <bulbitain@gmail.com>
2019-03-20 00:36:16 +0900

    Solve Day 25: Running Time and Complexity
    https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem

---
+++ hacker_rank/tutorials/tutorial_30_days_of_code/day_25_running_time_and_complexity_test.go
@@ -0,0 +1,24 @@
+package tutorial_30_days_of_code
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem", func() {
+	It("문제를 풀었다", func() {
+		Expect(isPrime(1)).Should(BeFalse())
+		Expect(isPrime(2)).Should(BeTrue())
+		Expect(isPrime(3)).Should(BeTrue())
+		Expect(isPrime(4)).Should(BeFalse())
+		Expect(isPrime(5)).Should(BeTrue())
+		Expect(isPrime(7)).Should(BeTrue())
+		Expect(isPrime(10)).Should(BeFalse())
+		Expect(isPrime(12)).Should(BeFalse())
+		Expect(isPrime(79)).Should(BeTrue())
+		Expect(isPrime(131)).Should(BeTrue())
+		Expect(isPrime(169)).Should(BeFalse())
+		Expect(isPrime(173)).Should(BeTrue())
+		Expect(isPrime(177)).Should(BeFalse())
+	})
+})
