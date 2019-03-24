a81365de28f61bc4aa34e0dd705dec533b610129
zrma <bulbitain@gmail.com>
2019-03-25 00:24:09 +0900

    Solve Day 29: Bitwise AND
    https://www.hackerrank.com/challenges/30-bitwise-and/problem

---
+++ hacker_rank/tutorials/tutorial_30_days_of_code/day_29_bitwise_and_test.go
@@ -0,0 +1,24 @@
+package tutorial_30_days_of_code
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("https://www.hackerrank.com/challenges/30-bitwise-and/problem", func() {
+	type testData struct {
+		n, k   int32
+		expect int32
+	}
+	DescribeTable("문제를 풀었다.",
+		func(data testData) {
+			actual := bitwiseAND(data.n, data.k)
+			Expect(actual).Should(BeNumerically("==", data.expect))
+		},
+		Entry("test_0", testData{5, 2, 1}),
+		Entry("test_1", testData{8, 5, 4}),
+		Entry("test_2", testData{2, 2, 0}),
+		Entry("test_2", testData{955, 236, 235}),
+	)
+})
