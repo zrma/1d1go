4669746ed9c9561b386687ad6898700a82d97afa
zrma <bulbitain@gmail.com>
2019-03-28 01:25:34 +0900

    Do refactoring - modify some variable name, and add comments.

--- hacker_rank/interview_preparation_kit/dictionaries_and_hashmaps/frequency_queries.go
+++ hacker_rank/interview_preparation_kit/dictionaries_and_hashmaps/frequency_queries.go
@@ -1,23 +1,23 @@
 package dictionaries_and_hashmaps
 
 func freqQuery(queries [][]int32) []int32 {
-	m1 := make(map[int32]int32)
-	m2 := make(map[int32]int32)
+	m1 := make(map[int32]int32) // 횟수 집계 용도
+	m2 := make(map[int32]int32) // 횟수 색인 용도
 
 	var result []int32
 	for _, q := range queries {
 		opCode, opLand := q[0], q[1]
 		switch opCode {
 		case 1:
-			old := m1[opLand]
-			m1[opLand] = old + 1
-			m2[old]--
-			m2[old+1]++
+			cnt := m1[opLand]
+			m1[opLand] = cnt + 1
+			m2[cnt]--
+			m2[cnt+1]++
 		case 2:
-			if old, ok := m1[opLand]; ok && old > 0 {
+			if cnt, ok := m1[opLand]; ok && cnt > 0 {
 				m1[opLand]--
-				m2[old]--
-				m2[old-1]++
+				m2[cnt]--
+				m2[cnt-1]++
 			}
 		case 3:
 			if m2[opLand] > 0 {
