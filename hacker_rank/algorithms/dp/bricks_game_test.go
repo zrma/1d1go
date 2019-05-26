5124d50d00d13ddf6c9ae52613530efc111d8be1
zrma <bulbitain@gmail.com>
2019-05-26 23:52:31 +0900

    #5 - solve Candies
    https://www.hackerrank.com/challenges/candies/problem

--- hacker_rank/algorithms/dp/bricks_game_test.go
+++ hacker_rank/algorithms/dp/bricks_game_test.go
@@ -28,6 +28,34 @@ var _ = Describe("https://www.hackerrank.com/challenges/play-game/problem", func
 	})
 
 	Measure("성능 테스트", func(b Benchmarker) {
+		readCSV := func(fileName string) ([][]int32, error) {
+			file, err := os.Open(fileName)
+			if err != nil {
+				return nil, err
+			}
+			defer file.Close()
+
+			r := csv.NewReader(bufio.NewReader(file))
+			rows, err := r.ReadAll()
+			if err != nil {
+				return nil, err
+			}
+
+			var arr [][]int32
+			for _, row := range rows {
+				var cols []int32
+				for _, col := range row {
+					num, err := strconv.ParseInt(col, 10, 32)
+					if err != nil {
+						return nil, err
+					}
+					cols = append(cols, int32(num))
+				}
+				arr = append(arr, cols)
+			}
+			return arr, nil
+		}
+
 		runtime := b.Time("long string", func() {
 			arr, err := readCSV("./test_data/bricks_game_0.csv")
 			Expect(err).ShouldNot(HaveOccurred())
@@ -59,31 +87,3 @@ var _ = Describe("https://www.hackerrank.com/challenges/play-game/problem", func
 		Expect(runtime.Seconds()).Should(BeNumerically("<", 5), "시간 초과")
 	}, 3)
 })
-
-func readCSV(fileName string) ([][]int32, error) {
-	file, err := os.Open(fileName)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	r := csv.NewReader(bufio.NewReader(file))
-	rows, err := r.ReadAll()
-	if err != nil {
-		return nil, err
-	}
-
-	var arr [][]int32
-	for _, row := range rows {
-		var cols []int32
-		for _, col := range row {
-			num, err := strconv.ParseInt(col, 10, 32)
-			if err != nil {
-				return nil, err
-			}
-			cols = append(cols, int32(num))
-		}
-		arr = append(arr, cols)
-	}
-	return arr, nil
-}
