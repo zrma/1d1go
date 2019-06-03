5080801e116ff3994d388281184857cf64677f37
zrma <bulbitain@gmail.com>
2019-06-04 08:03:59 +0900

    Add comment.

--- hacker_rank/algorithms/dp/sam_and_substrings.go
+++ hacker_rank/algorithms/dp/sam_and_substrings.go
@@ -4,25 +4,48 @@ import (
 	"strconv"
 )
 
+// 1     : 1                             = 1
+// 12    : 1 2 12                        = 15
+// 123   : 1 2 12 3 23 123               = 164
+// 1234  : 1 2 12 3 23 123 4 34 234 1234 = 1670
+//
+// 1     : 1
+//          |  a     |
+// 12    : 1 (2) (12)      ───┐
+//            b               │
+//                   |       a'       |
+// 123   : 1  2   12 | 3  (2)3 (12)3  |  => ((2) + (12)) * 10 + 3*3
+//                  b'                   => (0 + 2 + 12) * 10 + 3 + 3 + 3
+//                                       => 00 +20 +120       + 3 + 3 + 3
+//                                       => 03 +23 +123
+// a' = a * 10 + (i + 1) * cur
+//
+// b' = b + a'
+//
+// a   0           1            14               149
+// a'  1   10+2*2(14)  140+3*3(149)    1490+4*4(1506)
+// b   0           1            15               164
+// b'  1          15           164              1670
+
 func substrings(n string) int32 {
 	const modulo = 1000000007
 
 	length := len(n)
-	prev, err := strconv.Atoi(string(n[0]))
+	b, err := strconv.Atoi(string(n[0]))
 	if err != nil {
 		return 0
 	}
 
-	sum := prev
+	a := b
 	var i int
 	for i = 1; i < length; i++ {
-		sub, err := strconv.Atoi(string(n[i]))
+		cur, err := strconv.Atoi(string(n[i]))
 		if err != nil {
-			sub = 0
+			cur = 0
 		}
-		sum = (sum*10 + sub*(i+1)) % modulo
-		prev = (sum + prev) % modulo
+		a = (a*10 + (i+1)*cur) % modulo
+		b = (a + b) % modulo
 	}
 
-	return int32(prev)
+	return int32(b)
 }
