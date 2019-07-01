eef5e021c9909131b554e051b94871379e32cf02
zrma <bulbitain@gmail.com>
2019-07-01 14:01:25 +0900

    Reword l -> length, and fix mistaken rewording: l -> length -> list

--- hacker_rank/data_structures/arrays/left_rotate.go
+++ hacker_rank/data_structures/arrays/left_rotate.go
@@ -1,14 +1,14 @@
 package arrays
 
 func rotLeft(a []int32, d int32) []int32 {
-	l := int32(len(a))
+	length := int32(len(a))
 
-	if (d < 1) || (d%l == 0) {
+	if (d < 1) || (d%length == 0) {
 		return a
 	}
 
-	if d > l {
-		d = d % l
+	if d > length {
+		d = d % length
 	}
 
 	result := a[d:]
