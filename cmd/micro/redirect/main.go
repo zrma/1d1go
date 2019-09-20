e3336a4bea723180fc0ce39b9f241b09c339f11f
zrma <bulbitain@gmail.com>
2019-09-20 10:20:03 +0900

    Add go-micro redirection example

---
+++ cmd/micro/redirect/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+
+	"context"
+	"github.com/micro/go-micro"
+	api "github.com/micro/micro/api/proto"
+)
+
+// Redirect implements url route
+type Redirect struct{}
+
+// URL function does redirection
+func (r *Redirect) URL(ctx context.Context, req *api.Request, rsp *api.Response) error {
+	rsp.StatusCode = int32(301)
+	rsp.Header = map[string]*api.Pair{
+		"Location": {
+			Key:    "Location",
+			Values: []string{"https://google.com"},
+		},
+	}
+	return nil
+}
+
+func main() {
+	service := micro.NewService(
+		micro.Name("go.micro.api.redirect"),
+	)
+
+	// parse command line flags
+	service.Init()
+
+	if err := service.Server().Handle(
+		service.Server().NewHandler(new(Redirect)),
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
