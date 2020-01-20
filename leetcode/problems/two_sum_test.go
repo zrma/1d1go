6cd16bb7d0449a1f9b8df12f2f5af6e4a9fdb503
zrma <bulbitain@gmail.com>
2020-01-21 01:40:33 +0900

    Solve Two Sum II - Input array is sorted
    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/

--- leetcode/problems/two_sum_test.go
+++ leetcode/problems/two_sum_test.go
@@ -11,3 +11,10 @@ var _ = Describe("https://leetcode.com/problems/two-sum/", func() {
 		Expect(actual).Should(Equal([]int{0, 1}))
 	})
 })
+
+var _ = Describe("https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/", func() {
+	It("문제를 풀었다.", func() {
+		actual := twoSum2InputArrayIsSorted([]int{2, 7, 11, 15}, 9)
+		Expect(actual).Should(Equal([]int{1, 2}))
+	})
+})
