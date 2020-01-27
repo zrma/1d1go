26edd8f3c412c8d4d2323e12f92b3390b98e7757
zrma <bulbitain@gmail.com>
2020-01-27 21:34:28 +0900

    Solve Longest Substring Without Repeating Characters
    https://leetcode.com/problems/longest-substring-without-repeating-characters/

---
+++ leetcode/problems/longest_substring.go
@@ -0,0 +1,37 @@
+package problems
+
+import "math"
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	curLength := 1
+	maxLength := 1
+	prevIdx := 0
+
+	visited := make([]int, math.MaxUint8)
+	for i := range visited {
+		visited[i] = -1
+	}
+	visited[s[0]] = 0
+
+	for i := 1; i < len(s); i++ {
+		prevIdx = visited[s[i]]
+		if prevIdx == -1 || (i-curLength > prevIdx) {
+			curLength++
+		} else {
+			if curLength > maxLength {
+				maxLength = curLength
+			}
+			curLength = i - prevIdx
+		}
+		visited[s[i]] = i
+	}
+
+	if curLength > maxLength {
+		maxLength = curLength
+	}
+	return maxLength
+}
