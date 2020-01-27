8864f29098e17420977bdedb43ad1f5da80fbf49
zrma <bulbitain@gmail.com>
2020-01-27 21:34:28 +0900

    Solve Longest Substring Without Repeating Characters
    https://leetcode.com/problems/longest-substring-without-repeating-characters/

---
+++ leetcode/problems/longest_substring_test.go
@@ -0,0 +1,26 @@
+package problems
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("https://leetcode.com/problems/longest-substring-without-repeating-characters/", func() {
+	type testData struct {
+		input    string
+		expected int
+	}
+
+	//noinspection SpellCheckingInspection
+	DescribeTable("문제를 풀었다",
+		func(data testData) {
+			actual := lengthOfLongestSubstring(data.input)
+			Expect(actual).Should(Equal(data.expected))
+		},
+		Entry("0", testData{"abcabcbb", 3}),
+		Entry("1", testData{"bbbbb", 1}),
+		Entry("2", testData{"pwwkew", 3}),
+		Entry("3", testData{"", 0}),
+	)
+})
