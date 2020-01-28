aa87327d9dfe4edc8ee36cdb9547fe7ddb1d852a
zrma <bulbitain@gmail.com>
2020-01-28 22:13:12 +0900

    Use array instead of slice, add test cases

--- leetcode/problems/longest_substring_test.go
+++ leetcode/problems/longest_substring_test.go
@@ -22,5 +22,7 @@ var _ = Describe("https://leetcode.com/problems/longest-substring-without-repeat
 		Entry("1", testData{"bbbbb", 1}),
 		Entry("2", testData{"pwwkew", 3}),
 		Entry("3", testData{"", 0}),
+		Entry("4", testData{"au", 2}),
+		Entry("5", testData{"abba", 2}),
 	)
 })
