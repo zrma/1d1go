f9d1c79c009fca196ab911232bb499dcc78e242c
zrma <bulbitain@gmail.com>
2020-01-30 20:31:19 +0900

    Add comment

--- leetcode/problems/longest_substring.go
+++ leetcode/problems/longest_substring.go
@@ -17,6 +17,25 @@ func lengthOfLongestSubstring(s string) int {
 	}
 	visited[s[0]] = 0
 
+	// 한 문자가 다시 출현할 때까지의 최대 거리를 구한다.
+	// abcabc
+	// |--|
+	// 0123
+	//
+	// prevIdx = 0
+	// curLength -> 2까지 증가
+	//
+	// i < 3 일 경우
+	//
+	// b, c의 경우 모두 prevIdx == -1
+	//
+	// i == 1 : 1 - 1 == 0 (prevIdx == 0)
+	// i == 2 : 2 - 1 == 0 -> curLength++ -> 2
+	// i == 3 : curLength(2) > maxLength(1) -> maxLength(2), curLength = 3 - 0 = 3
+	//
+	// ... 중략
+	//
+	// curLength(3) > maxLength(2) -> maxLength(3)
 	for i := 1; i < len(s); i++ {
 		prevIdx = visited[s[i]]
 		if prevIdx == -1 || (i-curLength > prevIdx) {
