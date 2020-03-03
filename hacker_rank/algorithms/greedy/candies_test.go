cdcf8448cce6a9c327914c6b94304687dab7aa40
zrma <bulbitain@gmail.com>
2020-03-03 19:08:27 +0900

    Convert ginkgo test to testing

--- hacker_rank/algorithms/greedy/candies_test.go
+++ hacker_rank/algorithms/greedy/candies_test.go
@@ -3,55 +3,68 @@ package greedy
 import (
 	"bufio"
 	"encoding/csv"
-	. "github.com/onsi/ginkgo/extensions/table"
 	"os"
 	"strconv"
+	"testing"
+	"time"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"gotest.tools/assert"
 )
 
-var _ = Describe("https://www.hackerrank.com/challenges/candies/problem", func() {
-	type testData struct {
-		arr      []int32
-		expected int64
-	}
+type Done chan struct{}
 
-	DescribeTable("문제를 풀었다",
-		func(data testData) {
+func TestCandies(t *testing.T) {
+	t.Parallel()
+
+	t.Run("https://www.hackerrank.com/challenges/candies/problem", func(t *testing.T) {
+		for _, data := range []struct {
+			arr      []int32
+			expected int64
+		}{
+			{[]int32{1, 2, 2}, 4},
+			{[]int32{2, 4, 2, 6, 1, 7, 8, 9, 2, 1}, 19},
+			{[]int32{2, 4, 3, 5, 2, 6, 4, 5}, 12},
+			{[]int32{1, 3, 3, 3, 2, 1}, 10},
+			{[]int32{1, 3, 3, 3, 2, 2, 2, 3, 3, 3, 1}, 15},
+		} {
 			actual := candies(int32(len(data.arr)), data.arr)
-			Expect(actual).Should(BeNumerically("==", data.expected))
-		},
-		Entry("test_0", testData{[]int32{1, 2, 2}, 4}),
-		Entry("test_1", testData{[]int32{2, 4, 2, 6, 1, 7, 8, 9, 2, 1}, 19}),
-		Entry("test_2", testData{[]int32{2, 4, 3, 5, 2, 6, 4, 5}, 12}),
-		Entry("test_3", testData{[]int32{1, 3, 3, 3, 2, 1}, 10}),
-		Entry("test_4", testData{[]int32{1, 3, 3, 3, 2, 2, 2, 3, 3, 3, 1}, 15}),
-	)
+			assert.Equal(t, actual, data.expected)
+		}
+	})
 
-	Measure("성능 테스트", func(b Benchmarker) {
-		runtime := b.Time("long array", func() {
+	t.Run("", func(t *testing.T) {
+		timeout := time.After(3 * time.Second)
+		waitCh := make(chan struct{})
+		go func(done Done) {
 			file, err := os.Open("./test_data/candies.csv")
-			Expect(err).ShouldNot(HaveOccurred())
+			assert.NilError(t, err)
 			defer file.Close()
 
 			r := csv.NewReader(bufio.NewReader(file))
 			rows, err := r.ReadAll()
-			Expect(err).ShouldNot(HaveOccurred())
+			assert.NilError(t, err)
 
 			var arr []int32
 			for _, col := range rows {
 				num, err := strconv.ParseInt(col[0], 10, 32)
-				Expect(err).ShouldNot(HaveOccurred())
+				assert.NilError(t, err)
 
 				arr = append(arr, int32(num))
 			}
 
-			Expect(len(arr)).Should(Equal(100000))
-			actual := candies(int32(len(arr)), arr)
-			Expect(actual).Should(BeNumerically("==", 160929))
-		})
+			assert.Equal(t, len(arr), 100000)
 
-		Expect(runtime.Seconds()).Should(BeNumerically("<", 5), "시간 초과")
-	}, 3)
-})
+			actual := candies(int32(len(arr)), arr)
+			var expected int64 = 160929
+			assert.Equal(t, actual, expected)
+
+			close(done)
+		}(waitCh)
+
+		select {
+		case <-timeout:
+			t.Fatal("timeout")
+		case <-waitCh:
+		}
+	})
+}
