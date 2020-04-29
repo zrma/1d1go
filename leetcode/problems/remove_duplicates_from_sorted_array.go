aaf2383366928cc08ef08bcbd186a58870d18a25
zrma <bulbitain@gmail.com>
2020-04-30 00:19:31 +0900

    leetcode: solve Remove Duplicates from Sorted Array
    https://leetcode.com/problems/remove-duplicates-from-sorted-array/

---
+++ leetcode/problems/remove_duplicates_from_sorted_array.go
@@ -0,0 +1,18 @@
+package problems
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	cnt := 1
+	prev := nums[0]
+	for _, n := range nums[1:] {
+		if n != prev {
+			nums[cnt] = n
+			cnt++
+			prev = n
+		}
+	}
+	return cnt
+}
