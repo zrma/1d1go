78d38bc94171d048371574d38efa5e309d5c073b
zrma <bulbitain@gmail.com>
2020-05-01 21:53:59 +0900

    leetcode: do refactoring; reduce cognitive complexity

--- leetcode/problems/lv1medium/longest_palindromic_substring.go
+++ leetcode/problems/lv1medium/longest_palindromic_substring.go
@@ -1,73 +1,63 @@
 package lv1medium
 
+func isPalindrome(s string, start, end int) bool {
+	for start < end {
+		if s[start] != s[end] {
+			return false
+		}
+		start++
+		end--
+	}
+	return true
+}
+
+type checkOptions struct {
+	index  int
+	offset int
+
+	maxLength int
+	min, max  int
+
+	length int
+	s      string
+}
+
+func checkByRange(opts *checkOptions) {
+	start := opts.index - opts.offset
+	if start < 0 {
+		return
+	}
+
+	for i := -1; i < 2; i++ {
+		end := opts.index + opts.offset + i
+		if end >= opts.length {
+			break
+		}
+
+		if isPalindrome(opts.s, start, end) && (end-start+1) > opts.maxLength {
+			opts.maxLength = end - start + 1
+			opts.min = start
+			opts.max = end
+		}
+	}
+	return
+}
+
 func longestPalindrome(s string) string {
 	length := len(s)
 	if length < 2 || (length <= 3 && s[0] == s[length-1]) {
 		return s
 	}
 
-	isPalindrome := func(start, end int) bool {
-		for start < end {
-			if s[start] != s[end] {
-				return false
-			}
-			start++
-			end--
-		}
-		return true
+	opts := &checkOptions{
+		maxLength: 1,
+		length:    length,
+		s:         s,
 	}
-
-	var min, max int
-	maxLength := 1
-	for i := 1; i < length-1; i++ {
-		for offset := 0; offset <= length/2; offset++ {
-			start := i - offset
-			if start < 0 {
-				break
-			}
-
-			// 짝수 길이 0
-			{
-				end := i + offset - 1
-				if end >= length {
-					break
-				}
-
-				if isPalindrome(start, end) && (end-start+1) > maxLength {
-					maxLength = end - start + 1
-					min = start
-					max = end
-				}
-			}
-
-			// 홀수 길이
-			{
-				end := i + offset
-				if end >= length {
-					break
-				}
-
-				if isPalindrome(start, end) && (end-start+1) > maxLength {
-					maxLength = end - start + 1
-					min = start
-					max = end
-				}
-			}
-
-			// 짝수 길이 1
-			{
-				end := i + offset + 1
-				if end >= length {
-					break
-				}
-
-				if isPalindrome(start, end) && (end-start+1) > maxLength {
-					maxLength = end - start + 1
-					min = start
-					max = end
-				}
-			}
+	for opts.index = 1; opts.index < length-1; opts.index++ {
+		for opts.offset = 0; opts.offset <= length/2; opts.offset++ {
+			checkByRange(opts)
 		}
 	}
-	return s[min : max+1]
+	return s[opts.min : opts.max+1]
 }
