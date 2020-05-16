9aa3a46e776817a48ce9e5138d8b9cf0d9dc2f34
zrma <bulbitain@gmail.com>
2020-05-16 22:35:39 +0900

    leetcode: do refactoring; don't assign new slice

--- leetcode/problems/lv2hard/median_of_two_sorted_arrays.go
+++ leetcode/problems/lv2hard/median_of_two_sorted_arrays.go
@@ -1,39 +1,49 @@
 package lv2hard
 
-func mergeSortedArray(nums1 []int, nums2 []int) []int {
-	totalLength := len(nums1) + len(nums2)
-	total := make([]int, totalLength)
-
-	var i, j, cur int
-	for i < len(nums1) && j < len(nums2) {
-		if nums1[i] < nums2[j] {
-			total[cur] = nums1[i]
-			cur++
-			i++
+func findTwoValuesNearMid(nums1 []int, nums2 []int, totalLength int) (prev, curr int) {
+	target := totalLength / 2
+	var idx1, idx2, idxTot int
+	for idx1 < len(nums1) && idx2 < len(nums2) {
+		prev = curr
+		if nums1[idx1] < nums2[idx2] {
+			curr = nums1[idx1]
+			idxTot++
+			idx1++
 		} else {
-			total[cur] = nums2[j]
-			cur++
-			j++
+			curr = nums2[idx2]
+			idxTot++
+			idx2++
+		}
+		if idxTot == target+1 {
+			return
 		}
 	}
-	for i < len(nums1) {
-		total[cur] = nums1[i]
-		cur++
-		i++
+	for idx1 < len(nums1) {
+		prev = curr
+		curr = nums1[idx1]
+		idxTot++
+		idx1++
+		if idxTot == target+1 {
+			return
+		}
 	}
-	for j < len(nums2) {
-		total[cur] = nums2[j]
-		cur++
-		j++
+	for idx2 < len(nums2) {
+		prev = curr
+		curr = nums2[idx2]
+		idxTot++
+		idx2++
+		if idxTot == target+1 {
+			return
+		}
 	}
-	return total
+	return
 }
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	total := mergeSortedArray(nums1, nums2)
-	if len(total)%2 == 0 {
-		return float64(total[len(total)/2-1]+total[len(total)/2]) / 2
-	} else {
-		return float64(total[len(total)/2])
+	totalLength := len(nums1) + len(nums2)
+	val1, val2 := findTwoValuesNearMid(nums1, nums2, totalLength)
+	if totalLength%2 == 0 {
+		return float64(val1+val2) / 2
 	}
+	return float64(val2)
 }
