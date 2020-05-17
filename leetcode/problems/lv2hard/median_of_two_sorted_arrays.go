6262f2b44a8ad8e8914f41fd9e9c7ba686c762cf
zrma <bulbitain@gmail.com>
2020-05-17 12:31:55 +0900

    leetcode: recover removed code and add benchmarks

--- leetcode/problems/lv2hard/median_of_two_sorted_arrays.go
+++ leetcode/problems/lv2hard/median_of_two_sorted_arrays.go
@@ -1,5 +1,7 @@
 package lv2hard
 
+import "math"
+
 func findTwoValuesNearMid(nums1 []int, nums2 []int, totalLength int) (prev, curr int) {
 	target := totalLength / 2
 	var idx1, idx2, idxTot int
@@ -46,4 +48,88 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 		return float64(val1+val2) / 2
 	}
 	return float64(val2)
+}
+
+func mergeSortedArray(nums1 []int, nums2 []int) []int {
+	totalLength := len(nums1) + len(nums2)
+	total := make([]int, totalLength)
+
+	var i, j, cur int
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] < nums2[j] {
+			total[cur] = nums1[i]
+			cur++
+			i++
+		} else {
+			total[cur] = nums2[j]
+			cur++
+			j++
+
+		}
+	}
+	for i < len(nums1) {
+		total[cur] = nums1[i]
+		cur++
+		i++
+
+	}
+	for j < len(nums2) {
+		total[cur] = nums2[j]
+		cur++
+		j++
+
+	}
+	return total
+}
+
+func findMedianSortedArraysWithMerge(nums1 []int, nums2 []int) float64 {
+	total := mergeSortedArray(nums1, nums2)
+	if len(total)%2 == 0 {
+		return float64(total[len(total)/2-1]+total[len(total)/2]) / 2
+	} else {
+		return float64(total[len(total)/2])
+	}
+
+}
+
+func findMedianSortedArraysWithBinSearch(nums1 []int, nums2 []int) float64 {
+	if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+	half := (len(nums1) + len(nums2) - 1) / 2
+
+	l, r := 0, len(nums1)
+	i := (l + r) / 2
+	j := half - i
+	for l < r {
+		if nums1[i] < nums2[j] {
+			l = i + 1
+		} else if i > 0 && nums1[i-1] > nums2[j+1] {
+			r = i
+		} else {
+			break
+		}
+		i = (l + r) / 2
+		j = half - i
+	}
+
+	var medianLeft, medianRight float64
+	if j < 0 {
+		medianLeft = float64(nums1[i-1])
+	} else if i < 1 {
+		medianLeft = float64(nums2[j])
+	} else {
+		medianLeft = math.Max(float64(nums1[i-1]), float64(nums2[j]))
+	}
+
+	if (len(nums1)+len(nums2))%2 == 1 {
+		return medianLeft
+	} else if i == len(nums1) {
+		medianRight = float64(nums2[j+1])
+	} else if j+1 == len(nums2) {
+		medianRight = float64(nums1[i])
+	} else {
+		medianRight = math.Min(float64(nums1[i]), float64(nums2[j+1]))
+	}
+	return (medianLeft + medianRight) / 2
 }
