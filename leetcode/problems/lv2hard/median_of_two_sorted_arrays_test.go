f80cccab94fe143f8a09bca38c2f039a1fe0cd0a
zrma <bulbitain@gmail.com>
2020-05-24 22:38:06 +0900

    leetcode: add more simple solution

--- leetcode/problems/lv2hard/median_of_two_sorted_arrays_test.go
+++ leetcode/problems/lv2hard/median_of_two_sorted_arrays_test.go
@@ -30,6 +30,11 @@ var _ = Describe("https://leetcode.com/problems/median-of-two-sorted-arrays/", f
 			actual := findMedianSortedArraysWithBinSearch(data.arr1, data.arr2)
 			Expect(actual).Should(Equal(data.expected))
 		})
+
+		By("By bin search solution", func() {
+			actual := findMedianSortedArraysWithBinSearchSolution(data.arr1, data.arr2)
+			Expect(actual).Should(Equal(data.expected))
+		})
 	},
 		Entry("0", testData{
 			arr1:     []int{1, 3},
@@ -91,48 +96,63 @@ var _ = Describe("https://leetcode.com/problems/median-of-two-sorted-arrays/", f
 
 //goos: linux
 //goarch: amd64
-//BenchmarkFindMedian1/By_merge-16                126576813                9.36 ns/op
-//BenchmarkFindMedian1/By_index-16                1000000000               0.864 ns/op
-//BenchmarkFindMedian1/By_bin_search-16           1000000000               0.734 ns/op
-//BenchmarkFindMedian2/By_merge-16                86559476                12.5 ns/op
-//BenchmarkFindMedian2/By_index-16                1000000000               0.892 ns/op
-//BenchmarkFindMedian2/By_bin_search-16           1000000000               0.609 ns/op
-//BenchmarkFindMedian4/By_merge-16                51975051                23.1 ns/op
-//BenchmarkFindMedian4/By_index-16                946856868                1.24 ns/op
-//BenchmarkFindMedian4/By_bin_search-16           849017156                1.40 ns/op
-//BenchmarkFindMedian8/By_merge-16                26094500                42.2 ns/op
-//BenchmarkFindMedian8/By_index-16                672385303                1.83 ns/op
-//BenchmarkFindMedian8/By_bin_search-16           925608818                1.30 ns/op
-//BenchmarkFindMedian16/By_merge-16               21669801                52.5 ns/op
-//BenchmarkFindMedian16/By_index-16               494116309                2.50 ns/op
-//BenchmarkFindMedian16/By_bin_search-16          882739788                1.39 ns/op
-//BenchmarkFindMedian32/By_merge-16               18645150                64.3 ns/op
-//BenchmarkFindMedian32/By_index-16               571174806                2.19 ns/op
-//BenchmarkFindMedian32/By_bin_search-16          1000000000               1.09 ns/op
-//BenchmarkFindMedian64/By_merge-16                6408602               194 ns/op
-//BenchmarkFindMedian64/By_index-16               122519014                9.77 ns/op
-//BenchmarkFindMedian64/By_bin_search-16          918924092                1.28 ns/op
-//BenchmarkFindMedian128/By_merge-16               3674931               307 ns/op
-//BenchmarkFindMedian128/By_index-16              81134256                14.8 ns/op
-//BenchmarkFindMedian128/By_bin_search-16         585064473                1.99 ns/op
-//BenchmarkFindMedian256/By_merge-16               2416862               503 ns/op
-//BenchmarkFindMedian256/By_index-16              62001084                18.2 ns/op
-//BenchmarkFindMedian256/By_bin_search-16         690182328                1.73 ns/op
-//BenchmarkFindMedian512/By_merge-16               1669188               728 ns/op
-//BenchmarkFindMedian512/By_index-16              18578439                63.8 ns/op
-//BenchmarkFindMedian512/By_bin_search-16         953511543                1.30 ns/op
-//BenchmarkFindMedian1024/By_merge-16              1357064               888 ns/op
-//BenchmarkFindMedian1024/By_index-16             26570716                46.7 ns/op
-//BenchmarkFindMedian1024/By_bin_search-16        811126491                1.48 ns/op
-//BenchmarkFindMedian2048/By_merge-16               507120              2079 ns/op
-//BenchmarkFindMedian2048/By_index-16              7414038               164 ns/op
-//BenchmarkFindMedian2048/By_bin_search-16        1000000000               1.05 ns/op
-//BenchmarkFindMedian4096/By_merge-16               179924              6438 ns/op
-//BenchmarkFindMedian4096/By_index-16              2484904               485 ns/op
-//BenchmarkFindMedian4096/By_bin_search-16        467696221                2.52 ns/op
-//BenchmarkFindMedian8192/By_merge-16               122500              9718 ns/op
-//BenchmarkFindMedian8192/By_index-16              1262004               953 ns/op
-//BenchmarkFindMedian8192/By_bin_search-16        940063140                1.31 ns/op
+//pkg: github.com/zrma/1d1c/leetcode/problems/lv2hard
+//BenchmarkFindMedian1/By_merge-16                         127035747               9.02   ns/op
+//BenchmarkFindMedian1/By_index-16                        1000000000               0.929  ns/op
+//BenchmarkFindMedian1/By_bin_search-16                   1000000000               1.12   ns/op
+//BenchmarkFindMedian1/By_bin_search_solution-16          1000000000               0.803  ns/op
+//BenchmarkFindMedian2/By_merge-16                          75473596              13.3    ns/op
+//BenchmarkFindMedian2/By_index-16                        1000000000               0.863  ns/op
+//BenchmarkFindMedian2/By_bin_search-16                   1000000000               0.850  ns/op
+//BenchmarkFindMedian2/By_bin_search_solution-16          1000000000               0.682  ns/op
+//BenchmarkFindMedian4/By_merge-16                          46541391              23.9    ns/op
+//BenchmarkFindMedian4/By_index-16                         908933222               1.33   ns/op
+//BenchmarkFindMedian4/By_bin_search-16                    723928178               1.67   ns/op
+//BenchmarkFindMedian4/By_bin_search_solution-16           833685333               1.43   ns/op
+//BenchmarkFindMedian8/By_merge-16                          27980440              40.8    ns/op
+//BenchmarkFindMedian8/By_index-16                         658515704               1.81   ns/op
+//BenchmarkFindMedian8/By_bin_search-16                   1000000000               1.14   ns/op
+//BenchmarkFindMedian8/By_bin_search_solution-16           922768860               1.31   ns/op
+//BenchmarkFindMedian16/By_merge-16                         21129810              55.5    ns/op
+//BenchmarkFindMedian16/By_index-16                        493847685               2.43   ns/op
+//BenchmarkFindMedian16/By_bin_search-16                   756249932               1.61   ns/op
+//BenchmarkFindMedian16/By_bin_search_solution-16          867374154               1.40   ns/op
+//BenchmarkFindMedian32/By_merge-16                         17221905              67.9    ns/op
+//BenchmarkFindMedian32/By_index-16                        587363749               2.07   ns/op
+//BenchmarkFindMedian32/By_bin_search-16                  1000000000               1.10   ns/op
+//BenchmarkFindMedian32/By_bin_search_solution-16         1000000000               1.11   ns/op
+//BenchmarkFindMedian64/By_merge-16                          6141870             201      ns/op
+//BenchmarkFindMedian64/By_index-16                        123069411               9.83   ns/op
+//BenchmarkFindMedian64/By_bin_search-16                  1000000000               1.11   ns/op
+//BenchmarkFindMedian64/By_bin_search_solution-16          919578985               1.27   ns/op
+//BenchmarkFindMedian128/By_merge-16                         3690441             325      ns/op
+//BenchmarkFindMedian128/By_index-16                        80059777              14.6    ns/op
+//BenchmarkFindMedian128/By_bin_search-16                  638245250               1.86   ns/op
+//BenchmarkFindMedian128/By_bin_search_solution-16         617231878               1.94   ns/op
+//BenchmarkFindMedian256/By_merge-16                         2252612             503      ns/op
+//BenchmarkFindMedian256/By_index-16                        67390363              18.0    ns/op
+//BenchmarkFindMedian256/By_bin_search-16                  755317909               1.59   ns/op
+//BenchmarkFindMedian256/By_bin_search_solution-16         705693475               1.71   ns/op
+//BenchmarkFindMedian512/By_merge-16                         1644506             732      ns/op
+//BenchmarkFindMedian512/By_index-16                        19918366              61.5    ns/op
+//BenchmarkFindMedian512/By_bin_search-16                 1000000000               1.11   ns/op
+//BenchmarkFindMedian512/By_bin_search_solution-16         914383945               1.29   ns/op
+//BenchmarkFindMedian1024/By_merge-16                        1335741             891      ns/op
+//BenchmarkFindMedian1024/By_index-16                       26047544              45.3    ns/op
+//BenchmarkFindMedian1024/By_bin_search-16                 636004533               1.85   ns/op
+//BenchmarkFindMedian1024/By_bin_search_solution-16        853142835               1.39   ns/op
+//BenchmarkFindMedian2048/By_merge-16                         479786            2138      ns/op
+//BenchmarkFindMedian2048/By_index-16                        7599582             161      ns/op
+//BenchmarkFindMedian2048/By_bin_search-16                1000000000               0.816  ns/op
+//BenchmarkFindMedian2048/By_bin_search_solution-16       1000000000               0.977  ns/op
+//BenchmarkFindMedian4096/By_merge-16                         186649            6579      ns/op
+//BenchmarkFindMedian4096/By_index-16                        2550916             471      ns/op
+//BenchmarkFindMedian4096/By_bin_search-16                 299504170               4.01   ns/op
+//BenchmarkFindMedian4096/By_bin_search_solution-16        493479288               2.43   ns/op
+//BenchmarkFindMedian8192/By_merge-16                         122881            9501      ns/op
+//BenchmarkFindMedian8192/By_index-16                        1304148             924      ns/op
+//BenchmarkFindMedian8192/By_bin_search-16                1000000000               1.11   ns/op
+//BenchmarkFindMedian8192/By_bin_search_solution-16        947535735               1.28   ns/op
 func BenchmarkFindMedian1(b *testing.B)    { benchmarkFindMedianSortedArrays(1, b) }
 func BenchmarkFindMedian2(b *testing.B)    { benchmarkFindMedianSortedArrays(2, b) }
 func BenchmarkFindMedian4(b *testing.B)    { benchmarkFindMedianSortedArrays(4, b) }
@@ -182,6 +202,14 @@ func benchmarkFindMedianSortedArrays(cnt1 int, b *testing.B) {
 		b.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
 				findMedianSortedArraysWithBinSearch(nums1, nums2)
+			}
+		})
+	})
+
+	b.Run("By bin search solution", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				findMedianSortedArraysWithBinSearchSolution(nums1, nums2)
 			}
 		})
 	})
