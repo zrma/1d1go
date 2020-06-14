42dfd7d08ad80063614d5dea7b27a5df767f24ca
zrma <bulbitain@gmail.com>
2020-06-14 22:45:52 +0900

    hacker_rank: do refactoring; codesprint2019/alpha_and_beta

--- hacker_rank/contests/hacker_rank/allwomens/codesprint2019/alpha_and_beta_test.go
+++ hacker_rank/contests/hacker_rank/allwomens/codesprint2019/alpha_and_beta_test.go
@@ -2,14 +2,21 @@ package codesprint2019
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("https://www.hackerrank.com/contests/hackerrank-all-womens-codesprint-2019/challenges/alpha-and-beta", func() {
-	It("문제를 풀었다", func() {
-		Expect(alphaBeta([]int32{1, 2, 3, 3, 2, 1})).Should(BeNumerically("==", 2))
-		Expect(alphaBeta([]int32{1, 2, 3, 4, 5})).Should(BeNumerically("==", 4))
-		Expect(alphaBeta([]int32{1, 2, 3, 2, 1})).Should(BeNumerically("==", 2))
-		Expect(alphaBeta([]int32{1, 2, 4, 3, 2, 1})).Should(BeNumerically("==", 3))
-	})
+	type testData struct {
+		pile     []int32
+		expected int32
+	}
+	DescribeTable("문제를 풀었다", func(data testData) {
+		Expect(alphaBeta(data.pile)).Should(Equal(data.expected))
+	},
+		Entry("0", testData{[]int32{1, 2, 3, 3, 2, 1}, 2}),
+		Entry("1", testData{[]int32{1, 2, 3, 4, 5}, 4}),
+		Entry("2", testData{[]int32{1, 2, 3, 2, 1}, 2}),
+		Entry("3", testData{[]int32{1, 2, 4, 3, 2, 1}, 3}),
+	)
 })
