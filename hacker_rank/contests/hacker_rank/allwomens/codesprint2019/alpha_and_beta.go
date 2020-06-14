13992a5822c5b0a83ec22f4978824e982469a340
zrma <bulbitain@gmail.com>
2020-06-14 22:45:52 +0900

    hacker_rank: do refactoring; codesprint2019/alpha_and_beta

--- hacker_rank/contests/hacker_rank/allwomens/codesprint2019/alpha_and_beta.go
+++ hacker_rank/contests/hacker_rank/allwomens/codesprint2019/alpha_and_beta.go
@@ -1,13 +1,13 @@
 package codesprint2019
 
 func alphaBeta(pile []int32) int32 {
-	var best, better int32
-	for _, p := range pile {
-		if p > best {
-			best, better = p, best
-		} else if p < best && p > better {
-			better = p
+	var alpha, beta int32
+	for _, cur := range pile {
+		if cur > alpha {
+			alpha, beta = cur, alpha
+		} else if cur < alpha && cur > beta {
+			beta = cur
 		}
 	}
-	return better
+	return beta
 }
