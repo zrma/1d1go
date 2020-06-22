2becee223a7b52372b2b427d46987f046e40d9bd
zrma <bulbitain@gmail.com>
2020-06-23 01:06:17 +0900

    hacker_rank: do refactoring; algorithms/warmup/simple_array_sum
    change variables name

--- hacker_rank/algorithms/warmup/simple_array_sum.go
+++ hacker_rank/algorithms/warmup/simple_array_sum.go
@@ -1,8 +1,8 @@
 package warmup
 
-func simpleArraySum(ar []int32) int32 {
+func simpleArraySum(arr []int32) int32 {
 	var sum int32
-	for _, n := range ar {
+	for _, n := range arr {
 		sum += n
 	}
 
