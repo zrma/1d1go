1f9e777032503b16c477af3ebb4ac3bbaa50755a
zrma <bulbitain@gmail.com>
2020-10-26 22:10:25 +0900

    add testify.mock example

--- practice/mocking/book/book_test.go
+++ practice/mocking/book/book_test.go
@@ -5,34 +5,72 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 
 	. "github.com/zrma/1d1go/practice/mocking/book"
 	"github.com/zrma/1d1go/practice/mocking/mocks"
 )
 
 func TestPlayWithBook(t *testing.T) {
-	t.Log("gomock 사용 연습")
+	t.Parallel()
 
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	t.Run("gomock 사용 연습", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
 
-	m := mocks.NewMockBook(ctrl)
+		m := mocks.NewMockBook(ctrl)
 
-	m.EXPECT().Read(gomock.Eq("a")).Return(true).AnyTimes()
-	m.EXPECT().Page(gomock.Eq("a")).Return(123).AnyTimes()
+		m.EXPECT().Read(gomock.Eq("a")).Return(true).AnyTimes()
+		m.EXPECT().Page(gomock.Eq("a")).Return(123).AnyTimes()
 
-	m.EXPECT().Read(gomock.Eq("b")).Return(false).AnyTimes()
-	m.EXPECT().Page(gomock.Eq("b")).Return(0).AnyTimes()
+		m.EXPECT().Read(gomock.Eq("b")).Return(false).AnyTimes()
+		m.EXPECT().Page(gomock.Eq("b")).Return(0).AnyTimes()
 
-	t.Run("name a", func(t *testing.T) {
-		cnt, ok := PlayWithBook(m, "a")
-		assert.True(t, ok)
-		assert.Equal(t, cnt, 123)
+		t.Run("name a", func(t *testing.T) {
+			cnt, ok := PlayWithBook(m, "a")
+			assert.True(t, ok)
+			assert.Equal(t, cnt, 123)
+		})
+
+		t.Run("name b", func(t *testing.T) {
+			cnt, ok := PlayWithBook(m, "b")
+			assert.False(t, ok)
+			assert.Equal(t, cnt, 0)
+		})
 	})
 
-	t.Run("name b", func(t *testing.T) {
-		cnt, ok := PlayWithBook(m, "b")
-		assert.False(t, ok)
-		assert.Equal(t, cnt, 0)
+	t.Run("testify.mock 사용 연습", func(t *testing.T) {
+		m := new(myMockBook)
+		m.On("Read", "a").Return(true)
+		m.On("Page", "a").Return(123)
+
+		m.On("Read", "b").Return(false)
+		m.On("Page", "b").Return(0)
+
+		t.Run("name a", func(t *testing.T) {
+			cnt, ok := PlayWithBook(m, "a")
+			assert.True(t, ok)
+			assert.Equal(t, cnt, 123)
+		})
+
+		t.Run("name b", func(t *testing.T) {
+			cnt, ok := PlayWithBook(m, "b")
+			assert.False(t, ok)
+			assert.Equal(t, cnt, 0)
+		})
 	})
+}
+
+type myMockBook struct {
+	mock.Mock
+}
+
+func (m *myMockBook) Page(s string) int {
+	args := m.Called(s)
+	return args.Int(0)
+}
+
+func (m *myMockBook) Read(s string) bool {
+	args := m.Called(s)
+	return args.Bool(0)
 }
