10d572e5db204039c5402a11763bb5b2df46da5d
zrma <bulbitain@gmail.com>
2020-10-31 21:54:16 +0900

    hacker_rank: do refactoring; hacker_rank/algorithms/dp/bricks_game

--- hacker_rank/algorithms/dp/bricks_game.go
+++ hacker_rank/algorithms/dp/bricks_game.go
@@ -4,69 +4,45 @@ import (
 	"github.com/zrma/going/utils/integer"
 )
 
-// 메모이제이션을 위해 캐시를 사용한다.
-// 그러지 않으면 성능 테스트 케이스 타임아웃에 걸린다.
-type brickCache map[int]innerCache
-
-type innerCache map[int]int64
-
-func (cache *brickCache) get(begin, end int) (int64, bool) {
-	if inner, ok := (*cache)[begin]; ok {
-		result, ok := inner[end]
-		return result, ok
-	}
-
-	(*cache)[begin] = make(innerCache)
-	return 0, false
-}
-
-func playGameRecur(arr []int32, begin, end int, cache brickCache) int64 {
-	if result, ok := cache.get(begin, end); ok {
-		return result
-	}
-
-	length := end - begin
-	// 재귀 함수 탈출 조건
+func bricksGame(arr []int32) int64 {
+	length := len(arr)
 	if length <= 3 {
 		var sum int64
-		for i := 0; i <= integer.MinInt(length, 2); i++ {
-			sum += int64(arr[begin+i])
+		for _, n := range arr {
+			sum += int64(n)
 		}
-
-		cache[begin][end] = sum
 		return sum
 	}
 
-	// 나는 최대한 점수를 많이 내려고 한다.
-	result := integer.MaxInt64(
-		// 상대방은 내 점수를 최대한 적게 내도록 한다.
-		integer.MinInt64(
-			int64(arr[begin])+playGameRecur(arr, begin+2, end, cache),
-			int64(arr[begin])+playGameRecur(arr, begin+3, end, cache),
-			int64(arr[begin])+playGameRecur(arr, begin+4, end, cache),
-		),
-		// 상대방은 내 점수를 최대한 적게 내도록 한다.
-		integer.MinInt64(
-			int64(arr[begin])+int64(arr[begin+1])+playGameRecur(arr, begin+3, end, cache),
-			int64(arr[begin])+int64(arr[begin+1])+playGameRecur(arr, begin+4, end, cache),
-			int64(arr[begin])+int64(arr[begin+1])+playGameRecur(arr, begin+5, end, cache),
-		),
-		// 상대방은 내 점수를 최대한 적게 내도록 한다.
-		integer.MinInt64(
-			int64(arr[begin])+int64(arr[begin+1])+int64(arr[begin+2])+playGameRecur(arr, begin+4, end, cache),
-			int64(arr[begin])+int64(arr[begin+1])+int64(arr[begin+2])+playGameRecur(arr, begin+5, end, cache),
-			int64(arr[begin])+int64(arr[begin+1])+int64(arr[begin+2])+playGameRecur(arr, begin+6, end, cache),
-		),
-	)
-	cache[begin][end] = result
-	return result
-}
+	result := make([]int64, length)
+	var total int64
 
-func bricksGameRecur(arr []int32) int64 {
-	cache := make(brickCache)
-	return playGameRecur(arr, 0, len(arr)-1, cache)
-}
+	lastIndex := length - 1
 
-func bricksGameLoop(arr []int32) int64 {
-	panic("implement me")
+	// case 0-2
+	// arr    :  1,   2,   3,   4,   5
+	// total  : 15 ← 14 ← 12  ← 9  ← 5
+	// result :  6,   9,  12,   9,   5
+
+	// case 1
+	// arr    :  999,   1,   1,   1,   0
+	// total  : 1002 ←  3 ←  2  ← 1  ← 0
+	// result : 1001,   2,   2,   1,   0
+
+	// case 3
+	// arr    :    0,     1,     1,     1,   999,  999
+	// total  : 2001 ← 2001 ← 2000 ← 1999 ← 1998 ← 999
+	// result : 1001,  1000,  1001,  1999,  1998,  999
+
+	for i := 0; i < 3; i++ {
+		total += int64(arr[lastIndex-i])
+		result[lastIndex-i] = total
+	}
+
+	for i := 3; i <= lastIndex; i++ {
+		total += int64(arr[lastIndex-i])
+		result[lastIndex-i] = total - integer.MinInt64(result[lastIndex-i+1], result[lastIndex-i+2], result[lastIndex-i+3])
+	}
+
+	return result[0]
 }
