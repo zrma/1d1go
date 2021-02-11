c9cffeeb1fc8ed414435427b8d49c88230b91b1b
zrma <bulbitain@gmail.com>
2021-02-12 00:29:28 +0900

    do refactoring; hacker_rank/implementation/between_two_sets.go
    - reverse condition
    - rename variables

--- hacker_rank/algorithms/implementation/between_two_sets.go
+++ hacker_rank/algorithms/implementation/between_two_sets.go
@@ -4,17 +4,16 @@ import (
 	"sort"
 )
 
-func getTotalX(a []int32, b []int32) int32 {
-	sort.Slice(a, func(i, j int) bool {
-		return a[i] < a[j]
+func getTotalX(arr1, arr2 []int32) int32 {
+	sort.Slice(arr1, func(i, j int) bool {
+		return arr1[i] < arr1[j]
+	})
+	sort.Slice(arr2, func(i, j int) bool {
+		return arr2[i] < arr2[j]
 	})
 
-	sort.Slice(b, func(i, j int) bool {
-		return b[i] < b[j]
-	})
-
-	num1 := a[len(a)-1]
-	num2 := b[0]
+	begin := arr1[len(arr1)-1]
+	end := arr2[0]
 
 	cond1 := func(num int32, arr []int32) bool {
 		for _, n := range arr {
@@ -24,7 +23,6 @@ func getTotalX(a []int32, b []int32) int32 {
 		}
 		return true
 	}
-
 	cond2 := func(num int32, arr []int32) bool {
 		for _, n := range arr {
 			if n%num != 0 {
@@ -35,11 +33,10 @@ func getTotalX(a []int32, b []int32) int32 {
 	}
 
 	var cnt int32
-	for i := num1; i <= num2; i++ {
-		if !cond1(i, a) || !cond2(i, b) {
-			continue
+	for n := begin; n <= end; n++ {
+		if cond1(n, arr1) && cond2(n, arr2) {
+			cnt++
 		}
-		cnt++
 	}
 	return cnt
 }
