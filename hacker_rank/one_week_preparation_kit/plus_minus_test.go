d98633566c82600fecb9b66e1e784340afbfd8de
zrma <bulbitain@gmail.com>
2022-03-18 23:01:18 +0900

    hacker_rank: resolve plusMinus
    - https://www.hackerrank.com/challenges/plus-minus/problem
    - https://www.hackerrank.com/challenges/one-week-preparation-kit-plus-minus/problem

---
+++ hacker_rank/one_week_preparation_kit/plus_minus_test.go
@@ -0,0 +1,25 @@
+package one_week_preparation_kit
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/utils"
+)
+
+func TestPlusMinus(t *testing.T) {
+	t.Log("https://www.hackerrank.com/challenges/plus-minus/problem")
+	t.Log("https://www.hackerrank.com/challenges/one-week-preparation-kit-plus-minus/problem")
+
+	want := []string{
+		"0.500000",
+		"0.333333",
+		"0.166667",
+	}
+	got, err := utils.GetPrinted(func() {
+		plusMinus([]int32{-4, 3, -9, 0, 4, 1})
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, want, got)
+}
