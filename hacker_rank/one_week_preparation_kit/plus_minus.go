0b37a0095e6b56dc2e05da4b406d8c21395fca8c
zrma <bulbitain@gmail.com>
2022-03-18 23:01:18 +0900

    hacker_rank: resolve plusMinus
    - https://www.hackerrank.com/challenges/plus-minus/problem
    - https://www.hackerrank.com/challenges/one-week-preparation-kit-plus-minus/problem

---
+++ hacker_rank/one_week_preparation_kit/plus_minus.go
@@ -0,0 +1,22 @@
+package one_week_preparation_kit
+
+import "fmt"
+
+func plusMinus(arr []int32) {
+	var pos, neg, zero float64
+	// NOTE - time complexity O(n)
+	for _, n := range arr {
+		if n > 0 {
+			pos++
+		} else if n < 0 {
+			neg++
+		} else {
+			zero++
+		}
+	}
+
+	const fmtStr = "%.6f\n"
+	fmt.Printf(fmtStr, pos/float64(len(arr)))
+	fmt.Printf(fmtStr, neg/float64(len(arr)))
+	fmt.Printf(fmtStr, zero/float64(len(arr)))
+}
