365218c84b7c3be8a407ba56cfcfe0ab87cfe986
zrma <bulbitain@gmail.com>
2022-03-18 23:15:38 +0900

    hacker_rank: resolve miniMaxSum
    - https://www.hackerrank.com/challenges/mini-max-sum/problem
    - https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum/problem

---
+++ hacker_rank/one_week_preparation_kit/mini_max_sum_test.go
@@ -0,0 +1,37 @@
+package one_week_preparation_kit
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/utils"
+)
+
+func TestMiniMaxSum(t *testing.T) {
+	t.Log("https://www.hackerrank.com/challenges/mini-max-sum/problem")
+	t.Log("https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum/problem")
+
+	for i, tt := range []struct {
+		given []int32
+		want  []string
+	}{
+		{
+			given: []int32{1, 2, 3, 4, 5},
+			want:  []string{"10 14"},
+		},
+		{
+			given: []int32{1, 3, 5, 7, 9},
+			want:  []string{"16 24"},
+		},
+	} {
+		t.Run(fmt.Sprintf(" %d", i), func(t *testing.T) {
+			got, err := utils.GetPrinted(func() {
+				miniMaxSum(tt.given)
+			})
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
