38a5d0e3e2db718da585a212fc4f871d2fc85c4b
zrma <bulbitain@gmail.com>
2022-03-18 23:15:38 +0900

    hacker_rank: resolve miniMaxSum
    - https://www.hackerrank.com/challenges/mini-max-sum/problem
    - https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum/problem

---
+++ hacker_rank/one_week_preparation_kit/mini_max_sum.go
@@ -0,0 +1,23 @@
+package one_week_preparation_kit
+
+import (
+	"fmt"
+	"math"
+)
+
+func miniMaxSum(arr []int32) {
+	var min int64 = math.MaxInt64
+	var max int64 = math.MinInt64
+	var sum int64 = 0
+	for _, n := range arr {
+		v := int64(n)
+		sum += v
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	fmt.Printf("%d %d", sum-max, sum-min)
+}
