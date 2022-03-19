5dbe3db7dcf5f1c14ac3b9333766a204f2e1e38d
zrma <bulbitain@gmail.com>
2022-03-20 00:12:11 +0900

    hacker_rank: diagonalDifference
    - https://www.hackerrank.com/challenges/diagonal-difference/problem
    - https://www.hackerrank.com/challenges/one-week-preparation-kit-diagonal-difference/problem

---
+++ hacker_rank/one_week_preparation_kit/day2/diagonal_difference.go
@@ -0,0 +1,23 @@
+package day2
+
+func diagonalDifference(arr [][]int32) int32 {
+	if len(arr) == 0 {
+		return 0
+	}
+	var sum0, sum1 int32
+	latIdx := len(arr) - 1
+
+	// NOTE - time complexity is O(n)
+	for i, row := range arr {
+		sum0 += row[i]
+		sum1 += row[latIdx-i]
+	}
+	return abs(sum0 - sum1)
+}
+
+func abs(x int32) int32 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
