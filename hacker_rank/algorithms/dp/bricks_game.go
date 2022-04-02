76cc4b6b36716f7034b61c98d67d3fb24fb042ee
zrma <bulbitain@gmail.com>
2022-04-02 20:02:11 +0900

    hacker_rank: add recursive solution of dp: bricks_game and add benchmark

--- hacker_rank/algorithms/dp/bricks_game.go
+++ hacker_rank/algorithms/dp/bricks_game.go
@@ -41,8 +41,85 @@ func bricksGame(arr []int32) int64 {
 
 	for i := 3; i <= lastIndex; i++ {
 		total += int64(arr[lastIndex-i])
-		result[lastIndex-i] = total - integer.MinInt64(result[lastIndex-i+1], result[lastIndex-i+2], result[lastIndex-i+3])
+
+		// 상대의 몫이 최소가 되도록
+		result[lastIndex-i] = total -
+			integer.MinInt64(
+				result[lastIndex-i+1],
+				result[lastIndex-i+2],
+				result[lastIndex-i+3],
+			)
+	}
+	return result[0]
+}
+
+func bricksGameReversal(arr []int32) int64 {
+	var sum int64
+	for _, n := range arr {
+		sum += int64(n)
+	}
+	return sum - bricksGame(arr)
+}
+
+// 메모이제이션을 위해 캐시를 사용한다.
+// 그러지 않으면 성능 테스트 케이스 타임아웃에 걸린다.
+type brickCache map[int]innerCache
+
+type innerCache map[int]int64
+
+func (cache brickCache) get(begin, end int) (int64, bool) {
+	if inner, ok := cache[begin]; ok {
+		result, ok := inner[end]
+		return result, ok
 	}
 
-	return result[0]
+	cache[begin] = make(innerCache)
+	return 0, false
+}
+
+func playGameRecur(arr []int32, begin, end int, cache brickCache) int64 {
+	if result, ok := cache.get(begin, end); ok {
+		return result
+	}
+
+	length := end - begin
+	// 재귀 함수 탈출 조건
+	if length <= 3 {
+		var sum int64
+		for i := 0; i <= integer.MinInt(length, 2); i++ {
+			sum += int64(arr[begin+i])
+		}
+
+		cache[begin][end] = sum
+		return sum
+	}
+
+	// 나는 최대한 점수를 많이 내려고 한다.
+	result := integer.MaxInt64(
+		// 상대방은 내 점수를 최대한 적게 내도록 한다.
+		integer.MinInt64(
+			int64(arr[begin])+playGameRecur(arr, begin+2, end, cache),
+			int64(arr[begin])+playGameRecur(arr, begin+3, end, cache),
+			int64(arr[begin])+playGameRecur(arr, begin+4, end, cache),
+		),
+		// 상대방은 내 점수를 최대한 적게 내도록 한다.
+		integer.MinInt64(
+			int64(arr[begin])+int64(arr[begin+1])+playGameRecur(arr, begin+3, end, cache),
+			int64(arr[begin])+int64(arr[begin+1])+playGameRecur(arr, begin+4, end, cache),
+			int64(arr[begin])+int64(arr[begin+1])+playGameRecur(arr, begin+5, end, cache),
+		),
+		// 상대방은 내 점수를 최대한 적게 내도록 한다.
+		integer.MinInt64(
+			int64(arr[begin])+int64(arr[begin+1])+int64(arr[begin+2])+playGameRecur(arr, begin+4, end, cache),
+			int64(arr[begin])+int64(arr[begin+1])+int64(arr[begin+2])+playGameRecur(arr, begin+5, end, cache),
+			int64(arr[begin])+int64(arr[begin+1])+int64(arr[begin+2])+playGameRecur(arr, begin+6, end, cache),
+		),
+	)
+	cache[begin][end] = result
+	return result
+}
+
+func bricksGameRecur(arr []int32) int64 {
+	cache := make(brickCache)
+	return playGameRecur(arr, 0, len(arr)-1, cache)
 }
--- hacker_rank/algorithms/dp/bricks_game_test.go
+++ hacker_rank/algorithms/dp/bricks_game_test.go
@@ -51,58 +51,117 @@ func TestBricksGame(t *testing.T) {
 			want:        1001,
 		},
 	} {
-		t.Run(tt.description, func(t *testing.T) {
+		t.Run(tt.description+"/loop", func(t *testing.T) {
 			got := bricksGame(tt.given)
+			assert.Equal(t, tt.want, got)
+		})
+
+		t.Run(tt.description+"/recur", func(t *testing.T) {
+			got := bricksGameRecur(tt.given)
 			assert.Equal(t, tt.want, got)
 		})
 	}
 }
 
-func TestBricksGamePerformance(t *testing.T) {
-	readCSV := func(fileName string) ([][]int32, error) {
-		file, err := os.Open(fileName)
-		if err != nil {
-			return nil, err
-		}
-		defer func() {
-			err := file.Close()
-			assert.NoError(t, err)
-		}()
+func TestBricksGame_Reversal(t *testing.T) {
+	for _, tt := range []struct {
+		description string
+		given       []int32
+		want        int64
+	}{
+		{
+			description: "1~3",
+			given:       []int32{1, 2, 3},
+			want:        0,
+		},
+		{
+			description: "1~4",
+			given:       []int32{1, 2, 3, 4},
+			want:        4,
+		},
+		{
+			description: "1~5",
+			given:       []int32{1, 2, 3, 4, 5},
+			want:        9,
+		},
+		{
+			description: "big number front, zero back",
+			given:       []int32{999, 1, 1, 1, 0},
+			want:        1,
+		},
+		{
+			description: "zero front, big number end",
+			given:       []int32{0, 1, 1, 1, 999},
+			want:        3,
+		},
+		{
+			description: "multiple duplicate values",
+			given:       []int32{0, 1, 1, 1, 999, 999},
+			want:        1000,
+		},
+	} {
+		t.Run(tt.description, func(t *testing.T) {
+			got := bricksGameReversal(tt.given)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
 
-		r := csv.NewReader(bufio.NewReader(file))
-		rows, err := r.ReadAll()
-		if err != nil {
-			return nil, err
-		}
+func readCSV(tb testing.TB, fileName string) ([][]int32, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err := file.Close()
+		assert.NoError(tb, err)
+	}()
 
-		var arr [][]int32
-		for _, row := range rows {
-			var cols []int32
-			for _, col := range row {
-				num, err := strconv.ParseInt(strings.TrimSpace(col), 10, 32)
-				if err != nil {
-					return nil, err
-
-				}
-				cols = append(cols, int32(num))
-			}
-			arr = append(arr, cols)
-		}
-		return arr, nil
+	r := csv.NewReader(bufio.NewReader(file))
+	rows, err := r.ReadAll()
+	if err != nil {
+		return nil, err
 	}
 
-	assert.Eventually(t, func() bool {
-		arr, err := readCSV("./test_data/bricks_game_0.csv")
-		assert.NoError(t, err)
+	var arr [][]int32
+	for _, row := range rows {
+		var cols []int32
+		for _, col := range row {
+			num, err := strconv.ParseInt(strings.TrimSpace(col), 10, 32)
+			if err != nil {
+				return nil, err
 
-		arr2, err := readCSV("./test_data/bricks_game_1.csv")
-		assert.NoError(t, err)
+			}
+			cols = append(cols, int32(num))
+		}
+		arr = append(arr, cols)
+	}
+	return arr, nil
+}
 
-		arr = append(arr, arr2...)
+type fixture struct {
+	given [][]int32
+	want  []struct {
+		len int
+		val int64
+	}
+}
 
-		want := []struct {
-			l int
-			e int64
+func newFixture(tb testing.TB) fixture {
+	arr, err := readCSV(tb, "./test_data/bricks_game_0.csv")
+	assert.NoError(tb, err)
+
+	arr2, err := readCSV(tb, "./test_data/bricks_game_1.csv")
+	assert.NoError(tb, err)
+
+	arr = append(arr, arr2...)
+	assert.Len(tb, arr, 8)
+
+	return fixture{
+		given: arr,
+		want: []struct {
+			len int
+			val int64
 		}{
 			{1000, 249147},
 			{1000, 251633},
@@ -112,16 +171,61 @@ func TestBricksGamePerformance(t *testing.T) {
 			{100000, 249791261588},
 			{100000, 249894676936},
 			{100000, 250224672758},
-		}
-		assert.Len(t, arr, 8)
+		},
+	}
+}
 
-		for i, arr := range arr {
-			assert.Len(t, arr, want[i].l)
+func TestBricksGamePerformance(t *testing.T) {
+	assert.Eventually(t, func() bool {
+		f := newFixture(t)
+
+		for i, arr := range f.given {
+			assert.Len(t, arr, f.want[i].len)
 
 			got := bricksGame(arr)
-			assert.EqualValues(t, want[i].e, got)
+			assert.EqualValues(t, f.want[i].val, got)
 		}
 
 		return true
 	}, time.Second*5, time.Millisecond*100, "시간 초과")
+}
+
+//goos: windows
+//goarch: amd64
+//pkg: 1d1go/hacker_rank/algorithms/dp
+//cpu: AMD Ryzen 7 3700X 8-Core Processor
+//BenchmarkBrickGame
+//BenchmarkBrickGame/Loop
+//BenchmarkBrickGame/Loop-16                  4346            265899 ns/op
+//BenchmarkBrickGame/Recur
+//BenchmarkBrickGame/Recur-16                   38          27910642 ns/op
+//PASS
+func BenchmarkBrickGame(b *testing.B) {
+	b.Run("Loop", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			b.StopTimer()
+			f := newFixture(b)
+			b.StartTimer()
+
+			for pb.Next() {
+				for _, arr := range f.given {
+					bricksGame(arr)
+				}
+			}
+		})
+	})
+
+	b.Run("Recur", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			b.StopTimer()
+			f := newFixture(b)
+			b.StartTimer()
+
+			for pb.Next() {
+				for _, arr := range f.given {
+					bricksGameRecur(arr)
+				}
+			}
+		})
+	})
 }
