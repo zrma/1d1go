c9754f9d692be98628b2f4235496a9658be97710
zrma <bulbitain@gmail.com>
2022-04-24 23:34:11 +0900

    boj: solve 2437 저울
    - https://www.acmicpc.net/problem/2437

---
+++ boj/p2437_test.go
@@ -0,0 +1,35 @@
+package boj
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve2437(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2437")
+
+	for i, tt := range []struct {
+		given []int
+		want  int
+	}{
+		{[]int{3, 1, 6, 2, 7, 30, 1}, 21},
+		{[]int{1, 1, 2, 3, 6, 7, 30}, 21},
+		{[]int{1}, 2},
+		{[]int{2}, 1},
+		{[]int{3}, 1},
+		{[]int{1, 1}, 3},
+		{[]int{1, 2}, 4},
+		{[]int{1, 3}, 2},
+		{[]int{1, 4}, 2},
+		{[]int{2, 4}, 1},
+		{[]int{1, 2, 4}, 8},
+		{[]int{1, 2, 5}, 4},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			got := Solve2437(tt.given)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
