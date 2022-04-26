c6b8cf2d2ce4623ed0c7dffe541e8af351bbec69
zrma <bulbitain@gmail.com>
2022-04-26 21:36:22 +0900

    boj: solve 2557 Hello World
    - https://www.acmicpc.net/problem/2557

---
+++ boj/p2557_test.go
@@ -0,0 +1,20 @@
+package boj
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/utils"
+)
+
+func TestSolve2557(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2557")
+
+	want := []string{"Hello World!"}
+	got, err := utils.GetPrinted(func() {
+		Solve2557()
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, want, got)
+}
