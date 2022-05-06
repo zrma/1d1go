fe3c8f108f3e3399ce51b02be1eaefcfaa53937b
zrma <bulbitain@gmail.com>
2022-05-07 01:14:47 +0900

    boj: solve 18108 1998년생인 내가 태국에서는 2541년생?!
    - https://www.acmicpc.net/problem/18108

---
+++ cmd/boj/p10k/p18100/p18108/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"1d1go/boj/p18k/p18108"
+)
+
+func main() {
+	var year int
+	_, _ = fmt.Scanf("%d", &year)
+
+	res := p18108.Solve18108(year)
+	fmt.Println(res)
+}
