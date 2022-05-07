c892fe160a2afcbfe904451e3abe189f02dccae4
zrma <bulbitain@gmail.com>
2022-05-07 13:20:25 +0900

    boj: solve 9498 시험 성적
    - https://www.acmicpc.net/problem/9498

---
+++ boj/p9k/p9400/p9498.go
@@ -0,0 +1,16 @@
+package p9400
+
+func Solve9498(score int) string {
+	switch {
+	case score >= 90:
+		return "A"
+	case score >= 80:
+		return "B"
+	case score >= 70:
+		return "C"
+	case score >= 60:
+		return "D"
+	default:
+		return "F"
+	}
+}
---
+++ boj/p9k/p9400/p9498_test.go
@@ -0,0 +1,33 @@
+package p9400
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve9498(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/9498")
+
+	for _, tt := range []struct {
+		given int
+		want  string
+	}{
+		{100, "A"},
+		{90, "A"},
+		{89, "B"},
+		{80, "B"},
+		{79, "C"},
+		{70, "C"},
+		{69, "D"},
+		{60, "D"},
+		{59, "F"},
+		{0, "F"},
+	} {
+		t.Run(fmt.Sprintf("%d", tt.given), func(t *testing.T) {
+			got := Solve9498(tt.given)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
