2beaf10b71fe335478cafb42f81978995f6d98dd
zrma <bulbitain@gmail.com>
2022-05-10 08:16:07 +0900

    boj: solve 11021 A+B - 7
    - https://www.acmicpc.net/problem/11021

---
+++ boj/p11k/p11000/p11021_test.go
@@ -0,0 +1,33 @@
+package p11000
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/utils"
+)
+
+func TestSolve11021(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/11021")
+
+	want := []string{
+		"Case #1: 2",
+		"Case #2: 5",
+		"Case #3: 7",
+		"Case #4: 17",
+		"Case #5: 7",
+	}
+	arr2D := [][2]int{
+		{1, 1},
+		{2, 3},
+		{3, 4},
+		{9, 8},
+		{5, 2},
+	}
+	got, err := utils.GetPrinted(func() {
+		Solve11021(arr2D)
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, want, got)
+}
---
+++ cmd/boj/p11k/p11000/p11021/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+
+	"1d1go/boj/p11k/p11000"
+)
+
+func main() {
+	scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+	scanner.Split(bufio.ScanWords)
+
+	_ = scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	arr2D := make([][2]int, n)
+	for i := range arr2D {
+		_ = scanner.Scan()
+		arr2D[i][0], _ = strconv.Atoi(scanner.Text())
+		_ = scanner.Scan()
+		arr2D[i][1], _ = strconv.Atoi(scanner.Text())
+	}
+
+	p11000.Solve11021(arr2D)
+}
