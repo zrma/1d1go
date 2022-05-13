0f4f8f9ddc442f6c6644e5349a72dcb64b8279d0
zrma <bulbitain@gmail.com>
2022-05-14 01:44:50 +0900

    boj/p1k/p1100: do refactoring with I/O using mock

--- cmd/boj/p1k/p1100/p1110/main.go
+++ cmd/boj/p1k/p1100/p1110/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"1d1go/boj/p1k/p1100"
+	"1d1go/utils"
 )
 
 func main() {
-	var n int
-	_, _ = fmt.Scan(&n)
-
-	res := p1100.Solve1110(n)
-	fmt.Println(res)
+	p1100.Solve1110(utils.NewIO())
 }
