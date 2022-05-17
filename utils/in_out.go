b796cd02c9d0039a909defbbe4e9f2b0fffcf994
zrma <bulbitain@gmail.com>
2022-05-18 00:25:19 +0900

    utils: add StringWriter

--- utils/in_out.go
+++ utils/in_out.go
@@ -2,12 +2,6 @@ package utils
 
 //go:generate go run github.com/golang/mock/mockgen -destination=./mocks/mock_writer.go -package=mocks . Writer
 
-type Scanner interface {
-	Scan() bool
-	Text() string
-	Err() error
-}
-
 type Writer interface {
 	Write(p []byte) (n int, err error)
 	Flush() error
---
+++ utils/writer.go
@@ -0,0 +1,25 @@
+package utils
+
+func NewStringWriter() *StringWriter {
+	return &StringWriter{}
+}
+
+type StringWriter struct {
+	buffer []byte
+	result string
+}
+
+func (s *StringWriter) Write(p []byte) (n int, err error) {
+	s.buffer = append(s.buffer, p...)
+	return len(p), nil
+}
+
+func (s *StringWriter) Flush() error {
+	s.result = string(s.buffer)
+	s.buffer = nil
+	return nil
+}
+
+func (s StringWriter) String() string {
+	return s.result
+}
