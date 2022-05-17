11804979457e6735c96de5fece5d2ae98e95212f
zrma <bulbitain@gmail.com>
2022-05-17 22:50:06 +0900

    utils: remove unused interface InOut and implements

--- utils/in_out.go
+++ utils/in_out.go
@@ -1,33 +1,5 @@
 package utils
 
-import (
-	"fmt"
-)
-
-type InOut interface {
-	Scan(a ...any) (n int, err error)
-	Println(a ...any) (n int, err error)
-}
-
-func NewIO() InOut {
-	return &impl{}
-}
-
-type impl struct{}
-
-var (
-	fnIn  = fmt.Scan
-	fnOut = fmt.Println
-)
-
-func (i impl) Scan(args ...any) (n int, err error) {
-	return fnIn(args...)
-}
-
-func (i impl) Println(args ...any) (n int, err error) {
-	return fnOut(args...)
-}
-
 //go:generate go run github.com/golang/mock/mockgen -destination=./mocks/mock_scanner.go -package=mocks . Scanner
 //go:generate go run github.com/golang/mock/mockgen -destination=./mocks/mock_writer.go -package=mocks . Writer
 
