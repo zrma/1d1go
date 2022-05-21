0f60ca7acc47be3315afc237a1cfffa279d964ea
zrma <bulbitain@gmail.com>
2022-05-21 22:28:57 +0900

    boj: solve 5622 다이얼
    - https://www.acmicpc.net/problem/5622

---
+++ boj/p5k/p5600/p5622_test.go
@@ -0,0 +1,50 @@
+package p5600_test
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p5k/p5600"
+	"1d1go/utils"
+)
+
+func TestSolve5622(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/5622")
+
+	//goland:noinspection SpellCheckingInspection
+	for _, tt := range []struct {
+		s    string
+		want string
+	}{
+		{"WA", "13"},
+		{"WA\n", "13"},
+		{"WA\r\n", "13"},
+		{"UNUCIC", "36"},
+		{"C", "3"},
+		{"D", "4"},
+		{"F", "4"},
+		{"G", "5"},
+		{"P", "8"},
+		{"S", "8"},
+		{"T", "9"},
+		{"V", "9"},
+		{"W", "10"},
+		{"Z", "10"},
+	} {
+		t.Run(tt.s, func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.s))
+			writer := utils.NewStringWriter()
+
+			p5600.Solve5622(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
