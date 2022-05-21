1b439fbc531276bf515ad1bb048aba43745a6306
zrma <bulbitain@gmail.com>
2022-05-21 22:04:11 +0900

    boj/p1k: do refactoring with I/O using mock

--- boj/p1k/p1300/p1330_test.go
+++ boj/p1k/p1300/p1330_test.go
@@ -7,21 +7,30 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"1d1go/boj/p1k/p1300"
+	"1d1go/utils"
 )
 
 func TestSolve1330(t *testing.T) {
 	t.Log("https://www.acmicpc.net/problem/1330")
 
 	for i, tt := range []struct {
-		a, b int
+		s    string
 		want string
 	}{
-		{1, 2, "<"},
-		{10, 2, ">"},
-		{5, 5, "=="},
+		{"1 2", "<"},
+		{"10 2", ">"},
+		{"5 5", "=="},
 	} {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			got := p1300.Solve1330(tt.a, tt.b)
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p1300.Solve1330(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
 			assert.Equal(t, tt.want, got)
 		})
 	}
--- boj/p1k/p1400/p1463_test.go
+++ boj/p1k/p1400/p1463_test.go
@@ -1,37 +1,45 @@
 package p1400_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"1d1go/boj/p1k/p1400"
+	"1d1go/utils"
 )
 
 func TestSolve1463(t *testing.T) {
 	t.Log("https://www.acmicpc.net/problem/1463")
 
-	for _, test := range []struct {
-		n    int
-		want int
+	for _, tt := range []struct {
+		s    string
+		want string
 	}{
-		{1, 0},
-		{2, 1},
-		{3, 1},
-		{4, 2},
-		{5, 3},
-		{6, 2},
-		{7, 3},
-		{8, 3},
-		{9, 2},
-		{10, 3},
-		{11, 4},
-		{12, 3},
+		{"1", "0"},
+		{"2", "1"},
+		{"3", "1"},
+		{"4", "2"},
+		{"5", "3"},
+		{"6", "2"},
+		{"7", "3"},
+		{"8", "3"},
+		{"9", "2"},
+		{"10", "3"},
+		{"11", "4"},
+		{"12", "3"},
 	} {
-		t.Run(fmt.Sprintf("%d", test.n), func(t *testing.T) {
-			got := p1400.Solve1463(test.n)
-			assert.Equal(t, test.want, got)
+		t.Run(tt.s, func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p1400.Solve1463(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
