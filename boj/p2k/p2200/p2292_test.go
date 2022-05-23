6b362c1c9f7f99ba5f76d73621285b5e6c818546
zrma <bulbitain@gmail.com>
2022-05-23 22:12:26 +0900

    boj: solve 2292 벌집
    - https://www.acmicpc.net/problem/2292

---
+++ boj/p2k/p2200/p2292_test.go
@@ -0,0 +1,50 @@
+package p2200_test
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p2k/p2200"
+	"1d1go/utils"
+)
+
+func TestSolve2292(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2292")
+
+	for _, tt := range []struct {
+		s    string
+		want string
+	}{
+		{"1", "1"},
+		{"2", "2"},
+		{"7", "2"},
+		{"8", "3"},
+		{"19", "3"},
+		{"20", "4"},
+		{"37", "4"},
+		{"38", "5"},
+		{"61", "5"},
+		{"62", "6"},
+		{"91", "6"},
+		{"92", "7"},
+		{"1000000000", "18258"},
+		{"1234567890", "20287"},
+		{"9876543210", "57379"},
+	} {
+		t.Run(tt.s, func(t *testing.T) {
+			scanner := bufio.NewScanner(strings.NewReader(tt.s))
+			writer := utils.NewStringWriter()
+
+			p2200.Solve2292(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
