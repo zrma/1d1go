a1c810bdad7f7fd88371d353fe9a10eb933e52e9
zrma <bulbitain@gmail.com>
2022-05-23 09:48:09 +0900

    boj: solve 1712 손익분기점
    - https://www.acmicpc.net/problem/1712

---
+++ boj/p1k/p1700/p1712_test.go
@@ -0,0 +1,36 @@
+package p1700_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1700"
+	"1d1go/utils"
+)
+
+func TestSolve1712(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1712")
+
+	for _, tt := range []struct {
+		s    string
+		want string
+	}{
+		{"1000 70 170", "11"},
+		{"3 2 1", "-1"},
+		{"2100000000 9 10", "2100000001"},
+	} {
+		t.Run(tt.s, func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p1700.Solve1712(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
