d3e712134d1194c8128e35cd056cf90cbfada972
zrma <bulbitain@gmail.com>
2022-05-28 23:20:46 +0900

    boj: solve 2581 소수
    - https://www.acmicpc.net/problem/2581

---
+++ boj/p2k/p2500/p2581.go
@@ -0,0 +1,43 @@
+package p2500
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func Solve2581(scanner Scanner, writer Writer) {
+	scanner.Scan()
+	m, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	min := math.MaxInt
+	sum := 0
+	for i := m; i <= n; i++ {
+		if isPrimeNumber(i) {
+			sum += i
+			if i < min {
+				min = i
+			}
+		}
+	}
+	if sum == 0 {
+		_, _ = fmt.Fprint(writer, -1)
+	} else {
+		_, _ = fmt.Fprintln(writer, sum)
+		_, _ = fmt.Fprint(writer, min)
+	}
+}
+
+func isPrimeNumber(n int) bool {
+	if n == 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
