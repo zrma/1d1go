9a674435566c48d7a3bbbcf845e40a455504d80d
zrma <bulbitain@gmail.com>
2022-05-29 21:48:01 +0900

    boj: solve 2447 별 찍기 - 10
    - https://www.acmicpc.net/problem/2447

---
+++ boj/p2k/p2400/p2447.go
@@ -0,0 +1,47 @@
+package p2400
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Solve2447(scanner Scanner, writer Writer) {
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	board := make([][]rune, n)
+	for i := 0; i < n; i++ {
+		board[i] = make([]rune, n)
+		for j := 0; j < n; j++ {
+			board[i][j] = ' '
+		}
+	}
+
+	printStarRecur(board, n, n/3, 0, 0)
+
+	for y := 0; y < n; y++ {
+		for x := 0; x < n; x++ {
+			_, _ = fmt.Fprint(writer, string(board[y][x]))
+		}
+		_, _ = fmt.Fprint(writer, "\n")
+	}
+}
+
+func printStarRecur(board [][]rune, n, k, x, y int) {
+	if k == 1 {
+		board[y][x], board[y][x+1], board[y][x+2] = '*', '*', '*'
+		board[y+1][x], board[y+1][x+2] = '*', '*'
+		board[y+2][x], board[y+2][x+1], board[y+2][x+2] = '*', '*', '*'
+		return
+	}
+	printStarRecur(board, n, k/3, x, y)
+	printStarRecur(board, n, k/3, x+k, y)
+	printStarRecur(board, n, k/3, x+k+k, y)
+
+	printStarRecur(board, n, k/3, x, y+k)
+	printStarRecur(board, n, k/3, x+k+k, y+k)
+
+	printStarRecur(board, n, k/3, x, y+k+k)
+	printStarRecur(board, n, k/3, x+k, y+k+k)
+	printStarRecur(board, n, k/3, x+k+k, y+k+k)
+}
