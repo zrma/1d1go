984c73158bdf27c3c47d7e6c777d9e6cfaaf3eee
zrma <bulbitain@gmail.com>
2022-05-30 22:06:27 +0900

    boj: solve 1018 체스판 다시 칠하기
    - https://www.acmicpc.net/problem/1018

---
+++ boj/p1k/p1000/p1018_test.go
@@ -0,0 +1,127 @@
+package p1000_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1000"
+	"1d1go/utils"
+)
+
+func TestSolve1018(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1018")
+
+	//goland:noinspection SpellCheckingInspection
+	for i, tt := range []struct {
+		s    string
+		want string
+	}{
+		{
+			`8 8
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBBBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBWBWBW`,
+			"1",
+		},
+		{
+			`10 13
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+WWWWWWWWWWBWB
+WWWWWWWWWWBWB`,
+			"12",
+		},
+		{
+			`8 8
+BWBWBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB`,
+			"0",
+		},
+		{
+			`9 23
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBW`,
+			"31",
+		},
+		{
+			`10 10
+BBBBBBBBBB
+BBWBWBWBWB
+BWBWBWBWBB
+BBWBWBWBWB
+BWBWBWBWBB
+BBWBWBWBWB
+BWBWBWBWBB
+BBWBWBWBWB
+BWBWBWBWBB
+BBBBBBBBBB`,
+			"0",
+		},
+		{
+			`8 8
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBBBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWWWB
+BWBWBWBW`,
+			"2",
+		},
+		{
+			`11 12
+BWWBWWBWWBWW
+BWWBWBBWWBWW
+WBWWBWBBWWBW
+BWWBWBBWWBWW
+WBWWBWBBWWBW
+BWWBWBBWWBWW
+WBWWBWBBWWBW
+BWWBWBWWWBWW
+WBWWBWBBWWBW
+BWWBWBBWWBWW
+WBWWBWBBWWBW`,
+			"15",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p1000.Solve1018(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
