8cab3d7a3ee27b813b66b554ccad61e5085e1878
zrma <bulbitain@gmail.com>
2022-05-30 23:03:37 +0900

    boj: solve 2750 수 정렬하기
    - https://www.acmicpc.net/problem/2750

---
+++ boj/p2k/p2700/p2750.go
@@ -0,0 +1,33 @@
+package p2700
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Solve2750(scanner Scanner, writer Writer) {
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	arr := make([]int, n)
+	for i := range arr {
+		scanner.Scan()
+		arr[i], _ = strconv.Atoi(scanner.Text())
+	}
+
+	sort(arr)
+
+	for i := range arr {
+		_, _ = fmt.Fprintln(writer, arr[i])
+	}
+}
+
+func sort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
