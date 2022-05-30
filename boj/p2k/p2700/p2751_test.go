9ea9846f7004362a5140e230f940518c704dda4f
zrma <bulbitain@gmail.com>
2022-05-30 23:07:21 +0900

    boj: solve 2751 수 정렬하기 2
    - https://www.acmicpc.net/problem/2751

---
+++ boj/p2k/p2700/p2751_test.go
@@ -0,0 +1,40 @@
+package p2700_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p2k/p2700"
+	"1d1go/utils"
+)
+
+func TestSolve2751(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2751")
+
+	const (
+		s = `5
+5
+2
+3
+4
+1`
+		want = `1
+2
+3
+4
+5
+`
+	)
+
+	scanner := utils.NewStringScanner(s)
+	writer := utils.NewStringWriter()
+
+	p2700.Solve2751(scanner, writer)
+
+	err := writer.Flush()
+	assert.NoError(t, err)
+
+	got := writer.String()
+	assert.Equal(t, want, got)
+}
