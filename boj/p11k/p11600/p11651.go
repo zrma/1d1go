559e57fbbfde6eeba61ea71abb12d62577ba9fe7
zrma <bulbitain@gmail.com>
2022-05-31 19:05:37 +0900

    boj: solve 11651 좌표 정렬하기 2
    - https://www.acmicpc.net/problem/11651

---
+++ boj/p11k/p11600/p11651.go
@@ -0,0 +1,34 @@
+package p11600
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+func Solve11651(scanner Scanner, writer Writer) {
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	arr := make([][2]int, n)
+	for i := range arr {
+		scanner.Scan()
+		arr[i][0], _ = strconv.Atoi(scanner.Text())
+		scanner.Scan()
+		arr[i][1], _ = strconv.Atoi(scanner.Text())
+	}
+
+	sort.Slice(arr, func(i, j int) bool {
+		if arr[i][1] < arr[j][1] {
+			return true
+		}
+		if arr[i][1] > arr[j][1] {
+			return false
+		}
+		return arr[i][0] < arr[j][0]
+	})
+
+	for _, v := range arr {
+		_, _ = fmt.Fprintln(writer, v[0], v[1])
+	}
+}
