84ce281a97751dea3e89924bcb3ff923522d1dca
zrma <bulbitain@gmail.com>
2022-06-04 22:31:27 +0900

    boj: solve 2981 검문
    - https://www.acmicpc.net/problem/2981

--- boj/p4k/p4600/p4673_셀프_넘버_test.go
+++ boj/p4k/p4600/p4673_셀프_넘버_test.go
@@ -1,6 +1,7 @@
 package p4600_test
 
 import (
+	_ "embed"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,18 +10,18 @@ import (
 	"1d1go/utils"
 )
 
+//go:embed test_data/p4673_want.txt
+var p4673want string
+
 func TestSolve4673(t *testing.T) {
 	t.Log("https://www.acmicpc.net/problem/4673")
-
-	want, err := utils.ReadStringFromFile("./test_data/p4673_want.txt")
-	assert.NoError(t, err)
 
 	writer := utils.NewStringWriter()
 	p4600.Solve4673(writer)
 
-	err = writer.Flush()
+	err := writer.Flush()
 	assert.NoError(t, err)
 
 	got := writer.String()
-	assert.Equal(t, want, got)
+	assert.Equal(t, p4673want, got)
 }
