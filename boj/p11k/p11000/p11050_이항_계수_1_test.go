dea333bb637fb7d594bfa0cef2cf31c578b3e928
zrma <bulbitain@gmail.com>
2022-06-06 19:04:13 +0900

    boj: solve 11050 이항 계수 1
    - https://www.acmicpc.net/problem/11050

---
+++ boj/p11k/p11000/p11050_이항_계수_1_test.go
@@ -0,0 +1,40 @@
+package p11000_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p11k/p11000"
+	"1d1go/utils"
+)
+
+func TestSolve11050(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/11050")
+
+	for _, tt := range []struct {
+		s    string
+		want string
+	}{
+		{"1 0", "1"},
+		{"1 1", "1"},
+		{"1 2", "0"},
+		{"5 2", "10"},
+		{"6 1", "6"},
+		{"6 2", "15"},
+		{"6 3", "20"},
+	} {
+		t.Run(tt.s, func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p11000.Solve11050(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
