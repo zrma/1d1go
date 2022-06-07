3860ccc1c43c77fe5c4bcc07061f605698d24971
zrma <bulbitain@gmail.com>
2022-06-08 00:35:52 +0900

    boj: solve 1676 팩토리얼 0의 개수
    - https://www.acmicpc.net/problem/1676

---
+++ boj/p1k/p1600/p1676_팩토리얼_0의_개수_test.go
@@ -0,0 +1,60 @@
+package p1600_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1600"
+	"1d1go/utils"
+)
+
+func TestSolve1676(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1676")
+
+	for _, tt := range []struct {
+		s    string
+		want string
+	}{
+		{"1", "0"},
+		{"2", "0"},
+		{"3", "0"},
+		{"4", "0"},
+		{"5", "1"},
+		{"6", "1"},
+		{"7", "1"},
+		{"8", "1"},
+		{"9", "1"},
+		{"10", "2"},
+		{"14", "2"},
+		{"15", "3"},
+		{"19", "3"},
+		{"20", "4"},
+		{"24", "4"},
+		{"25", "6"},
+		{"29", "6"},
+		{"30", "7"},
+		{"75", "18"},
+		{"79", "18"},
+		{"80", "19"},
+		{"84", "19"},
+		{"85", "20"},
+		{"124", "28"},
+		{"125", "31"},
+		{"499", "121"},
+		{"500", "124"},
+	} {
+		t.Run(tt.s, func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p1600.Solve1676(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
