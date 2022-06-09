990759b6f915eec0bd0a1eee894f80ab36d90033
zrma <bulbitain@gmail.com>
2022-06-10 00:17:26 +0900

    boj: solve 15652 N과 M (4)
    - https://www.acmicpc.net/problem/15652

---
+++ boj/p15k/p15600/p15652_N과_M_(4)_test.go
@@ -0,0 +1,68 @@
+package p15600_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p15k/p15600"
+	"1d1go/utils"
+)
+
+func TestSolve15652(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/15652")
+
+	for _, tt := range []struct {
+		s    string
+		want string
+	}{
+		{
+			"3 1",
+			`1
+2
+3
+`,
+		},
+		{
+			"4 2",
+			`1 1
+1 2
+1 3
+1 4
+2 2
+2 3
+2 4
+3 3
+3 4
+4 4
+`,
+		},
+		{
+			"3 3",
+			`1 1 1
+1 1 2
+1 1 3
+1 2 2
+1 2 3
+1 3 3
+2 2 2
+2 2 3
+2 3 3
+3 3 3
+`,
+		},
+	} {
+		t.Run(tt.s, func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p15600.Solve15652(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
