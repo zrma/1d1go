f03c1d1f00b958ec306422e95447303d72125365
zrma <bulbitain@gmail.com>
2022-06-14 22:03:25 +0900

    boj: solve 1904 01타일
    - https://www.acmicpc.net/problem/1904

---
+++ boj/p1k/p1900/p1904_01타일_test.go
@@ -0,0 +1,42 @@
+package p1900_test
+
+import (
+	"testing"
+	"time"
+
+	"1d1go/boj/p1k/p1900"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve1904(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1904")
+
+	for _, tt := range []struct {
+		s    string
+		want string
+	}{
+		{"0", "1"},
+		{"1", "1"},
+		{"2", "2"},
+		{"3", "3"},
+		{"4", "5"},
+		{"5", "8"},
+		{"1000000", "7871"},
+	} {
+		t.Run(tt.s, func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			assert.Eventually(t, func() bool {
+				p1900.Solve1904(scanner, writer)
+
+				err := writer.Flush()
+				assert.NoError(t, err)
+
+				got := writer.String()
+				return assert.Equal(t, tt.want, got)
+			}, time.Second, time.Millisecond*100, "시간 초과")
+		})
+	}
+}
