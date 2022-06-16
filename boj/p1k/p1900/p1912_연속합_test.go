d07b3d6a88b9e46760c0e54214a4926c0c7820d9
zrma <bulbitain@gmail.com>
2022-06-16 22:45:24 +0900

    boj: solve 1912 연속합
    - https://www.acmicpc.net/problem/1912

---
+++ boj/p1k/p1900/p1912_연속합_test.go
@@ -0,0 +1,48 @@
+package p1900_test
+
+import (
+	"fmt"
+	"testing"
+
+	"1d1go/boj/p1k/p1900"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve1912(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1912")
+
+	for i, tt := range []struct {
+		s    string
+		want string
+	}{
+		{
+			`10
+10 -4 3 1 5 6 -35 12 21 -1`,
+			"33",
+		},
+		{
+			`10
+2 1 -4 3 4 -4 6 5 -5 1`,
+			"14",
+		},
+		{
+			`5
+-1 -2 -3 -4 -5`,
+			"-1",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p1900.Solve1912(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
