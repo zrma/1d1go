f6dff47f0b3a49625da84b56b4f614a90110f4a0
zrma <bulbitain@gmail.com>
2022-06-17 22:27:30 +0900

    boj: solve 1149 RGB거리
    - https://www.acmicpc.net/problem/1149

---
+++ boj/p1k/p1100/p1149_RGB거리.go
@@ -0,0 +1,32 @@
+package p1100
+
+import (
+	"fmt"
+	"strconv"
+
+	"1d1go/utils/integer"
+)
+
+func Solve1149(scanner Scanner, writer Writer) {
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	const colorNum = 3
+	arr := make([][colorNum]int, n)
+	for i := range arr {
+		for j := 0; j < colorNum; j++ {
+			scanner.Scan()
+			arr[i][j], _ = strconv.Atoi(scanner.Text())
+		}
+	}
+
+	dp := make([][colorNum]int, n)
+	dp[0] = arr[0]
+
+	for i := 1; i < n; i++ {
+		for j := 0; j < colorNum; j++ {
+			dp[i][j] = integer.MinInt(dp[i-1][(j+1)%colorNum], dp[i-1][(j+2)%colorNum]) + arr[i][j]
+		}
+	}
+	_, _ = fmt.Fprint(writer, integer.MinInt(dp[n-1][0], dp[n-1][1], dp[n-1][2]))
+}
---
+++ boj/p1k/p1100/p1149_RGB거리_test.go
@@ -0,0 +1,75 @@
+package p1100_test
+
+import (
+	"testing"
+
+	"1d1go/boj/p1k/p1100"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve1149(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1149")
+
+	for _, tt := range []struct {
+		s    string
+		want string
+	}{
+		{
+			`3
+26 40 83
+49 60 57
+13 89 99`,
+			"96",
+		},
+		{
+			`3
+1 100 100
+100 1 100
+100 100 1`,
+			"3",
+		},
+		{
+			`3
+1 100 100
+100 100 100
+1 100 100`,
+			"102",
+		},
+		{
+			`6
+30 19 5
+64 77 64
+15 19 97
+4 71 57
+90 86 84
+93 32 91`,
+			"208",
+		},
+		{
+			`8
+71 39 44
+32 83 55
+51 37 63
+89 29 100
+83 58 11
+65 13 15
+47 25 29
+60 66 19`,
+			"253",
+		},
+	} {
+		t.Run(tt.s, func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p1100.Solve1149(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
