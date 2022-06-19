412baa45dff98ddb071f80cd38a89238aacc7c6c
zrma <bulbitain@gmail.com>
2022-06-19 21:39:53 +0900

    boj: solve 2579 계단 오르기
    - https://www.acmicpc.net/problem/2579

---
+++ boj/p2k/p2500/p2579_계단_오르기_test.go
@@ -0,0 +1,69 @@
+package p2500_test
+
+import (
+	"fmt"
+	"testing"
+
+	"1d1go/boj/p2k/p2500"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve2579(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2579")
+
+	for i, tt := range []struct {
+		s    string
+		want string
+	}{
+		{
+			`6
+10
+20
+15
+25
+10
+20`,
+			"75",
+		},
+		{
+			`1
+10`,
+			"10",
+		},
+		{
+			`2
+5
+10`,
+			"15",
+		},
+		{
+			`3
+10
+20
+30`,
+			"50",
+		},
+		{
+			`4
+10
+20
+30
+40`,
+			"80",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p2500.Solve2579(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
