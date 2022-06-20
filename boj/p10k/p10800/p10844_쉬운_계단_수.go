e99750161af3c6e39d9349d148c29484eaf6f86b
zrma <bulbitain@gmail.com>
2022-06-20 22:51:44 +0900

    boj: solve 10844 쉬운 계단 수
    - https://www.acmicpc.net/problem/10844

---
+++ boj/p10k/p10800/p10844_쉬운_계단_수.go
@@ -0,0 +1,43 @@
+package p10800
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Solve10844(scanner Scanner, writer Writer) {
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	const base = 10
+	if n == 1 {
+		_, _ = fmt.Fprint(writer, base-1) // exclude 0
+		return
+	}
+
+	dp := make([][base]int64, n)
+	for i := 1; i < base; i++ {
+		dp[0][i] = 1
+	}
+
+	const mod = 1_000_000_000
+	for i := 1; i < n; i++ {
+		for j := 0; j < base; j++ {
+			switch j {
+			case 0:
+				dp[i][j] = dp[i-1][1] % mod
+			case base - 1:
+				dp[i][j] = dp[i-1][base-2] % mod
+			default:
+				dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod
+			}
+		}
+	}
+
+	var res int64
+	for _, v := range dp[n-1] {
+		res = (res + v) % mod
+	}
+
+	_, _ = fmt.Fprint(writer, res)
+}
