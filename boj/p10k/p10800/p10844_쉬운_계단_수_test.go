2393b54648f1d5153f6fa1ca9dda30484b7691bb
zrma <bulbitain@gmail.com>
2022-06-20 22:51:44 +0900

    boj: solve 10844 쉬운 계단 수
    - https://www.acmicpc.net/problem/10844

---
+++ boj/p10k/p10800/p10844_쉬운_계단_수_test.go
@@ -0,0 +1,34 @@
+package p10800_test
+
+import (
+	"testing"
+
+	"1d1go/boj/p10k/p10800"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve10844(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/10844")
+
+	for _, tt := range []struct {
+		s    string
+		want string
+	}{
+		{"1", "9"},
+		{"2", "17"},
+	} {
+		t.Run(tt.s, func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.s)
+			writer := utils.NewStringWriter()
+
+			p10800.Solve10844(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
