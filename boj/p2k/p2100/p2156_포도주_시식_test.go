cd2edb520f35e3548d08cc48fe9ff5c27439795a
zrma <bulbitain@gmail.com>
2022-06-21 21:58:09 +0900

    boj: solve 2156 포도주 시식
    - https://www.acmicpc.net/problem/2156

---
+++ boj/p2k/p2100/p2156_포도주_시식_test.go
@@ -0,0 +1,92 @@
+package p2100_test
+
+import (
+	"fmt"
+	"testing"
+
+	"1d1go/boj/p2k/p2100"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve2156(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2156")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`6
+6
+10
+13
+9
+8
+1`,
+			"33",
+		},
+		{
+			`7
+6
+10
+13
+9
+8
+1
+2`,
+			"35",
+		},
+		{
+			`8
+6
+10
+13
+9
+8
+1
+2
+101`,
+			"136",
+		},
+		{
+			`1
+123`,
+			"123",
+		},
+		{
+			`2
+12
+34`,
+			"46",
+		},
+		{
+			`3
+1
+2
+3`,
+			"5",
+		},
+		{
+			`4
+1
+2
+3
+4`,
+			"8",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.give)
+			writer := utils.NewStringWriter()
+
+			p2100.Solve2156(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
