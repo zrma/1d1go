997e772e5fe2832db286e0c75b949d09f4d4fd22
zrma <bulbitain@gmail.com>
2022-06-22 22:14:39 +0900

    boj: solve 11053 가장 긴 증가하는 부분 수열
    - https://www.acmicpc.net/problem/11053

---
+++ boj/p11k/p11000/p11053_가장_긴_증가하는_부분_수열_test.go
@@ -0,0 +1,58 @@
+package p11000_test
+
+import (
+	"fmt"
+	"testing"
+
+	"1d1go/boj/p11k/p11000"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve11053(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/11053")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`1
+1`,
+			"1",
+		},
+		{
+			`6
+10 20 10 30 20 50`,
+			"4",
+		},
+		{
+			`10
+1 2 3 4 5 6 7 8 9 10`,
+			"10",
+		},
+		{
+			`10
+1 5 3 4 5 6 7 4 9 10`,
+			"8",
+		},
+		{
+			`10
+1 2 3 1 2 3 4 1 2 3`,
+			"4",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.give)
+			writer := utils.NewStringWriter()
+
+			p11000.Solve11053(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
