72ee4bf67166fc235e60b163e27888f62f8267c1
zrma <bulbitain@gmail.com>
2022-06-25 01:03:40 +0900

    utils/integer: add longest integer subsequence time complexity log n

--- utils/integer/longest_increase_subsequence.go
+++ utils/integer/longest_increase_subsequence.go
@@ -1,10 +1,12 @@
 package integer
 
 import (
+	"sort"
+
 	"golang.org/x/exp/constraints"
 )
 
-func LongestIncSubSeqLen[T constraints.Integer](arr []T) T {
+func LongestIncSubSeqLenSquare[T constraints.Integer](arr []T) T {
 	if len(arr) == 0 {
 		var zero T
 		return zero
@@ -15,13 +17,37 @@ func LongestIncSubSeqLen[T constraints.Integer](arr []T) T {
 		dp[i] = 1
 	}
 
-	for i := 1; i < len(arr); i++ {
-		for j := 0; j < i; j++ {
-			if arr[i] > arr[j] && dp[i] < dp[j]+1 {
-				dp[i] = dp[j] + 1
+	for j := 1; j < len(arr); j++ {
+		for i := 0; i < j; i++ {
+			if arr[i] < arr[j] {
+				dp[j] = Max(dp[i]+1, dp[j])
 			}
 		}
 	}
 
 	return Max(dp...)
+}
+
+func LongestIncSubSeqLenLog[T constraints.Integer](arr []T) T {
+	if len(arr) == 0 {
+		var zero T
+		return zero
+	}
+
+	dp := make([]T, len(arr))
+	dp[0] = arr[0]
+
+	i := 0
+	for j := 1; j < len(arr); j++ {
+		if dp[i] < arr[j] {
+			dp[i+1] = arr[j]
+			i++
+		} else {
+			idx := sort.Search(i, func(k int) bool {
+				return arr[j] <= arr[k]
+			})
+			dp[idx] = arr[j]
+		}
+	}
+	return T(i + 1)
 }
