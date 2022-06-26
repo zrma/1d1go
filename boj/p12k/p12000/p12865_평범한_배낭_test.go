7634b9596ca4d36de940ba528740a7a218530546
zrma <bulbitain@gmail.com>
2022-06-26 21:39:20 +0900

    boj: solve 12865 평범한 배낭
    - https://www.acmicpc.net/problem/12865

---
+++ boj/p12k/p12000/p12865_평범한_배낭_test.go
@@ -0,0 +1,41 @@
+package p12000_test
+
+import (
+	"fmt"
+	"testing"
+
+	"1d1go/boj/p12k/p12000"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve12865(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/12865")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`4 7
+6 13
+4 8
+3 6
+5 12`,
+			"14",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			scanner := utils.NewStringScanner(tt.give)
+			writer := utils.NewStringWriter()
+
+			p12000.Solve12865(scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
