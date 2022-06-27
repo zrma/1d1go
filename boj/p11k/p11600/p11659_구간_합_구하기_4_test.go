363db7a5274294a071548c5bb7a772dbc13e5b29
zrma <bulbitain@gmail.com>
2022-06-27 22:03:08 +0900

    boj: solve 11659 구간 합 구하기 4
    - https://www.acmicpc.net/problem/11659

---
+++ boj/p11k/p11600/p11659_구간_합_구하기_4_test.go
@@ -0,0 +1,36 @@
+package p11600_test
+
+import (
+	"testing"
+
+	"1d1go/boj/p11k/p11600"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve11659(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/11659")
+
+	const (
+		give = `5 3
+5 4 3 2 1
+1 3
+2 4
+5 5`
+		want = `12
+9
+1
+`
+	)
+
+	scanner := utils.NewStringScanner(give)
+	writer := utils.NewStringWriter()
+
+	p11600.Solve11659(scanner, writer)
+
+	err := writer.Flush()
+	assert.NoError(t, err)
+
+	got := writer.String()
+	assert.Equal(t, want, got)
+}
