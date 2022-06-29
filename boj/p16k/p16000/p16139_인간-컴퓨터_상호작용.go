9210d970fd129cc806d7003fff9e56c85dbd3a77
zrma <bulbitain@gmail.com>
2022-06-29 22:15:39 +0900

    boj: solve 16139 인간-컴퓨터 상호작용
    - https://www.acmicpc.net/problem/16139

---
+++ boj/p16k/p16000/p16139_인간-컴퓨터_상호작용.go
@@ -0,0 +1,34 @@
+package p16000
+
+import (
+	"fmt"
+)
+
+func Solve16139(reader FastReader, writer Writer) {
+	var s string
+	var n int
+	_, _ = fmt.Fscan(reader, &s, &n)
+
+	const maxLen = 200_000 + 1
+	arr := make([][maxLen]int, 26)
+
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < 26; j++ {
+			arr[j][i+1] = arr[j][i]
+
+			if int(s[i]-'a') == j {
+				arr[j][i+1]++
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		var s string
+		var begin, end int
+		_, _ = fmt.Fscan(reader, &s, &begin, &end)
+		char := int(s[0] - 'a')
+
+		res := arr[char][end+1] - arr[char][begin]
+		_, _ = fmt.Fprintln(writer, res)
+	}
+}
