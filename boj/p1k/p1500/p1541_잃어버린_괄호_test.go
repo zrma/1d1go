2ab0cca4657b02de91bf695343f98d5a9964bd0a
zrma <bulbitain@gmail.com>
2022-07-05 22:40:03 +0900

    boj: solve 1541 잃어버린 괄호
    - https://www.acmicpc.net/problem/1541

---
+++ boj/p1k/p1500/p1541_잃어버린_괄호_test.go
@@ -0,0 +1,39 @@
+package p1500_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"1d1go/boj/p1k/p1500"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve1541(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1541")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{"55-50+40", "-35"},
+		{"10+20+30+40", "100"},
+		{"00009-00009", "0"},
+		{"10-0-0+1", "9"},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1500.Solve1541(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
