4d15eacdb5cc3f35f35d2a96f5113855ea772f15
zrma <bulbitain@gmail.com>
2022-07-07 21:43:29 +0900

    boj: solve 10828 스택
    - https://www.acmicpc.net/problem/10828

---
+++ boj/p10k/p10800/p10828_스택.go
@@ -0,0 +1,74 @@
+package p10800
+
+import (
+	"fmt"
+)
+
+func Solve10828(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	stack := newStack(n)
+	for i := 0; i < n; i++ {
+		var s string
+		_, _ = fmt.Fscan(reader, &s)
+
+		switch s {
+		case "push":
+			var v int
+			_, _ = fmt.Fscan(reader, &v)
+			stack.push(v)
+		case "pop":
+			_, _ = fmt.Fprintln(writer, stack.pop())
+		case "size":
+			_, _ = fmt.Fprintln(writer, stack.size())
+		case "empty":
+			if stack.empty() {
+				_, _ = fmt.Fprintln(writer, 1)
+			} else {
+				_, _ = fmt.Fprintln(writer, 0)
+			}
+		case "top":
+			_, _ = fmt.Fprintln(writer, stack.top())
+		}
+	}
+}
+
+type stack struct {
+	data []int
+}
+
+func newStack(size int) *stack {
+	return &stack{data: make([]int, 0, size)}
+}
+
+func (s *stack) push(v int) {
+	s.data = append(s.data, v)
+}
+
+func (s *stack) pop() int {
+	if s.empty() {
+		return -1
+	}
+
+	lastIdx := s.size() - 1
+	v := s.data[lastIdx]
+	s.data = s.data[:lastIdx]
+	return v
+}
+
+func (s stack) size() int {
+	return len(s.data)
+}
+
+func (s stack) empty() bool {
+	return s.size() == 0
+}
+
+func (s stack) top() int {
+	if s.empty() {
+		return -1
+	}
+	lastIdx := s.size() - 1
+	return s.data[lastIdx]
+}
