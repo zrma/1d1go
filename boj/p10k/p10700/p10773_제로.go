fd35f6018c91a56c5925ead697ac81607e5f096b
zrma <bulbitain@gmail.com>
2022-07-08 22:42:48 +0900

    boj: solve 10773 제로
    - https://www.acmicpc.net/problem/10773

---
+++ boj/p10k/p10700/p10773_제로.go
@@ -0,0 +1,27 @@
+package p10700
+
+import (
+	"fmt"
+)
+
+func Solve10773(reader Reader, writer Writer) {
+	var k int
+	_, _ = fmt.Fscan(reader, &k)
+
+	arr := make([]int, 0, k)
+	for i := 0; i < k; i++ {
+		var n int
+		_, _ = fmt.Fscan(reader, &n)
+		if n == 0 {
+			arr = arr[:len(arr)-1]
+			continue
+		}
+		arr = append(arr, n)
+	}
+
+	sum := 0
+	for _, n := range arr {
+		sum += n
+	}
+	_, _ = fmt.Fprint(writer, sum)
+}
