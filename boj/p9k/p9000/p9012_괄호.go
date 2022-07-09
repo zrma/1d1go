f268bb23b475cf9556aca59b4edd14a884a91d5e
zrma <bulbitain@gmail.com>
2022-07-09 17:51:04 +0900

    boj: solve 9012 괄호
    - https://www.acmicpc.net/problem/9012

---
+++ boj/p9k/p9000/p9012_괄호.go
@@ -0,0 +1,36 @@
+package p9000
+
+import (
+	"fmt"
+)
+
+func Solve9012(reader Reader, writer Writer) {
+	var t int
+	_, _ = fmt.Fscan(reader, &t)
+
+	for i := 0; i < t; i++ {
+		var s string
+		_, _ = fmt.Fscan(reader, &s)
+
+		if checkValidParenthesisString(s) {
+			_, _ = fmt.Fprintln(writer, "YES")
+		} else {
+			_, _ = fmt.Fprintln(writer, "NO")
+		}
+	}
+}
+
+func checkValidParenthesisString(s string) bool {
+	stack := make([]rune, 0, len(s))
+	for _, c := range s {
+		if c == '(' {
+			stack = append(stack, c)
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
