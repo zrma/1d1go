2a51443912569cc65c0ac31fe1273036b911ab86
zrma <bulbitain@gmail.com>
2022-07-09 17:51:04 +0900

    boj: solve 9012 괄호
    - https://www.acmicpc.net/problem/9012

---
+++ boj/p9k/p9000/p9012_괄호_test.go
@@ -0,0 +1,61 @@
+package p9000_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"1d1go/boj/p9k/p9000"
+	"1d1go/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve9012(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/9012")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`6
+(())())
+(((()())()
+(()())((()))
+((()()(()))(((())))()
+()()()()(()()())()
+(()((())()(`,
+			`NO
+NO
+YES
+NO
+YES
+NO
+`,
+		},
+		{
+			`3
+((
+))
+())(()`,
+			`NO
+NO
+NO
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p9000.Solve9012(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
