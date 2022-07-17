8cd9046fa3acf65738e77ff410362413cea9f11c
zrma <bulbitain@gmail.com>
2022-07-17 12:40:40 +0900

    boj: solve 10866 덱
    - https://www.acmicpc.net/problem/10866

---
+++ boj/p10k/p10800/p10866_덱.go
@@ -0,0 +1,101 @@
+package p10800
+
+import (
+	"fmt"
+)
+
+func Solve10866(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	deq := newQueue(n)
+
+	for i := 0; i < n; i++ {
+		var s string
+		_, _ = fmt.Fscan(reader, &s)
+		switch s {
+		case "push_front":
+			var v int
+			_, _ = fmt.Fscan(reader, &v)
+			deq.pushFront(v)
+		case "push_back":
+			var v int
+			_, _ = fmt.Fscan(reader, &v)
+			deq.pushBack(v)
+		case "pop_front":
+			_, _ = fmt.Fprintln(writer, deq.popFront())
+		case "pop_back":
+			_, _ = fmt.Fprintln(writer, deq.popBack())
+		case "size":
+			_, _ = fmt.Fprintln(writer, deq.size())
+		case "empty":
+			if deq.empty() {
+				_, _ = fmt.Fprintln(writer, 1)
+			} else {
+				_, _ = fmt.Fprintln(writer, 0)
+			}
+		case "front":
+			_, _ = fmt.Fprintln(writer, deq.front())
+		case "back":
+			_, _ = fmt.Fprintln(writer, deq.back())
+		}
+	}
+}
+
+func newQueue(n int) *deque {
+	return &deque{
+		data: make([]int, 0, n),
+	}
+}
+
+type deque struct {
+	data []int
+}
+
+func (d *deque) pushFront(v int) {
+	d.data = append([]int{v}, d.data...)
+}
+
+func (d *deque) pushBack(v int) {
+	d.data = append(d.data, v)
+}
+
+func (d *deque) popFront() int {
+	if d.empty() {
+		return -1
+	}
+	v := d.data[0]
+	d.data = d.data[1:]
+	return v
+}
+
+func (d *deque) popBack() int {
+	if d.empty() {
+		return -1
+	}
+	v := d.data[len(d.data)-1]
+	d.data = d.data[:len(d.data)-1]
+	return v
+}
+
+func (d deque) size() int {
+	return len(d.data)
+}
+
+func (d deque) empty() bool {
+	return d.size() == 0
+}
+
+func (d deque) front() int {
+	if d.empty() {
+		return -1
+	}
+	return d.data[0]
+}
+
+func (d deque) back() int {
+	if d.empty() {
+		return -1
+	}
+	return d.data[len(d.data)-1]
+}
---
+++ boj/p10k/p10800/p10866_덱_test.go
@@ -0,0 +1,109 @@
+package p10800_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p10k/p10800"
+	"1d1go/utils"
+)
+
+func TestSolve10866(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/10866")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			give: `15
+push_back 1
+push_front 2
+front
+back
+size
+empty
+pop_front
+pop_back
+pop_front
+size
+empty
+pop_back
+push_front 3
+empty
+front`,
+			want: `2
+1
+2
+0
+2
+1
+-1
+0
+1
+-1
+0
+3
+`,
+		},
+		{
+			`22
+front
+back
+pop_front
+pop_back
+push_front 1
+front
+pop_back
+push_back 2
+back
+pop_front
+push_front 10
+push_front 333
+front
+back
+pop_back
+pop_back
+push_back 20
+push_back 1234
+front
+back
+pop_back
+pop_back`,
+			`-1
+-1
+-1
+-1
+1
+1
+2
+2
+333
+10
+10
+333
+20
+1234
+1234
+20
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p10800.Solve10866(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
