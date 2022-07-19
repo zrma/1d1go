37215e2cc3220560be14edc23eadc3d0caaec97f
zrma <bulbitain@gmail.com>
2022-07-19 22:30:03 +0900

    boj: solve 5430 AC
    - https://www.acmicpc.net/problem/5430

---
+++ boj/p5k/p5400/p5430_AC.go
@@ -0,0 +1,61 @@
+package p5400
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Solve5430(reader Reader, writer Writer) {
+	var t int
+	_, _ = fmt.Fscan(reader, &t)
+
+	for i := 0; i < t; i++ {
+		processAC(reader, writer)
+	}
+}
+
+func processAC(reader Reader, writer Writer) {
+	var code string
+	_, _ = fmt.Fscan(reader, &code)
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+	var input string
+	_, _ = fmt.Fscan(reader, &input)
+	input = input[1 : len(input)-1]
+
+	ss := strings.Split(input, ",")
+	arr := make([]string, 0, len(ss))
+	for _, s := range ss {
+		if s != "" {
+			arr = append(arr, s)
+		}
+	}
+
+	reverse := false
+	for _, c := range code {
+		switch c {
+		case 'R':
+			reverse = !reverse
+		case 'D':
+			if len(arr) == 0 {
+				_, _ = fmt.Fprintln(writer, "error")
+				return
+			}
+			if reverse {
+				arr = arr[:len(arr)-1]
+			} else {
+				arr = arr[1:]
+			}
+		}
+	}
+	if reverse {
+		reverseString(arr)
+	}
+	_, _ = fmt.Fprintf(writer, "[%s]\n", strings.Join(arr, ","))
+}
+
+func reverseString(arr []string) {
+	for i, j := 0, len(arr)-1; i < len(arr)/2; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+}
---
+++ boj/p5k/p5400/p5430_AC_test.go
@@ -0,0 +1,68 @@
+package p5400_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p5k/p5400"
+	"1d1go/utils"
+)
+
+func TestSolve5430(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/5430")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`7
+RDD
+4
+[1,2,3,4]
+DD
+1
+[42]
+RRD
+6
+[1,1,2,3,5,8]
+D
+0
+[]
+D
+1
+[1]
+D
+1
+[123]
+RDDRDDR
+6
+[123,123,123,456,456,456]`,
+			`[2,1]
+error
+[1,2,3,5,8]
+error
+[]
+[]
+[456,123]
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p5400.Solve5430(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
