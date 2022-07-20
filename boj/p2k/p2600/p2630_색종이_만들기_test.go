963007bf023f8585031805013032af91a713bc12
zrma <bulbitain@gmail.com>
2022-07-20 22:04:41 +0900

    boj: solve 2630 색종이 만들기
    - https://www.acmicpc.net/problem/2630

---
+++ boj/p2k/p2600/p2630_색종이_만들기_test.go
@@ -0,0 +1,49 @@
+package p2600_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p2k/p2600"
+	"1d1go/utils"
+)
+
+func TestSolve2630(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2630")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`8
+1 1 0 0 0 0 1 1
+1 1 0 0 0 0 1 1
+0 0 0 0 1 1 0 0
+0 0 0 0 1 1 0 0
+1 0 0 0 1 1 1 1
+0 1 0 0 1 1 1 1
+0 0 1 1 1 1 1 1
+0 0 1 1 1 1 1 1`,
+			`9
+7`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p2600.Solve2630(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
