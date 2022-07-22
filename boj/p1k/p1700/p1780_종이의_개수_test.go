5a1a4283f936b27ce18583f95a367a49cc6e5c68
zrma <bulbitain@gmail.com>
2022-07-22 22:44:54 +0900

    boj: solve 1780 종이의 개수
    - https://www.acmicpc.net/problem/1780

---
+++ boj/p1k/p1700/p1780_종이의_개수_test.go
@@ -0,0 +1,114 @@
+package p1700_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1700"
+	"1d1go/utils"
+)
+
+func TestSolve1780(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1780")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`9
+0 0 0 1 1 1 -1 -1 -1
+0 0 0 1 1 1 -1 -1 -1
+0 0 0 1 1 1 -1 -1 -1
+1 1 1 0 0 0 0 0 0
+1 1 1 0 0 0 0 0 0
+1 1 1 0 0 0 0 0 0
+0 1 -1 0 1 -1 0 1 -1
+0 -1 1 0 1 -1 0 1 -1
+0 1 -1 1 0 -1 0 1 -1`,
+			`10
+12
+11
+`,
+		},
+		{
+			`3
+-1 0 1
+-1 0 1
+-1 0 1`,
+			`3
+3
+3
+`,
+		},
+		{
+			`3
+1 1 1
+1 1 1
+1 1 1`,
+			`0
+0
+1
+`,
+		},
+		{
+			`9
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1`,
+			`0
+0
+1
+`,
+		},
+		{
+			`3
+0 0 0
+0 0 0
+0 0 1`,
+			`0
+8
+1
+`,
+		},
+		{
+			`9
+1 1 1 1 1 1 1 1 1
+1 1 1 0 0 0 1 1 1
+1 1 1 0 0 0 1 1 1
+-1 -1 -1 -1 -1 -1 -1 -1 -1
+-1 -1 -1 -1 -1 -1 -1 -1 -1
+-1 -1 -1 -1 -1 -1 -1 -1 -1
+0 0 0 1 1 1 1 1 1
+0 0 0 0 0 0 1 1 1
+0 0 0 -1 -1 -1 1 1 1`,
+			`6
+10
+9
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1700.Solve1780(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
