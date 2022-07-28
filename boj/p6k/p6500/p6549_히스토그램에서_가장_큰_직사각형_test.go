32ecc746598cb88da76ecd32ffb573ca5e584be4
zrma <bulbitain@gmail.com>
2022-07-28 22:37:47 +0900

    boj: solve 6549 히스토그램에서 가장 큰 직사각형
    - https://www.acmicpc.net/problem/6549

---
+++ boj/p6k/p6500/p6549_히스토그램에서_가장_큰_직사각형_test.go
@@ -0,0 +1,52 @@
+package p6500_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p6k/p6500"
+	"1d1go/utils"
+)
+
+func TestSolve6549(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/6549")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`7 2 1 4 5 1 3 3
+4 1000 1000 1000 1000
+12 3 2 3 4 5 4 3 2 3 1 1 1
+12 28 17 0 7 29 18 16 12 3 30 29 26
+2 10 7
+4 8 9 10 1
+0`,
+			`8
+4000
+18
+78
+14
+24
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p6500.Solve6549(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
