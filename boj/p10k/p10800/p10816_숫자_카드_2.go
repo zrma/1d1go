c8a3cc9d41b6e52f9da0ee3037ca8361541e41a7
zrma <bulbitain@gmail.com>
2022-07-29 23:04:05 +0900

    boj: solve p10816 숫자 카드 2; using binary search
    - https://www.acmicpc.net/problem/10816

--- boj/p10k/p10800/p10816_숫자_카드_2.go
+++ boj/p10k/p10800/p10816_숫자_카드_2.go
@@ -2,9 +2,40 @@ package p10800
 
 import (
 	"fmt"
+	"sort"
 )
 
 func Solve10816(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	arr := make([]int, n)
+	for i := range arr {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+	sort.Ints(arr)
+
+	var m int
+	_, _ = fmt.Fscan(reader, &m)
+
+	for i := 0; i < m; i++ {
+		var v int
+		_, _ = fmt.Fscan(reader, &v)
+
+		next := v + 1
+
+		idx := sort.SearchInts(arr, v)
+		if idx >= len(arr) || arr[idx] != v {
+			_, _ = fmt.Fprint(writer, 0, " ")
+			continue
+		}
+
+		nextIdx := sort.SearchInts(arr, next)
+		_, _ = fmt.Fprint(writer, nextIdx-idx, " ")
+	}
+}
+
+func Solve10816WithMap(reader Reader, writer Writer) {
 	var n int
 	_, _ = fmt.Fscan(reader, &n)
 
