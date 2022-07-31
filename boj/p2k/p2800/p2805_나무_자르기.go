51efb34bb739b08b34305677ca631d63112371fe
zrma <bulbitain@gmail.com>
2022-07-31 15:55:12 +0900

    boj: solve 2805 나무 자르기
    - https://www.acmicpc.net/problem/2805

---
+++ boj/p2k/p2800/p2805_나무_자르기.go
@@ -0,0 +1,42 @@
+package p2800
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve2805(reader Reader, writer Writer) {
+	var n, m int64
+	_, _ = fmt.Fscan(reader, &n, &m)
+
+	arr := make([]int, n)
+	for i := range arr {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+	sort.Ints(arr)
+
+	var lo int64 = 0
+	var hi = int64(arr[len(arr)-1])
+	var res int64
+
+	for lo <= hi {
+		mid := (lo + hi) / 2
+
+		var tot int64
+		for _, v := range arr {
+			cut := int64(v) - mid
+			if cut < 0 {
+				cut = 0
+			}
+			tot += cut
+		}
+		if tot < m {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+			res = mid
+		}
+	}
+
+	_, _ = fmt.Fprint(writer, res)
+}
---
+++ boj/p2k/p2800/p2805_나무_자르기_test.go
@@ -0,0 +1,46 @@
+package p2800_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p2k/p2800"
+	"1d1go/utils"
+)
+
+func TestSolve2805(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2805")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`4 7
+20 15 10 17`,
+			"15",
+		},
+		{
+			`5 20
+4 42 40 26 46`,
+			"36",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p2800.Solve2805(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
