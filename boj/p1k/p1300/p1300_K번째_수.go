5a3b2f11c3c5910d83a6d2ecf5aeb92e3309d0f8
zrma <bulbitain@gmail.com>
2022-08-02 21:01:25 +0900

    boj: solve 1300 K번째 수
    - https://www.acmicpc.net/problem/1300

---
+++ boj/p1k/p1300/p1300_K번째_수.go
@@ -0,0 +1,35 @@
+package p1300
+
+import (
+	"fmt"
+)
+
+func Solve1300(reader Reader, writer Writer) {
+	var n, k int64
+	_, _ = fmt.Fscan(reader, &n, &k)
+
+	lo := int64(1)
+	hi := n * n
+	res := int64(0)
+
+	for lo <= hi {
+		mid := (lo + hi) / 2
+
+		cnt := int64(0)
+		for i := int64(1); i <= n; i++ {
+			v := mid / i
+			if v > n {
+				v = n
+			}
+			cnt += v
+		}
+		if cnt < k {
+			lo = mid + 1
+		} else {
+			hi = mid - 1
+			res = mid
+		}
+	}
+
+	_, _ = fmt.Fprint(writer, res)
+}
