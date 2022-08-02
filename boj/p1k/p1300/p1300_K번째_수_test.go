198f8022f87bbb7b6442e35c08e8c18683d55ec0
zrma <bulbitain@gmail.com>
2022-08-02 21:01:25 +0900

    boj: solve 1300 K번째 수
    - https://www.acmicpc.net/problem/1300

---
+++ boj/p1k/p1300/p1300_K번째_수_test.go
@@ -0,0 +1,46 @@
+package p1300_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1300"
+	"1d1go/utils"
+)
+
+func TestSolve1300(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1300")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`3
+7`,
+			"6",
+		},
+		{
+			`100000
+1000000000`,
+			"204535821",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1300.Solve1300(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
