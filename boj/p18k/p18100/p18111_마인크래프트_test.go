7d6f315b667c3dee982309436af4fcb11389f845
zrma <bulbitain@gmail.com>
2022-08-08 23:20:41 +0900

    boj: solve 18111 마인크래프트
    - https://www.acmicpc.net/problem/18111

---
+++ boj/p18k/p18100/p18111_마인크래프트_test.go
@@ -0,0 +1,57 @@
+package p18100_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p18k/p18100"
+	"1d1go/utils"
+)
+
+func TestSolve18111(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/18111")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`3 4 99
+0 0 0 0
+0 0 0 0
+0 0 0 1`,
+			"2 0",
+		},
+		{
+			`3 4 1
+64 64 64 64
+64 64 64 64
+64 64 64 63`,
+			"1 64",
+		},
+		{
+			`3 4 0
+64 64 64 64
+64 64 64 64
+64 64 64 63`,
+			"22 63",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p18100.Solve18111(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
