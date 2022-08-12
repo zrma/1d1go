413d52d5884f93f7ed08b429abb54e5d3e9dff1d
zrma <bulbitain@gmail.com>
2022-08-12 21:17:56 +0900

    boj: solve 25304 영수증
    - https://www.acmicpc.net/problem/25304

---
+++ boj/p25k/p25300/p25304_영수증_test.go
@@ -0,0 +1,54 @@
+package p25300_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p25k/p25300"
+	"1d1go/utils"
+)
+
+func TestSolve25304(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/25304")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`260000
+4
+20000 5
+30000 2
+10000 6
+5000 8`,
+			"Yes",
+		},
+		{
+			`250000
+4
+20000 5
+30000 2
+10000 6
+5000 8`,
+			"No",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p25300.Solve25304(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
