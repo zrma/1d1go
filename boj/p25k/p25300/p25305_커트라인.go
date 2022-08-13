f7605782a123fbf8eaec32e94344e7a8aecf9377
zrma <bulbitain@gmail.com>
2022-08-13 22:17:16 +0900

    boj: solve 25305 커트라인
    - https://www.acmicpc.net/problem/25305

---
+++ boj/p25k/p25300/p25305_커트라인.go
@@ -0,0 +1,22 @@
+package p25300
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve25305(reader Reader, writer Writer) {
+	var n, k int
+	_, _ = fmt.Fscan(reader, &n, &k)
+
+	arr := make([]int, n)
+	for i := range arr {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] > arr[j]
+	})
+
+	_, _ = fmt.Fprint(writer, arr[k-1])
+}
