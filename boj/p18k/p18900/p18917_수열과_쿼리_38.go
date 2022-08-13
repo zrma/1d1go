32c4bcf4043468523791ee508a46d31e5dbbc98c
zrma <bulbitain@gmail.com>
2022-08-13 22:35:24 +0900

    boj: solve 18917 수열과 쿼리 38
    - https://www.acmicpc.net/problem/18917

---
+++ boj/p18k/p18900/p18917_수열과_쿼리_38.go
@@ -0,0 +1,34 @@
+package p18900
+
+import (
+	"fmt"
+)
+
+func Solve18917(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	var sum int64
+	var xor int64
+	for i := 0; i < n; i++ {
+		var q int64
+		_, _ = fmt.Fscan(reader, &q)
+
+		switch q {
+		case 1:
+			var v int64
+			_, _ = fmt.Fscan(reader, &v)
+			sum += v
+			xor ^= v
+		case 2:
+			var v int64
+			_, _ = fmt.Fscan(reader, &v)
+			sum -= v
+			xor ^= v
+		case 3:
+			_, _ = fmt.Fprintln(writer, sum)
+		default:
+			_, _ = fmt.Fprintln(writer, xor)
+		}
+	}
+}
---
+++ boj/p18k/p18900/p18917_수열과_쿼리_38_test.go
@@ -0,0 +1,58 @@
+package p18900_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p18k/p18900"
+	"1d1go/utils"
+)
+
+func TestSolve18917(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/18917")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`12
+1 3
+1 1
+1 4
+3
+4
+1 1
+3
+4
+2 1
+2 4
+3
+4`,
+			`8
+6
+9
+7
+4
+2
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p18900.Solve18917(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
