c7382706c72ba72096fd66069a5b1a0b7c386ce7
zrma <bulbitain@gmail.com>
2022-08-24 22:50:52 +0900

    boj: solve 24480 알고리즘 수업 - 깊이 우선 탐색 2
    - https://www.acmicpc.net/problem/24480

---
+++ boj/p24k/p24400/p24480_알고리즘_수업-깊이_우선_탐색_2_test.go
@@ -0,0 +1,50 @@
+package p24400_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p24k/p24400"
+	"1d1go/utils"
+)
+
+func TestSolve24480(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/24480")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`5 5 1
+1 4
+1 2
+2 3
+2 4
+3 4`,
+			`1
+4
+3
+2
+0
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p24400.Solve24480(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
