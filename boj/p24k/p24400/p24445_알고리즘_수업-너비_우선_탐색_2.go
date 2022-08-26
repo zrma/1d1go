33333fc3ad0bfb84ec122f96bafef61e661f1948
zrma <bulbitain@gmail.com>
2022-08-26 21:18:10 +0900

    boj: solve 24445 알고리즘 수업 - 너비 우선 탐색 2
    - https://www.acmicpc.net/problem/24445

---
+++ boj/p24k/p24400/p24445_알고리즘_수업-너비_우선_탐색_2.go
@@ -0,0 +1,33 @@
+package p24400
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve24445(reader Reader, writer Writer) {
+	var n, m, r int
+	_, _ = fmt.Fscan(reader, &n, &m, &r)
+
+	graph = make([][]int, n+1)
+	seq = make([]int, n+1)
+
+	for i := 0; i < m; i++ {
+		var a, b int
+		_, _ = fmt.Fscan(reader, &a, &b)
+		graph[a] = append(graph[a], b)
+		graph[b] = append(graph[b], a)
+	}
+
+	for i := 0; i <= n; i++ {
+		sort.Slice(graph[i], func(j, k int) bool {
+			return graph[i][j] > graph[i][k]
+		})
+	}
+
+	bfs(r)
+
+	for i := 1; i <= n; i++ {
+		_, _ = fmt.Fprintln(writer, seq[i])
+	}
+}
