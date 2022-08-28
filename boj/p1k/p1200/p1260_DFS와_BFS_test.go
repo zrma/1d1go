623a53eeb59e5cada39a65d11893eb14860253a9
zrma <bulbitain@gmail.com>
2022-08-28 17:34:44 +0900

    boj: solve 1260 DFS와 BFS
    - https://www.acmicpc.net/problem/1260

---
+++ boj/p1k/p1200/p1260_DFS와_BFS_test.go
@@ -0,0 +1,62 @@
+package p1200_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1200"
+	"1d1go/utils"
+)
+
+func TestSolve1260(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1260")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`4 5 1
+1 2
+1 3
+1 4
+2 4
+3 4`,
+			`1 2 4 3
+1 2 3 4`,
+		},
+		{
+			`5 5 3
+5 4
+5 2
+1 2
+3 4
+3 1`,
+			`3 1 2 5 4
+3 1 4 2 5`,
+		},
+		{
+			`1000 1 1000
+999 1000`,
+			`1000 999
+1000 999`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1200.Solve1260(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
