c0d009e245b526b6b6b01936715bd0d681c7e75b
zrma <bulbitain@gmail.com>
2022-08-28 17:34:44 +0900

    boj: solve 1260 DFS와 BFS
    - https://www.acmicpc.net/problem/1260

---
+++ boj/p1k/p1200/p1260_DFS와_BFS.go
@@ -0,0 +1,71 @@
+package p1200
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve1260(reader Reader, writer Writer) {
+	var n, m, r int
+	_, _ = fmt.Fscan(reader, &n, &m, &r)
+
+	graph = make([][]int, n+1)
+	visited = make([]bool, n+1)
+	for i := 0; i < m; i++ {
+		var a, b int
+		_, _ = fmt.Fscan(reader, &a, &b)
+		graph[a] = append(graph[a], b)
+		graph[b] = append(graph[b], a)
+	}
+
+	for i := 0; i <= n; i++ {
+		sort.Ints(graph[i])
+	}
+
+	dfs(r, writer)
+	_, _ = fmt.Fprintln(writer)
+
+	visited = make([]bool, n+1)
+	bfs(r, writer)
+}
+
+var (
+	graph   [][]int
+	visited []bool
+)
+
+func dfs(r int, writer Writer) {
+	visited[r] = true
+	_, _ = fmt.Fprint(writer, r)
+
+	for _, v := range graph[r] {
+		if visited[v] {
+			continue
+		}
+		_, _ = fmt.Fprint(writer, " ")
+		dfs(v, writer)
+	}
+}
+
+func bfs(r int, writer Writer) {
+	queue := make([]int, 0, len(visited))
+	queueIdx := 0
+
+	queue = append(queue, r)
+	visited[r] = true
+	_, _ = fmt.Fprint(writer, r)
+
+	for queueIdx < len(queue) {
+		here := queue[queueIdx]
+		queueIdx++
+
+		for _, next := range graph[here] {
+			if visited[next] {
+				continue
+			}
+			visited[next] = true
+			_, _ = fmt.Fprint(writer, " ", next)
+			queue = append(queue, next)
+		}
+	}
+}
