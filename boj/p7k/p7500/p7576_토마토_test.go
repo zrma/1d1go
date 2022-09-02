03cbb6967eecfe9398f58434d561bb1c58d4118b
zrma <bulbitain@gmail.com>
2022-09-02 23:22:14 +0900

    boj: solve 7576 토마토
    - https://www.acmicpc.net/problem/7576

---
+++ boj/p7k/p7500/p7576_토마토_test.go
@@ -0,0 +1,75 @@
+package p7500_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p7k/p7500"
+	"1d1go/utils"
+)
+
+func TestSolve7576(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/7576")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`6 4
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 1`,
+			"8",
+		},
+		{
+			`6 4
+0 -1 0 0 0 0
+-1 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 1`,
+			"-1",
+		},
+		{
+			`6 4
+1 -1 0 0 0 0
+0 -1 0 0 0 0
+0 0 0 0 -1 0
+0 0 0 0 -1 1`,
+			"6",
+		},
+		{
+			`5 5
+-1 1 0 0 0
+0 -1 -1 -1 0
+0 -1 -1 -1 0
+0 -1 -1 -1 0
+0 0 0 0 0`,
+			"14",
+		},
+		{
+			`2 2
+1 -1
+-1 1`,
+			"0",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p7500.Solve7576(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
