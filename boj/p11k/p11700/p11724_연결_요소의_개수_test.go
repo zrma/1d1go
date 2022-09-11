6ab6afadd79aa0982c3df7cd7e09ef1191753969
zrma <bulbitain@gmail.com>
2022-09-11 15:23:52 +0900

    boj: solve 11724 연결 요소의 개수
    - https://www.acmicpc.net/problem/11724

---
+++ boj/p11k/p11700/p11724_연결_요소의_개수_test.go
@@ -0,0 +1,57 @@
+package p11700_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p11k/p11700"
+	"1d1go/utils"
+)
+
+func TestSolve11724(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/11724")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`6 5
+1 2
+2 5
+5 1
+3 4
+4 6`,
+			"2",
+		},
+		{
+			`6 8
+1 2
+2 5
+5 1
+3 4
+4 6
+5 4
+2 4
+2 3`,
+			"1",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p11700.Solve11724(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
