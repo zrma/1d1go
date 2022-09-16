ba8f178849ee76aadd265348f3c8ec3d9de43ae1
zrma <bulbitain@gmail.com>
2022-09-16 22:10:14 +0900

    boj: solve 25501 재귀의 귀재
    - https://www.acmicpc.net/problem/25501

---
+++ boj/p25k/p25500/p25501_재귀의_귀재.go
@@ -0,0 +1,31 @@
+package p25500
+
+import (
+	"fmt"
+)
+
+func Solve25501(reader Reader, writer Writer) {
+	var t int
+	_, _ = fmt.Fscan(reader, &t)
+
+	for i := 0; i < t; i++ {
+		var s string
+		_, _ = fmt.Fscan(reader, &s)
+
+		l := 0
+		isPalindrome := true
+		for r := len(s) - 1; l < r; l, r = l+1, r-1 {
+			if s[l] != s[r] {
+				isPalindrome = false
+				break
+			}
+		}
+
+		l += 1
+		if isPalindrome {
+			_, _ = fmt.Fprintf(writer, "%d %d\n", 1, l)
+		} else {
+			_, _ = fmt.Fprintf(writer, "%d %d\n", 0, l)
+		}
+	}
+}
