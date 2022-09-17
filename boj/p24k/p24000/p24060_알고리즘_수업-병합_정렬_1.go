f6bcbabc80e5a0c0e4fcfe4f81ca1dbd21129406
zrma <bulbitain@gmail.com>
2022-09-17 13:44:41 +0900

    boj: solve 24060 알고리즘 수업 - 병합 정렬 1
    - https://www.acmicpc.net/problem/24060

---
+++ boj/p24k/p24000/p24060_알고리즘_수업-병합_정렬_1.go
@@ -0,0 +1,74 @@
+package p24000
+
+import (
+	"fmt"
+)
+
+func Solve24060(reader Reader, writer Writer) {
+	var a, k int
+	_, _ = fmt.Fscan(reader, &a, &k)
+
+	arr := make([]int, a)
+	for i := 0; i < a; i++ {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+
+	saveCount := 0
+	tmp := make([]int, a+2)
+	merge := func(p, q, r int) {
+		var (
+			i = p
+			j = q + 1
+			t = 1
+		)
+		for i <= q && j <= r {
+			if arr[i] <= arr[j] {
+				tmp[t] = arr[i]
+				t++
+				i++
+			} else {
+				tmp[t] = arr[j]
+				t++
+				j++
+			}
+		}
+		for i <= q {
+			tmp[t] = arr[i]
+			t++
+			i++
+		}
+		for j <= r {
+			tmp[t] = arr[j]
+			t++
+			j++
+		}
+
+		i, j = p, 1
+		for i <= r {
+			arr[i] = tmp[j]
+			saveCount++
+			if saveCount == k {
+				_, _ = fmt.Fprint(writer, tmp[j])
+			}
+			i++
+			j++
+		}
+	}
+
+	var mergeSort func(p, r int)
+	mergeSort = func(p, r int) {
+		if p >= r {
+			return
+		}
+		q := (p + r) / 2
+		mergeSort(p, q)
+		mergeSort(q+1, r)
+		merge(p, q, r)
+	}
+
+	mergeSort(0, a-1)
+
+	if saveCount < k {
+		_, _ = fmt.Fprint(writer, -1)
+	}
+}
