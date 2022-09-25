314ddd01210b2f558ad36f371f45c744cc6b7232
zrma <bulbitain@gmail.com>
2022-09-25 16:15:00 +0900

    boj: solve 17299 오등큰수
    - https://www.acmicpc.net/problem/17299

---
+++ boj/p17k/p17200/p17299_오등큰수.go
@@ -0,0 +1,35 @@
+package p17200
+
+import (
+	"fmt"
+)
+
+func Solve17299(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	const max = 1_000_000 + 1
+
+	arr := make([]int, n)
+	res := make([]int, n)
+	counts := make([]int, max)
+
+	for i := 0; i < n; i++ {
+		res[i] = -1
+		_, _ = fmt.Fscan(reader, &arr[i])
+		counts[arr[i]]++
+	}
+
+	stack := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		for len(stack) > 0 && counts[arr[stack[len(stack)-1]]] < counts[arr[i]] {
+			res[stack[len(stack)-1]] = arr[i]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+
+	for i := 0; i < n; i++ {
+		_, _ = fmt.Fprintf(writer, "%d ", res[i])
+	}
+}
---
+++ boj/p17k/p17200/p17299_오등큰수_test.go
@@ -0,0 +1,41 @@
+package p17200_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p17k/p17200"
+	"1d1go/utils"
+)
+
+func TestSolve17299(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/17299")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`7
+1 1 2 3 4 2 1`,
+			`-1 -1 1 2 2 1 -1 `,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p17200.Solve17299(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
