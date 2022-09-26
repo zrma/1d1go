cf0fd4e5150ff8606495a529aec3030717264f4a
zrma <bulbitain@gmail.com>
2022-09-26 20:41:28 +0900

    boj: solve 11404 플로이드
    - https://www.acmicpc.net/problem/11404

---
+++ boj/p11k/p11400/p11404_플로이드.go
@@ -0,0 +1,49 @@
+package p11400
+
+import (
+	"fmt"
+
+	"1d1go/boj/p1k/p1300"
+)
+
+func Solve11404(reader Reader, writer Writer) {
+	var n, m int
+	_, _ = fmt.Fscan(reader, &n, &m)
+
+	const max = 1_000_000_000
+
+	graph := make([][]int, n+1)
+	for from := 1; from <= n; from++ {
+		graph[from] = make([]int, n+1)
+		for to := 1; to <= n; to++ {
+			if from != to {
+				graph[from][to] = max
+			}
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		var from, to, cost int
+		_, _ = fmt.Fscan(reader, &from, &to, &cost)
+		if cost < graph[from][to] {
+			graph[from][to] = cost
+		}
+	}
+
+	p1300.FloydWarshall(graph, n)
+
+	for from := 1; from <= n; from++ {
+		for to := 1; to <= n; to++ {
+			if graph[from][to] >= max {
+				_, _ = fmt.Fprint(writer, 0)
+			} else {
+				_, _ = fmt.Fprint(writer, graph[from][to])
+			}
+
+			if to != n {
+				_, _ = fmt.Fprint(writer, " ")
+			}
+		}
+		_, _ = fmt.Fprintln(writer)
+	}
+}
---
+++ boj/p11k/p11400/p11404_플로이드_test.go
@@ -0,0 +1,71 @@
+package p11400_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p11k/p11400"
+	"1d1go/utils"
+)
+
+func TestSolve11404(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/11404")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`5
+14
+1 2 2
+1 3 3
+1 4 1
+1 5 10
+2 4 2
+3 4 1
+3 5 1
+4 5 3
+3 5 10
+3 1 8
+1 4 2
+5 1 7
+3 4 2
+5 2 4`,
+			`0 2 3 1 4
+12 0 15 2 5
+8 5 0 1 1
+10 7 13 0 3
+7 4 10 6 0
+`,
+		},
+		{
+			`5
+1
+1 2 5`,
+			`0 5 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p11400.Solve11404(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
