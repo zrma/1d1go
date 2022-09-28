579e77fda38dad5ef0b95a59543e138c7e3f4efb
zrma <bulbitain@gmail.com>
2022-09-29 00:23:17 +0900

    boj: solve 1725 히스토그램
    - https://www.acmicpc.net/problem/1725

---
+++ boj/p1k/p1700/p1725_히스토그램_test.go
@@ -0,0 +1,55 @@
+package p1700_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1700"
+	"1d1go/utils"
+)
+
+func TestSolve1725(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1725")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`7
+2
+1
+4
+5
+1
+3
+3
+`,
+			"8",
+		},
+		{"4 1000 1000 1000 1000", "4000"},
+		{"12 3 2 3 4 5 4 3 2 3 1 1 1", "18"},
+		{"12 28 17 0 7 29 18 16 12 3 30 29 26", "78"},
+		{"2 10 7", "14"},
+		{"4 8 9 10 1", "24"},
+		{"1 1", "1"},
+		{"1 100", "100"},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1700.Solve1725(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
