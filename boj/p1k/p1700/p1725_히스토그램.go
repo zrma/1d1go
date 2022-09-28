de7e3514fb5492e6f1bd54035cf123c6fe898dd8
zrma <bulbitain@gmail.com>
2022-09-29 00:23:17 +0900

    boj: solve 1725 히스토그램
    - https://www.acmicpc.net/problem/1725

---
+++ boj/p1k/p1700/p1725_히스토그램.go
@@ -0,0 +1,37 @@
+package p1700
+
+import (
+	"fmt"
+)
+
+func Solve1725(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	arr := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+
+	stack := make([]int, 0, n)
+	stack = append(stack, 0)
+	res := 0
+	for i := 1; i <= n; i++ {
+		for len(stack) > 0 && arr[stack[len(stack)-1]] > arr[i] {
+			h := arr[stack[len(stack)-1]]
+			stack = stack[:len(stack)-1]
+
+			w := i
+			if len(stack) > 0 {
+				w = i - stack[len(stack)-1] - 1
+			}
+
+			if res < h*w {
+				res = h * w
+			}
+		}
+		stack = append(stack, i)
+	}
+
+	_, _ = fmt.Fprint(writer, res)
+}
