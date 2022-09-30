63ca7afda96857b248a4a46c09d2ae661bef15e6
zrma <bulbitain@gmail.com>
2022-09-30 23:41:51 +0900

    boj: solve 3273 두 수의 합
    - https://www.acmicpc.net/problem/3273

---
+++ boj/p3k/p3200/p3273_두_수의_합.go
@@ -0,0 +1,74 @@
+package p3200
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve3273(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+
+	var x int
+	_, _ = fmt.Fscan(reader, &x)
+
+	const max = 2_000_000
+	counts := make([]int, max+1)
+
+	res := 0
+	for _, v := range arr {
+		if v >= x {
+			continue
+		}
+
+		if counts[v] > 0 {
+			res++
+			counts[v]--
+			continue
+		}
+
+		counts[x-v]++
+	}
+
+	_, _ = fmt.Fprint(writer, res)
+}
+
+func Solve3273WithTwoPoints(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+
+	var x int
+	_, _ = fmt.Fscan(reader, &x)
+
+	sort.Ints(arr)
+
+	res := 0
+	lo, hi := 0, n-1
+	for lo < hi {
+		if arr[lo]+arr[hi] == x {
+			res++
+			lo++
+			hi--
+			continue
+		}
+
+		if arr[lo]+arr[hi] < x {
+			lo++
+			continue
+		}
+
+		hi--
+	}
+
+	_, _ = fmt.Fprint(writer, res)
+}
---
+++ boj/p3k/p3200/p3273_두_수의_합_test.go
@@ -0,0 +1,73 @@
+package p3200_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p3k/p3200"
+	"1d1go/utils"
+)
+
+func TestSolve3273(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/3273")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`9
+5 12 7 10 9 1 2 3 11
+13`,
+			"3",
+		},
+		{
+			`4
+1 3 5 1
+4`,
+			"1",
+		},
+		{
+			`5
+1 3 5 3 1
+4`,
+			"2",
+		},
+		{
+			`5
+3 3 5 1 1
+4`,
+			"2",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d/counting", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p3200.Solve3273(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+
+		t.Run(fmt.Sprintf("%d/two point", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p3200.Solve3273WithTwoPoints(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
