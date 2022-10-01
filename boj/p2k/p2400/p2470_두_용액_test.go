90be84ed73c8916dd99f4a730f16f205d5d1cd39
zrma <bulbitain@gmail.com>
2022-10-01 20:04:20 +0900

    boj: solve 2470 두 용액
    - https://www.acmicpc.net/problem/2470

---
+++ boj/p2k/p2400/p2470_두_용액_test.go
@@ -0,0 +1,46 @@
+package p2400_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p2k/p2400"
+	"1d1go/utils"
+)
+
+func TestSolve2470(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2470")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`5
+-2 4 -99 -1 98`,
+			"-99 98",
+		},
+		{
+			`5
+-2 3 -99 1 97`,
+			"-2 3",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p2400.Solve2470(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
