35af271a87af02d830ad0488036b36eb7dc84051
zrma <bulbitain@gmail.com>
2022-10-02 21:16:21 +0900

    boj: solve 1806 부분합
    - https://www.acmicpc.net/problem/1806

---
+++ boj/p1k/p1800/p1806_부분합_test.go
@@ -0,0 +1,56 @@
+package p1800_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1800"
+	"1d1go/utils"
+)
+
+func TestSolve1806(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1806")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`10 15
+5 1 3 5 10 7 4 9 2 8`,
+			"2",
+		},
+		{
+			`10 11
+10 1 1 1 1 1 1 1 1 1`,
+			"2",
+		},
+		{
+			`10 10
+10 1 1 1 1 1 1 1 1 1`,
+			"1",
+		},
+		{
+			`10 100
+1 1 1 1 1 1 1 1 1 1`,
+			"0",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1800.Solve1806(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
