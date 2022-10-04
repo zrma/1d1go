27f5d40b46734f7c68b7b41e5c5fad608f7eecaa
zrma <bulbitain@gmail.com>
2022-10-04 23:06:40 +0900

    boj: solve 1450 냅색문제
    - https://www.acmicpc.net/problem/1450

---
+++ boj/p1k/p1400/p1450_냅색문제_test.go
@@ -0,0 +1,66 @@
+package p1400_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1400"
+	"1d1go/utils"
+)
+
+func TestSolve1450(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1450")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`2 1
+1 1`,
+			"3",
+		},
+		{
+			`1 1
+1`,
+			"2",
+		},
+		{
+			`1 2
+1`,
+			"2",
+		},
+		{
+			`2 1
+2 2`,
+			"1",
+		},
+		{
+			`2 2
+1 1`,
+			"4",
+		},
+		{
+			`30 30
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1`,
+			"1073741824",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1400.Solve1450(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
