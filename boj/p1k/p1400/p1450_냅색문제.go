c0cbb26b63dc010fd476e1dc499e3218b1de35aa
zrma <bulbitain@gmail.com>
2022-10-04 23:06:40 +0900

    boj: solve 1450 냅색문제
    - https://www.acmicpc.net/problem/1450

---
+++ boj/p1k/p1400/p1450_냅색문제.go
@@ -0,0 +1,59 @@
+package p1400
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve1450(reader Reader, writer Writer) {
+	var n, c int
+	_, _ = fmt.Fscan(reader, &n, &c)
+
+	weights := make([]int, n)
+	for i := 0; i < n; i++ {
+		_, _ = fmt.Fscan(reader, &weights[i])
+	}
+
+	// 0 ~ n/2 까지의 무게 조합
+	result0 := knapsack(weights, 0, n/2, c, 0)
+
+	// n/2 ~ n 까지의 무게 조합
+	result1 := knapsack(weights, n/2, n, c, 0)
+
+	sort.Ints(result0)
+	sort.Ints(result1)
+
+	res := 0
+	lo, hi := 0, len(result1)-1
+
+	for {
+		if lo >= len(result0) || hi < 0 {
+			break
+		}
+
+		if result0[lo]+result1[hi] > c {
+			hi--
+			continue
+		}
+
+		res += hi + 1
+		lo++
+	}
+
+	_, _ = fmt.Fprint(writer, res)
+}
+
+func knapsack(weights []int, start, end, c, sum int) []int {
+	if sum > c {
+		return nil
+	}
+
+	if start == end {
+		return []int{sum}
+	}
+
+	return append(
+		knapsack(weights, start+1, end, c, sum+weights[start]), // 넣어봄.
+		knapsack(weights, start+1, end, c, sum)...,             // 빼봄.
+	)
+}
