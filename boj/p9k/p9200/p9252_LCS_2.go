3e0f8f2e121d7eb46a872ea7eb4e917344203513
zrma <bulbitain@gmail.com>
2022-10-08 23:25:59 +0900

    boj: solve 9252 LCS 2
    - https://www.acmicpc.net/problem/9252

--- boj/p9k/p9200/p9252_LCS_2.go
+++ boj/p9k/p9200/p9252_LCS_2.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func Solve9251(reader Reader, writer Writer) {
+func Solve9252(reader Reader, writer Writer) {
 	var s0, s1 string
 	_, _ = fmt.Fscan(reader, &s0, &s1)
 
@@ -27,5 +27,23 @@ func Solve9251(reader Reader, writer Writer) {
 			}
 		}
 	}
-	_, _ = fmt.Fprint(writer, dp[n0][n1])
+	_, _ = fmt.Fprintln(writer, dp[n0][n1])
+
+	res := make([]rune, 0, dp[n0][n1])
+	for i, j := n0, n1; i > 0 && j > 0; {
+		if s0[i-1] == s1[j-1] {
+			res = append(res, rune(s0[i-1]))
+			i--
+			j--
+		} else {
+			if dp[i-1][j] > dp[i][j-1] {
+				i--
+			} else {
+				j--
+			}
+		}
+	}
+	for i := len(res) - 1; i >= 0; i-- {
+		_, _ = fmt.Fprintf(writer, "%c", res[i])
+	}
 }
---
+++ boj/p9k/p9200/p9252_LCS_2_test.go
@@ -0,0 +1,55 @@
+package p9200_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p9k/p9200"
+	"1d1go/utils"
+)
+
+func TestSolve9252(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/9252")
+
+	//goland:noinspection SpellCheckingInspection
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`ACAYKP
+CAPCAK`,
+			`4
+ACAK`,
+		},
+		{
+			`ABCDEF
+GBCDEF`,
+			`5
+BCDEF`,
+		},
+		{
+			`ABCDEF
+GBCDFE`,
+			`4
+BCDF`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p9200.Solve9252(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
