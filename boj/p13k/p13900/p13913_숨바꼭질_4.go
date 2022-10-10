ce6a79815426565a5c52fd77bd986ca9ca4ce689
zrma <bulbitain@gmail.com>
2022-10-10 18:17:53 +0900

    boj: solve 13913 숨바꼭질 4
    - https://www.acmicpc.net/problem/13913

---
+++ boj/p13k/p13900/p13913_숨바꼭질_4.go
@@ -0,0 +1,54 @@
+package p13900
+
+import (
+	"fmt"
+)
+
+func Solve13913(reader Reader, writer Writer) {
+	var n, k int
+	_, _ = fmt.Fscan(reader, &n, &k)
+
+	const max = 200000 + 1
+	points := make([]int, max)
+	prev := make([]int, max)
+
+	queue := make([]int, 0, max)
+	queue = append(queue, n)
+
+	for len(queue) > 0 {
+		p := queue[0]
+		queue = queue[1:]
+
+		if p == k {
+			break
+		}
+
+		cur := points[p]
+		if p > 0 && points[p-1] == 0 {
+			points[p-1] = cur + 1
+			prev[p-1] = p
+			queue = append(queue, p-1)
+		}
+		if p < max-1 && points[p+1] == 0 {
+			points[p+1] = cur + 1
+			prev[p+1] = p
+			queue = append(queue, p+1)
+		}
+		if p*2 < max && points[p*2] == 0 {
+			points[p*2] = cur + 1
+			prev[p*2] = p
+			queue = append(queue, p*2)
+		}
+	}
+	_, _ = fmt.Fprintln(writer, points[k])
+
+	path := make([]int, 0, points[k]+1)
+	for p := k; p != n; p = prev[p] {
+		path = append(path, p)
+	}
+	path = append(path, n)
+
+	for i := len(path) - 1; i >= 0; i-- {
+		_, _ = fmt.Fprintf(writer, "%d ", path[i])
+	}
+}
