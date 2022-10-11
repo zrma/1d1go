661e669e4919f405a368de4864406bd4a50ba5a8
zrma <bulbitain@gmail.com>
2022-10-11 23:40:47 +0900

    boj: solve 9019 DSLR
    - https://www.acmicpc.net/problem/9019

---
+++ boj/p9k/p9000/p9019_DSLR.go
@@ -0,0 +1,67 @@
+package p9000
+
+import (
+	"fmt"
+)
+
+func Solve9019(reader Reader, writer Writer) {
+	var t int
+	_, _ = fmt.Fscan(reader, &t)
+
+	for i := 0; i < t; i++ {
+		solve9019(reader, writer)
+	}
+}
+
+func solve9019(reader Reader, writer Writer) {
+	var a, b int
+	_, _ = fmt.Fscan(reader, &a, &b)
+
+	const max = 10000
+	visited := make([]bool, max)
+	commands := make([]string, max)
+
+	queue := make([]int, 0, max)
+	queue = append(queue, a)
+	visited[a] = true
+
+	for len(queue) > 0 {
+		p := queue[0]
+		queue = queue[1:]
+
+		if p == b {
+			break
+		}
+
+		d := (p * 2) % max
+		if !visited[d] {
+			visited[d] = true
+			commands[d] = commands[p] + "D"
+			queue = append(queue, d)
+		}
+
+		s := (p - 1 + max) % max
+		if !visited[s] {
+			visited[s] = true
+			commands[s] = commands[p] + "S"
+			queue = append(queue, s)
+		}
+
+		l := (p % 1000) * 10
+		l += p / 1000
+		if !visited[l] {
+			visited[l] = true
+			commands[l] = commands[p] + "L"
+			queue = append(queue, l)
+		}
+
+		r := (p % 10) * 1000
+		r += p / 10
+		if !visited[r] {
+			visited[r] = true
+			commands[r] = commands[p] + "R"
+			queue = append(queue, r)
+		}
+	}
+	_, _ = fmt.Fprintln(writer, commands[b])
+}
---
+++ boj/p9k/p9000/p9019_DSLR_test.go
@@ -0,0 +1,71 @@
+package p9000_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p9k/p9000"
+	"1d1go/utils"
+)
+
+func TestSolve9019(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/9019")
+
+	//goland:noinspection SpellCheckingInspection
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`3
+1234 3412
+1000 1
+1 16`,
+			`LL
+L
+DDDD
+`,
+		},
+		{
+			`4
+1 0
+1 10
+1 1000
+1 1001`,
+			`S
+L
+R
+DDSDDSR
+`,
+		},
+		{
+			`4
+123 1230
+123 3012
+0 9999
+1 1`,
+			`L
+R
+S
+
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p9000.Solve9019(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
