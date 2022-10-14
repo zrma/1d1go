e28f77f7c762e7eeac310299a2ca59c482cfe890
zrma <bulbitain@gmail.com>
2022-10-15 00:16:27 +0900

    boj: solve 1167 트리의 지름
    - https://www.acmicpc.net/problem/1167

---
+++ boj/p1k/p1100/p1167_트리의_지름.go
@@ -0,0 +1,69 @@
+package p1100
+
+import (
+	"fmt"
+)
+
+func Solve1167(reader Reader, writer Writer) {
+	var v int
+	_, _ = fmt.Fscan(reader, &v)
+
+	tree := make([]Node, v+1)
+	for i := 1; i <= v; i++ {
+		var idx int
+		_, _ = fmt.Fscan(reader, &idx)
+
+		for {
+			var n int
+			_, _ = fmt.Fscan(reader, &n)
+			if n == -1 {
+				break
+			}
+			var c int
+			_, _ = fmt.Fscan(reader, &c)
+			tree[idx].Children = append(tree[idx].Children, Edge{Idx: n, Cost: c})
+		}
+	}
+
+	farthestNode0, _ := GetFarthestNode(tree, 1)
+	_, dist := GetFarthestNode(tree, farthestNode0)
+	_, _ = fmt.Fprint(writer, dist)
+
+}
+
+func GetFarthestNode(tree []Node, start int) (farthestNode int, maxCost int) {
+	visited := make([]bool, len(tree))
+	visited[start] = true
+
+	costs := make([]int, len(tree))
+
+	queue := []int{start}
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+
+		for _, child := range tree[cur].Children {
+			if visited[child.Idx] {
+				continue
+			}
+
+			visited[child.Idx] = true
+			queue = append(queue, child.Idx)
+			costs[child.Idx] = costs[cur] + child.Cost
+			if maxCost < costs[child.Idx] {
+				maxCost = costs[child.Idx]
+				farthestNode = child.Idx
+			}
+		}
+	}
+	return
+}
+
+type Node struct {
+	Children []Edge
+}
+
+type Edge struct {
+	Idx  int
+	Cost int
+}
---
+++ boj/p1k/p1100/p1167_트리의_지름_test.go
@@ -0,0 +1,61 @@
+package p1100_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1100"
+	"1d1go/utils"
+)
+
+func TestSolve1167(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1167")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`5
+1 3 2 -1
+2 4 4 -1
+3 1 2 4 3 -1
+4 2 4 3 3 5 6 -1
+5 4 6 -1`,
+			"11",
+		},
+		{
+			`12
+1 2 3 3 2 -1
+2 1 3 4 5 -1
+3 1 2 5 11 6 9 -1
+4 2 5 7 1 8 7 -1
+5 3 11 9 15 10 4 -1
+6 3 9 11 6 12 10 -1
+7 4 1 -1
+8 4 7 -1
+9 5 15 -1
+10 5 4 -1
+11 6 6 -1
+12 6 10 -1`,
+			"45",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1100.Solve1167(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
