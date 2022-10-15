2501c576eacfca7468f94c18019cdd0574d1c8c2
zrma <bulbitain@gmail.com>
2022-10-15 11:46:51 +0900

    boj: solve 1967 트리의 지름
    - https://www.acmicpc.net/problem/1967

---
+++ boj/p1k/p1900/p1967_트리의_지름.go
@@ -0,0 +1,25 @@
+package p1900
+
+import (
+	"fmt"
+
+	"1d1go/boj/p1k/p1100"
+)
+
+func Solve1967(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	tree := make([]p1100.Node, n+1)
+	for i := 1; i <= n; i++ {
+		var from, to, cost int
+		_, _ = fmt.Fscan(reader, &from, &to, &cost)
+
+		tree[from].Children = append(tree[from].Children, p1100.Edge{Idx: to, Cost: cost})
+		tree[to].Children = append(tree[to].Children, p1100.Edge{Idx: from, Cost: cost})
+	}
+
+	farthestNode0, _ := p1100.GetFarthestNode(tree, 1)
+	_, dist := p1100.GetFarthestNode(tree, farthestNode0)
+	_, _ = fmt.Fprint(writer, dist)
+}
