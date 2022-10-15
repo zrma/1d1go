f23aa05cccb5c57d40973f6e3ab560bb22418701
zrma <bulbitain@gmail.com>
2022-10-15 11:46:51 +0900

    boj: solve 1967 트리의 지름
    - https://www.acmicpc.net/problem/1967

---
+++ boj/p1k/p1900/p1967_트리의_지름_test.go
@@ -0,0 +1,51 @@
+package p1900_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1900"
+	"1d1go/utils"
+)
+
+func TestSolve1967(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1967")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`12
+1 2 3
+1 3 2
+2 4 5
+3 5 11
+3 6 9
+4 7 1
+4 8 7
+5 9 15
+5 10 4
+6 11 6
+6 12 10`,
+			"45",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1900.Solve1967(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
