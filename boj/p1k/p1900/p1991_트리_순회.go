922229464f196aecc8ae2f8c8b24fbe7bf3dd887
zrma <bulbitain@gmail.com>
2022-10-19 05:04:06 +0900

    boj: solve 1991 트리 순회
    - https://www.acmicpc.net/problem/1991

---
+++ boj/p1k/p1900/p1991_트리_순회.go
@@ -0,0 +1,63 @@
+package p1900
+
+import (
+	"fmt"
+)
+
+func Solve1991(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	arr := make([]node, 26)
+	for i := 0; i < n; i++ {
+		var val, left, right string
+		_, _ = fmt.Fscan(reader, &val, &left, &right)
+
+		idx := val[0] - 'A'
+		arr[idx] = node{left, right}
+	}
+
+	traversePreOrder("A", arr, writer)
+	_, _ = fmt.Fprintln(writer)
+
+	traverseInOrder("A", arr, writer)
+	_, _ = fmt.Fprintln(writer)
+
+	traversePostOrder("A", arr, writer)
+	_, _ = fmt.Fprintln(writer)
+}
+
+type node struct {
+	left  string
+	right string
+}
+
+func traversePreOrder(root string, arr []node, writer Writer) {
+	if root == "." {
+		return
+	}
+	idx := root[0] - 'A'
+	_, _ = fmt.Fprint(writer, root)
+	traversePreOrder(arr[idx].left, arr, writer)
+	traversePreOrder(arr[idx].right, arr, writer)
+}
+
+func traverseInOrder(root string, arr []node, writer Writer) {
+	if root == "." {
+		return
+	}
+	idx := root[0] - 'A'
+	traverseInOrder(arr[idx].left, arr, writer)
+	_, _ = fmt.Fprint(writer, root)
+	traverseInOrder(arr[idx].right, arr, writer)
+}
+
+func traversePostOrder(root string, arr []node, writer Writer) {
+	if root == "." {
+		return
+	}
+	idx := root[0] - 'A'
+	traversePostOrder(arr[idx].left, arr, writer)
+	traversePostOrder(arr[idx].right, arr, writer)
+	_, _ = fmt.Fprint(writer, root)
+}
