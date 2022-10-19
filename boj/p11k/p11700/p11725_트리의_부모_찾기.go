0a05568bad525f939bcffa62a730fdb730451477
zrma <bulbitain@gmail.com>
2022-10-19 21:59:14 +0900

    boj: solve 11725 트리의 부모 찾기
    - https://www.acmicpc.net/problem/11725

---
+++ boj/p11k/p11700/p11725_트리의_부모_찾기.go
@@ -0,0 +1,42 @@
+package p11700
+
+import (
+	"fmt"
+)
+
+func Solve11725(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	graph := make([][]int, n+1)
+	parents := make([]int, n+1)
+	visited := make([]bool, n+1)
+
+	for i := 0; i < n; i++ {
+		var from, to int
+		_, _ = fmt.Fscan(reader, &from, &to)
+		graph[from] = append(graph[from], to)
+		graph[to] = append(graph[to], from)
+	}
+
+	var queue []int
+	queue = append(queue, 1)
+	visited[1] = true
+
+	for len(queue) > 0 {
+		p := queue[0]
+		queue = queue[1:]
+
+		for _, v := range graph[p] {
+			if !visited[v] {
+				parents[v] = p
+				visited[v] = true
+				queue = append(queue, v)
+			}
+		}
+	}
+
+	for i := 2; i <= n; i++ {
+		_, _ = fmt.Fprintln(writer, parents[i])
+	}
+}
