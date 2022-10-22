7a8e654b37d577a1f948ebdd54c3da325bd2a346
zrma <bulbitain@gmail.com>
2022-10-22 19:25:55 +0900

    boj: solve 1717 집합의 표현
    - https://www.acmicpc.net/problem/1717

---
+++ boj/p1k/p1700/p1717_집합의_표현_test.go
@@ -0,0 +1,51 @@
+package p1700_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1700"
+	"1d1go/utils"
+)
+
+func TestSolve1717(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1717")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`7 8
+0 1 3
+1 1 7
+0 7 6
+1 7 1
+0 3 7
+0 4 2
+0 1 1
+1 1 1`,
+			`NO
+NO
+YES
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1700.Solve1717(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
