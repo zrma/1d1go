f4b60905a50a5836616744655d0988d976a40606
zrma <bulbitain@gmail.com>
2022-10-23 20:47:07 +0900

    boj: solve 1976 여행 가자
    - https://www.acmicpc.net/problem/1976

---
+++ boj/p1k/p1900/p1976_여행_가자.go
@@ -0,0 +1,170 @@
+package p1900
+
+import (
+	"fmt"
+
+	"1d1go/utils/ds"
+)
+
+type solve1976Func func(int, int, Reader) bool
+
+func Solve1976(reader Reader, writer Writer, f solve1976Func) {
+	var n, m int
+	_, _ = fmt.Fscan(reader, &n, &m)
+
+	if f(n, m, reader) {
+		_, _ = fmt.Fprint(writer, "YES")
+	} else {
+		_, _ = fmt.Fprint(writer, "NO")
+	}
+}
+
+func Solve1976UnionFind(n, m int, reader Reader) bool {
+	uf := ds.NewUnionFind(n)
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			var x int
+			_, _ = fmt.Fscan(reader, &x)
+
+			if i <= j {
+				continue
+			}
+
+			if x == 0 {
+				continue
+			}
+
+			uf.Union(i, j)
+		}
+	}
+
+	var res int
+	for i := 0; i < m; i++ {
+		var a int
+		_, _ = fmt.Fscan(reader, &a)
+
+		if i == 0 {
+			res = uf.Find(a)
+		}
+		if res != uf.Find(a) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Solve1976DFS(n, m int, reader Reader) bool {
+	graph := make([][]int, n+1)
+	for i := 1; i <= n; i++ {
+		graph[i] = make([]int, 0, n)
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			var x int
+			_, _ = fmt.Fscan(reader, &x)
+
+			if i <= j {
+				continue
+			}
+
+			if x == 0 {
+				continue
+			}
+
+			graph[i] = append(graph[i], j)
+			graph[j] = append(graph[j], i)
+		}
+	}
+
+	var start int
+	_, _ = fmt.Fscan(reader, &start)
+
+	visited := make([]bool, n+1)
+	dfs1976(graph, visited, start)
+
+	for i := 1; i < m; i++ {
+		var a int
+		_, _ = fmt.Fscan(reader, &a)
+
+		if !visited[a] {
+			return false
+		}
+	}
+	return true
+}
+
+func dfs1976(graph [][]int, visited []bool, start int) {
+	visited[start] = true
+
+	for _, next := range graph[start] {
+		if visited[next] {
+			continue
+		}
+
+		dfs1976(graph, visited, next)
+	}
+}
+
+func Solve1976BFS(n, m int, reader Reader) bool {
+	graph := make([][]int, n+1)
+	for i := 1; i <= n; i++ {
+		graph[i] = make([]int, 0, n)
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			var x int
+			_, _ = fmt.Fscan(reader, &x)
+
+			if i <= j {
+				continue
+			}
+
+			if x == 0 {
+				continue
+			}
+
+			graph[i] = append(graph[i], j)
+			graph[j] = append(graph[j], i)
+		}
+	}
+
+	var start int
+	_, _ = fmt.Fscan(reader, &start)
+
+	visited := make([]bool, n+1)
+	bfs1976(graph, visited, start)
+
+	for i := 1; i < m; i++ {
+		var a int
+		_, _ = fmt.Fscan(reader, &a)
+
+		if !visited[a] {
+			return false
+		}
+	}
+	return true
+}
+
+func bfs1976(graph [][]int, visited []bool, start int) {
+	queue := make([]int, 0, len(graph))
+	queue = append(queue, start)
+
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+
+		visited[cur] = true
+
+		for _, next := range graph[cur] {
+			if visited[next] {
+				continue
+			}
+
+			queue = append(queue, next)
+		}
+	}
+}
---
+++ boj/p1k/p1900/p1976_여행_가자_test.go
@@ -0,0 +1,95 @@
+package p1900_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1900"
+	"1d1go/utils"
+)
+
+func TestSolve1976(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1976")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`3
+3
+0 1 0
+1 0 1
+0 1 0
+1 2 3`,
+			"YES",
+		},
+		{
+			`5
+3
+0 1 0 0 0
+1 0 1 0 0
+0 1 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+1 2 3
+`,
+			"YES",
+		},
+		{
+			`5
+4
+0 1 0 0 0
+1 0 1 0 0
+0 1 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+1 2 3 4
+`,
+			"NO",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d/UnionFind", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1900.Solve1976(reader, writer, p1900.Solve1976UnionFind)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+
+		t.Run(fmt.Sprintf("%d/DFS", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1900.Solve1976(reader, writer, p1900.Solve1976DFS)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+
+		t.Run(fmt.Sprintf("%d/BFS", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1900.Solve1976(reader, writer, p1900.Solve1976BFS)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
