95295954ac5b18dbb100e0bebd133018a1b862a4
zrma <bulbitain@gmail.com>
2022-10-25 21:28:01 +0900

    boj: solve 20040
    - https://www.acmicpc.net/problem/20040

---
+++ boj/p20k/p20000/p20040.go
@@ -0,0 +1,26 @@
+package p20000
+
+import (
+	"fmt"
+
+	"1d1go/utils/ds"
+)
+
+func Solve20040(reader Reader, writer Writer) {
+	var n, m int
+	_, _ = fmt.Fscan(reader, &n, &m)
+
+	uf := ds.NewUnionFind(n)
+
+	var res int
+	for i := 1; i <= m; i++ {
+		var a, b int
+		_, _ = fmt.Fscan(reader, &a, &b)
+
+		if uf.Find(a) == uf.Find(b) && res == 0 {
+			res = i
+		}
+		uf.Union(a, b)
+	}
+	_, _ = fmt.Fprint(writer, res)
+}
---
+++ boj/p20k/p20000/p20040_test.go
@@ -0,0 +1,63 @@
+package p20000_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p20k/p20000"
+	"1d1go/utils"
+)
+
+func TestSolve20040(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/20040")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`6 5
+0 1
+1 2
+2 3
+5 4
+0 4`,
+			"0",
+		},
+		{
+			`6 5
+0 1
+1 2
+1 3
+0 3
+4 5`,
+			"4",
+		},
+		{
+			`6 5
+0 1
+1 2
+1 3
+0 3
+2 3`,
+			"4",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p20000.Solve20040(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
