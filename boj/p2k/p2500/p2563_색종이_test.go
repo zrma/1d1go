f16a3b9b84369d7e8c87ff4b7b5430d0db003cb3
zrma <bulbitain@gmail.com>
2022-10-31 21:06:12 +0900

    boj: solve 2563 색종이
    - https://www.acmicpc.net/problem/2563

---
+++ boj/p2k/p2500/p2563_색종이_test.go
@@ -0,0 +1,43 @@
+package p2500_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p2k/p2500"
+	"1d1go/utils"
+)
+
+func TestSolve2563(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2563")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`3
+3 7
+15 7
+5 2`,
+			"260",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p2500.Solve2563(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
