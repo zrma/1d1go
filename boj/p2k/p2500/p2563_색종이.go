f3862ca1b15a1a62f705004842a0066c5e9cdc99
zrma <bulbitain@gmail.com>
2022-10-31 21:06:12 +0900

    boj: solve 2563 색종이
    - https://www.acmicpc.net/problem/2563

---
+++ boj/p2k/p2500/p2563_색종이.go
@@ -0,0 +1,39 @@
+package p2500
+
+import (
+	"fmt"
+)
+
+func Solve2563(reader Reader, writer Writer) {
+	const (
+		maxSize = 100
+		boxSize = 10
+	)
+	arr := make([][]bool, maxSize)
+	for i := 0; i < maxSize; i++ {
+		arr[i] = make([]bool, maxSize)
+	}
+
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	for i := 0; i < n; i++ {
+		var x, y int
+		_, _ = fmt.Fscan(reader, &x, &y)
+		for j := x; j < x+boxSize; j++ {
+			for k := y; k < y+boxSize; k++ {
+				arr[j][k] = true
+			}
+		}
+	}
+
+	var res int
+	for i := 0; i < maxSize; i++ {
+		for j := 0; j < maxSize; j++ {
+			if arr[i][j] {
+				res++
+			}
+		}
+	}
+	_, _ = fmt.Fprint(writer, res)
+}
