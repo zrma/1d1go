44f019fb08c41345f43fc5795d227067b1406e8d
zrma <bulbitain@gmail.com>
2022-11-01 22:31:08 +0900

    boj: solve 15828 Router
    - https://www.acmicpc.net/problem/15828

---
+++ boj/p15k/p15800/p15828_router.go
@@ -0,0 +1,40 @@
+package p15800
+
+import (
+	"fmt"
+)
+
+func Solve15828(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	queue := make([]int, 0, n)
+	for {
+		var v int
+		_, _ = fmt.Fscan(reader, &v)
+
+		if v == -1 {
+			break
+		}
+
+		if v == 0 {
+			queue = queue[1:]
+			continue
+		}
+
+		if len(queue) == n {
+			continue
+		}
+
+		queue = append(queue, v)
+	}
+
+	if len(queue) == 0 {
+		_, _ = fmt.Fprint(writer, "empty")
+		return
+	}
+
+	for _, v := range queue {
+		_, _ = fmt.Fprint(writer, v, " ")
+	}
+}
