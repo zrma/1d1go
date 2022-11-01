dac88b7cfdf6b531b104d1cf715fbe312bf581ab
zrma <bulbitain@gmail.com>
2022-11-01 22:31:08 +0900

    boj: solve 15828 Router
    - https://www.acmicpc.net/problem/15828

---
+++ boj/p15k/p15800/p15828_router_test.go
@@ -0,0 +1,79 @@
+package p15800_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p15k/p15800"
+	"1d1go/utils"
+)
+
+func TestSolve15828(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/15828")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`5
+1
+2
+0
+3
+4
+0
+5
+6
+0
+0
+-1`,
+			"5 6 ",
+		},
+		{
+			`1
+1
+2
+3
+4
+5
+6
+7
+-1`,
+			"1 ",
+		},
+		{
+			`1
+1
+2
+0
+3
+4
+0
+5
+6
+0
+7
+0
+-1`,
+			"empty",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p15800.Solve15828(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
