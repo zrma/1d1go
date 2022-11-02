02ce4f22035bb4add3c2d26350bafcf244c54fbb
zrma <bulbitain@gmail.com>
2022-11-02 23:38:59 +0900

    boj: solve 2440 별 찍기 - 3
    - https://www.acmicpc.net/problem/2440

---
+++ boj/p2k/p2400/p2440_별_찍기-3_test.go
@@ -0,0 +1,45 @@
+package p2400_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p2k/p2400"
+	"1d1go/utils"
+)
+
+func TestSolve2440(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2440")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			"5",
+			`*****
+****
+***
+**
+*
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p2400.Solve2440(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
