00ce623d16d1b15a2eee7f9ce120674af26ad81a
zrma <bulbitain@gmail.com>
2022-11-04 20:49:19 +0900

    boj: solve 2752 세수정렬
    - https://www.acmicpc.net/problem/2752

---
+++ boj/p2k/p2700/p2752_세수정렬_test.go
@@ -0,0 +1,48 @@
+package p2700_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p2k/p2700"
+	"1d1go/utils"
+)
+
+func TestSolve2752(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2752")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			"3 1 2",
+			"1 2 3",
+		},
+		{
+			"1 2 3",
+			"1 2 3",
+		},
+		{
+			"3 2 1",
+			"1 2 3",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p2700.Solve2752(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
