da535da1ee659cf443caf56327fe9fbfdd41b9cf
zrma <bulbitain@gmail.com>
2022-11-05 16:16:50 +0900

    boj: solve 25682 체스판 다시 칠하기 2
    - https://www.acmicpc.net/problem/25682

---
+++ boj/p25k/p25600/p25682_체스판_다시_칠하기_2.go
@@ -0,0 +1,62 @@
+package p25600
+
+import (
+	"fmt"
+)
+
+func Solve25682(reader Reader, writer Writer) {
+	var n, m, k int
+	_, _ = fmt.Fscan(reader, &n, &m, &k)
+
+	board0 := make([][]int, n+1)
+	board1 := make([][]int, n+1)
+	for i := range board0 {
+		board0[i] = make([]int, m+1)
+		board1[i] = make([]int, m+1)
+	}
+
+	const (
+		w byte = 'W'
+		b byte = 'B'
+	)
+
+	var (
+		startWant, startOther = w, b
+	)
+
+	for i := 1; i <= n; i++ {
+		var s string
+		_, _ = fmt.Fscan(reader, &s)
+
+		startWant, startOther = startOther, startWant
+		want, other := startWant, startOther
+
+		for j := 1; j <= m; j++ {
+			want, other = other, want
+
+			if s[j-1] == want {
+				board0[i][j] = board0[i][j-1] + board0[i-1][j] - board0[i-1][j-1]
+				board1[i][j] = board1[i][j-1] + board1[i-1][j] - board1[i-1][j-1] + 1
+			} else {
+				board0[i][j] = board0[i][j-1] + board0[i-1][j] - board0[i-1][j-1] + 1
+				board1[i][j] = board1[i][j-1] + board1[i-1][j] - board1[i-1][j-1]
+			}
+		}
+	}
+
+	res := m * n
+	for i := k; i <= n; i++ {
+		for j := k; j <= m; j++ {
+			diff0 := board0[i][j] - board0[i-k][j] - board0[i][j-k] + board0[i-k][j-k]
+			diff1 := board1[i][j] - board1[i-k][j] - board1[i][j-k] + board1[i-k][j-k]
+
+			if res > diff0 {
+				res = diff0
+			}
+			if res > diff1 {
+				res = diff1
+			}
+		}
+	}
+	_, _ = fmt.Fprint(writer, res)
+}
---
+++ boj/p25k/p25600/p25682_체스판_다시_칠하기_2_test.go
@@ -0,0 +1,116 @@
+package p25600_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p25k/p25600"
+	"1d1go/utils"
+)
+
+func TestSolve25682(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/25682")
+
+	//goland:noinspection SpellCheckingInspection
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`4 4 3
+BWBW
+WBWB
+BWBW
+WBWB`,
+			"0",
+		},
+		{
+			`4 4 3
+WBWB
+BWBW
+WBWB
+BWBW`,
+			"0",
+		},
+		{
+			`4 4 3
+WWWB
+BWBW
+WBWB
+BWBW`,
+			"0",
+		},
+		{
+			`4 4 3
+WBWB
+BWBW
+WBWB
+BWBB`,
+			"0",
+		},
+		{
+			`4 4 3
+BBBB
+BBBB
+BBBW
+BBWB`,
+			"2",
+		},
+		{
+			`8 8 8
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBBBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBWBWBW`,
+			"1",
+		},
+		{
+			`10 13 10
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+WWWWWWWWWWBWB
+WWWWWWWWWWBWB`,
+			"30",
+		},
+		{
+			`9 23 9
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBWWWWWWWW`,
+			"40",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p25600.Solve25682(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
