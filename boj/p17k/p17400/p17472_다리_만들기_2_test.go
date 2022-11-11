318eda0f799ddfbae8b7ac5471883799d12f0792
zrma <bulbitain@gmail.com>
2022-11-11 23:37:48 +0900

    boj: solve 17472 다리 만들기 2
    - https://www.acmicpc.net/problem/17472

---
+++ boj/p17k/p17400/p17472_다리_만들기_2_test.go
@@ -0,0 +1,80 @@
+package p17400_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p17k/p17400"
+	"1d1go/utils"
+)
+
+func TestSolve17472(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/17472")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`7 8
+0 0 0 0 0 0 1 1
+1 1 0 0 0 0 1 1
+1 1 0 0 0 0 0 0
+1 1 0 0 0 1 1 0
+0 0 0 0 0 1 1 0
+0 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1`,
+			"9",
+		},
+		{
+			`7 8
+0 0 0 1 1 0 0 0
+0 0 0 1 1 0 0 0
+1 1 0 0 0 0 1 1
+1 1 0 0 0 0 1 1
+1 1 0 0 0 0 0 0
+0 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1`,
+			"10",
+		},
+		{
+			`7 8
+1 0 0 1 1 1 0 0
+0 0 1 0 0 0 1 1
+0 0 1 0 0 0 1 1
+0 0 1 1 1 0 0 0
+0 0 0 0 0 0 0 0
+0 1 1 1 0 0 0 0
+1 1 1 1 1 1 0 0`,
+			"9",
+		},
+		{
+			`7 7
+1 1 1 0 1 1 1
+1 1 1 0 1 1 1
+1 1 1 0 1 1 1
+0 0 0 0 0 0 0
+1 1 1 0 1 1 1
+1 1 1 0 1 1 1
+1 1 1 0 1 1 1`,
+			"-1",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p17400.Solve17472(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
