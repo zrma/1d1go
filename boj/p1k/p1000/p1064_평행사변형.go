809dfafd24c64ef799fe59e34ba025118dd32a89
zrma <bulbitain@gmail.com>
2022-11-12 22:49:42 +0900

    boj: solve 1064 평행사변형
    - https://www.acmicpc.net/problem/1064

---
+++ boj/p1k/p1000/p1064_평행사변형.go
@@ -0,0 +1,44 @@
+package p1000
+
+import (
+	"fmt"
+	"math"
+)
+
+func Solve1064(reader Reader, writer Writer) {
+	var ax, ay, bx, by, cx, cy float64
+	_, _ = fmt.Fscan(reader, &ax, &ay, &bx, &by, &cx, &cy)
+
+	ab := distance(ax, ay, bx, by)
+	bc := distance(bx, by, cx, cy)
+	ca := distance(cx, cy, ax, ay)
+
+	if ab == 0 || bc == 0 || ca == 0 {
+		_, _ = fmt.Fprint(writer, -1)
+		return
+	}
+
+	if slope(ax, ay, bx, by) == slope(bx, by, cx, cy) ||
+		slope(bx, by, cx, cy) == slope(cx, cy, ax, ay) ||
+		slope(cx, cy, ax, ay) == slope(ax, ay, bx, by) {
+		_, _ = fmt.Fprint(writer, -1)
+		return
+	}
+
+	length0 := 2*ab + 2*bc
+	length1 := 2*bc + 2*ca
+	length2 := 2*ca + 2*ab
+
+	min := math.Min(math.Min(length0, length1), length2)
+	max := math.Max(math.Max(length0, length1), length2)
+
+	_, _ = fmt.Fprint(writer, max-min)
+}
+
+func distance(x0, y0, x1, y1 float64) float64 {
+	return math.Sqrt(math.Pow(x0-x1, 2) + math.Pow(y0-y1, 2))
+}
+
+func slope(x0, y0, x1, y1 float64) float64 {
+	return (y1 - y0) / (x1 - x0)
+}
