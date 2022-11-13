af6ba3ff588e1f1f9c36428393956a64b01065c0
zrma <bulbitain@gmail.com>
2022-11-13 14:35:19 +0900

    boj: solve 15681 트리와 쿼리
    - https://www.acmicpc.net/problem/15681

---
+++ boj/p15k/p15600/p15681_트리와_쿼리_test.go
@@ -0,0 +1,54 @@
+package p15600_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p15k/p15600"
+	"1d1go/utils"
+)
+
+func TestSolve15681(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/15681")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`9 5 3
+1 3
+4 3
+5 4
+5 6
+6 7
+2 3
+9 6
+6 8
+5
+4
+8`,
+			`9
+4
+1
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p15600.Solve15681(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
