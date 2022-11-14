46069cc551b40137ef25f86648e8da2deb4c2242
zrma <bulbitain@gmail.com>
2022-11-14 22:18:56 +0900

    boj: solve 2213 트리의 독립집
    - https://www.acmicpc.net/problem/2213

---
+++ boj/p2k/p2200/p2213_트리의_독립집합_test.go
@@ -0,0 +1,70 @@
+package p2200_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p2k/p2200"
+	"1d1go/utils"
+)
+
+func TestSolve2213(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2213")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`7
+10 30 40 10 20 20 70
+1 2
+2 3
+4 3
+4 5
+6 2
+6 7`,
+			`140
+1 3 5 7 `,
+		},
+		{
+			`6
+10 1 1 1 10 10
+1 2
+2 3
+3 4
+4 5
+3 6`,
+			`30
+1 5 6 `,
+		},
+		{
+			`6
+10 1 1 1 10 10
+6 3
+5 4
+4 3
+3 2
+2 1`,
+			`30
+1 5 6 `,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p2200.Solve2213(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
