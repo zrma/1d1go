c79d121fc404244ea24bc2e920ae90387bb129ab
zrma <bulbitain@gmail.com>
2022-11-16 21:03:32 +0900

    boj: solve 1949 우수 마을
    - https://www.acmicpc.net/problem/1949

---
+++ boj/p1k/p1900/p1949_우수_마을.go
@@ -0,0 +1,47 @@
+package p1900
+
+import (
+	"fmt"
+
+	"1d1go/utils/integer"
+)
+
+func Solve1949(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	people := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		_, _ = fmt.Fscan(reader, &people[i])
+	}
+
+	adj := make([][]int, n+1)
+	for i := 1; i <= n-1; i++ {
+		var from, to int
+		_, _ = fmt.Fscan(reader, &from, &to)
+
+		adj[from] = append(adj[from], to)
+		adj[to] = append(adj[to], from)
+	}
+
+	dp := make([][2]int, n+1)
+	getDp(adj, dp, people, 1, 0)
+
+	_, _ = fmt.Fprint(writer, integer.Max(dp[1][0], dp[1][1]))
+}
+
+func getDp(adj [][]int, dp [][2]int, people []int, cur, parent int) {
+	dp[cur][0] = 0 // not contain
+	dp[cur][1] = people[cur]
+
+	for _, next := range adj[cur] {
+		if next == parent {
+			continue
+		}
+
+		getDp(adj, dp, people, next, cur)
+
+		dp[cur][0] += integer.Max(dp[next][0], dp[next][1]) // cur 빼면 next (포함/빼는거) 중 큰 것
+		dp[cur][1] += dp[next][0]                           // cur 넣으면 next 무조건 뺌
+	}
+}
---
+++ boj/p1k/p1900/p1949_우수_마을_test.go
@@ -0,0 +1,47 @@
+package p1900_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1900"
+	"1d1go/utils"
+)
+
+func TestSolve1949(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1949")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`7
+1000 3000 4000 1000 2000 2000 7000
+1 2
+2 3
+4 3
+4 5
+6 2
+6 7`,
+			"14000",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1900.Solve1949(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
