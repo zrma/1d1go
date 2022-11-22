b7851048fa83fc43760d8b344d1925fd32f07ad6
zrma <bulbitain@gmail.com>
2022-11-22 23:07:42 +0900

    boj: solve 2407 조합
    - https://www.acmicpc.net/problem/2407

---
+++ boj/p2k/p2400/p2407_조합.go
@@ -0,0 +1,46 @@
+package p2400
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func Solve2407(reader Reader, writer Writer) {
+	var n, m big.Int
+	_, _ = fmt.Fscan(reader, &n, &m)
+
+	const maxLen = 1000 + 1
+	cache := make([][]*big.Int, maxLen)
+	for i := range cache {
+		cache[i] = make([]*big.Int, maxLen)
+	}
+
+	res := combination(&n, &m, cache)
+	_, _ = fmt.Fprint(writer, res.String())
+}
+
+func combination(n *big.Int, m *big.Int, cache [][]*big.Int) *big.Int {
+	if n.Cmp(m) < 0 {
+		return big.NewInt(0)
+	}
+	if n.Cmp(m) == 0 {
+		return big.NewInt(1)
+	}
+	if m.Cmp(big.NewInt(0)) == 0 {
+		return big.NewInt(1)
+	}
+	if m.Cmp(big.NewInt(1)) == 0 {
+		return n
+	}
+
+	if cache[n.Int64()][m.Int64()] != nil {
+		return cache[n.Int64()][m.Int64()]
+	}
+
+	res := big.NewInt(0)
+	res = res.Add(res, combination(new(big.Int).Sub(n, big.NewInt(1)), new(big.Int).Sub(m, big.NewInt(1)), cache))
+	res = res.Add(res, combination(new(big.Int).Sub(n, big.NewInt(1)), m, cache))
+	cache[n.Int64()][m.Int64()] = res
+
+	return res
+}
