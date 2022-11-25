3c8201b135744d70a79545d398cc564937b2d305
zrma <bulbitain@gmail.com>
2022-11-25 22:50:18 +0900

    boj: solve 2845 파티가 끝나고 난 뒤
    - https://www.acmicpc.net/problem/2845

---
+++ boj/p2k/p2800/p2845_파티가_끝나고_난_뒤.go
@@ -0,0 +1,21 @@
+package p2800
+
+import (
+	"fmt"
+)
+
+func Solve2845(reader Reader, writer Writer) {
+	var l, p int
+	_, _ = fmt.Fscan(reader, &l, &p)
+
+	tot := l * p
+
+	var arr [5]int
+	for i := range arr {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+
+	for _, v := range arr {
+		_, _ = fmt.Fprintf(writer, "%d ", v-tot)
+	}
+}
