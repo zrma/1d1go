b383294a27f52264885789aa4925d1c0d8580b3a
zrma <bulbitain@gmail.com>
2022-11-28 22:56:36 +0900

    boj: solve 11403 경로 찾기
    - https://www.acmicpc.net/problem/11403

---
+++ boj/p11k/p11400/p11403_경로_찾기_test.go
@@ -0,0 +1,64 @@
+package p11400_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p11k/p11400"
+	"1d1go/utils"
+)
+
+func TestSolve11403(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/11403")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`3
+0 1 0
+0 0 1
+1 0 0`,
+			`1 1 1
+1 1 1
+1 1 1
+`,
+		},
+		{
+			`7
+0 0 0 1 0 0 0
+0 0 0 0 0 0 1
+0 0 0 0 0 0 0
+0 0 0 0 1 1 0
+1 0 0 0 0 0 0
+0 0 0 0 0 0 1
+0 0 1 0 0 0 0`,
+			`1 0 1 1 1 1 1
+0 0 1 0 0 0 1
+0 0 0 0 0 0 0
+1 0 1 1 1 1 1
+1 0 1 1 1 1 1
+0 0 1 0 0 0 1
+0 0 1 0 0 0 0
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p11400.Solve11403(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
