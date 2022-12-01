c913d154cd7c4517069b44f2ed9129fb069988ae
zrma <bulbitain@gmail.com>
2022-12-01 22:08:25 +0900

    boj: solve 14500 테트로미노
    - https://www.acmicpc.net/problem/14500

---
+++ boj/p14k/p14500/p14500_테트로미노_test.go
@@ -0,0 +1,61 @@
+package p14500_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p14k/p14500"
+	"1d1go/utils"
+)
+
+func TestSolve14500(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/14500")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`5 5
+1 2 3 4 5
+5 4 3 2 1
+2 3 4 5 6
+6 5 4 3 2
+1 2 1 2 1`,
+			"19",
+		},
+		{
+			`4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5`,
+			"20",
+		},
+		{
+			`4 10
+1 2 1 2 1 2 1 2 1 2
+2 1 2 1 2 1 2 1 2 1
+1 2 1 2 1 2 1 2 1 2
+2 1 2 1 2 1 2 1 2 1`,
+			"7",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p14500.Solve14500(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
