93b29b9e0cf932823ba9fc0985379e90c4d900d4
zrma <bulbitain@gmail.com>
2022-12-01 22:08:25 +0900

    boj: solve 14500 테트로미노
    - https://www.acmicpc.net/problem/14500

---
+++ boj/p14k/p14500/p14500_테트로미노.go
@@ -0,0 +1,142 @@
+package p14500
+
+import (
+	"fmt"
+)
+
+func Solve14500(reader Reader, writer Writer) {
+	var n, m int
+	_, _ = fmt.Fscan(reader, &n, &m)
+
+	board := make([][]int, n)
+	for i := 0; i < n; i++ {
+		board[i] = make([]int, m)
+		for j := 0; j < m; j++ {
+			_, _ = fmt.Fscan(reader, &board[i][j])
+		}
+	}
+
+	max := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			area := calcArea(board, i, j, n, m)
+			if area > max {
+				max = area
+			}
+		}
+	}
+	_, _ = fmt.Fprint(writer, max)
+}
+
+func calcArea(board [][]int, i, j, n, m int) int {
+	max := 0
+	for _, block := range blocks {
+		row := len(block)
+		col := len(block[0])
+
+		if i+row > n || j+col > m {
+			continue
+		}
+
+		sum := 0
+		for r := 0; r < row; r++ {
+			for c := 0; c < col; c++ {
+				if block[r][c] == 1 {
+					sum += board[i+r][j+c]
+				}
+			}
+		}
+		if sum > max {
+			max = sum
+		}
+	}
+	return max
+}
+
+var blocks = [19][][]int{
+	{
+		{1, 1, 1, 1},
+	},
+	{
+		{1},
+		{1},
+		{1},
+		{1},
+	},
+	{
+		{1, 1},
+		{1, 1},
+	},
+	{
+		{1, 1, 1},
+		{1, 0, 0},
+	},
+	{
+		{1, 1},
+		{0, 1},
+		{0, 1},
+	},
+	{
+		{0, 0, 1},
+		{1, 1, 1},
+	},
+	{
+		{1, 0},
+		{1, 0},
+		{1, 1},
+	},
+	{
+		{1, 1, 1},
+		{0, 0, 1},
+	},
+	{
+		{0, 1},
+		{0, 1},
+		{1, 1},
+	},
+	{
+		{1, 0, 0},
+		{1, 1, 1},
+	},
+	{
+		{1, 1},
+		{1, 0},
+		{1, 0},
+	},
+	{
+		{1, 1, 1},
+		{0, 1, 0},
+	},
+	{
+		{0, 1},
+		{1, 1},
+		{0, 1},
+	},
+	{
+		{0, 1, 0},
+		{1, 1, 1},
+	},
+	{
+		{1, 0},
+		{1, 1},
+		{1, 0},
+	},
+	{
+		{1, 0},
+		{1, 1},
+		{0, 1},
+	},
+	{
+		{0, 1, 1},
+		{1, 1, 0},
+	},
+	{
+		{0, 1},
+		{1, 1},
+		{1, 0},
+	},
+	{
+		{1, 1, 0},
+		{0, 1, 1},
+	},
+}
