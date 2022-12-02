8a7ac20f70cfeb61d6551de7f210e33745f6e5ab
zrma <bulbitain@gmail.com>
2022-12-03 01:43:26 +0900

    boj: solve 4299 AFC 윔블던
    - https://www.acmicpc.net/problem/4299

---
+++ boj/p4k/p4200/p4299_AFC_윔블던.go
@@ -0,0 +1,22 @@
+package p4200
+
+import "fmt"
+
+func Solve4299(reader Reader, writer Writer) {
+	var sum, diff int
+	_, _ = fmt.Fscan(reader, &sum, &diff)
+
+	if sum < diff {
+		_, _ = fmt.Fprint(writer, -1)
+		return
+	}
+
+	a := (sum + diff) / 2
+	b := (sum - diff) / 2
+
+	if a+b != sum || a-b != diff {
+		_, _ = fmt.Fprint(writer, -1)
+		return
+	}
+	_, _ = fmt.Fprintf(writer, "%d %d", a, b)
+}
