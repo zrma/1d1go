1f074fb705fe8871cbba6e788c33785e84586c7e
zrma <bulbitain@gmail.com>
2022-12-06 21:56:08 +0900

    boj: solve 1069 집으로
    - https://www.acmicpc.net/problem/1069

---
+++ boj/p1k/p1000/p1069_집으로_test.go
@@ -0,0 +1,48 @@
+package p1000_test
+
+import (
+	"1d1go/boj/p1k/p1000"
+	"1d1go/utils"
+	"bufio"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestSolve1069(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1069")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{"6 8 5 3", "6.0"},
+		{"3 4 6 3", "4.0"},
+		{"318 445 1200 800", "546.9451526432975"},
+		{"400 300 150 10", "40.0"},
+		{"6 8 3 2", "7.0"},
+		{"10 10 1000 5", "10.0"},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1000.Solve1069(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			want, err := strconv.ParseFloat(tt.want, 64)
+			assert.NoError(t, err)
+
+			writen := writer.String()
+			got, err := strconv.ParseFloat(writen, 64)
+			assert.NoError(t, err)
+
+			const epsilon = 1e-9
+			assert.InDelta(t, want, got, epsilon)
+		})
+	}
+}
