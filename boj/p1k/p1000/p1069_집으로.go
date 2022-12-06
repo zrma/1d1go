06b2718c97177dba53ccaed729e499cb8048c06c
zrma <bulbitain@gmail.com>
2022-12-06 21:56:08 +0900

    boj: solve 1069 집으로
    - https://www.acmicpc.net/problem/1069

---
+++ boj/p1k/p1000/p1069_집으로.go
@@ -0,0 +1,25 @@
+package p1000
+
+import (
+	"1d1go/utils/integer"
+	"fmt"
+	"math"
+)
+
+func Solve1069(reader Reader, writer Writer) {
+	var x, y, d, t int
+	_, _ = fmt.Fscan(reader, &x, &y, &d, &t)
+
+	dist := math.Sqrt(float64(x*x + y*y))
+
+	// 한 번도 안 뛰었을 때
+	walkTime := dist
+
+	// 최소한 1번은 뛰었을 때
+	jumpCnt := integer.Max(int(dist)/d, 1)
+
+	jumAndWalkTime := float64(jumpCnt)*float64(t) + math.Abs(dist-float64(jumpCnt*d))
+	jumpOnlyTime := float64(t * (jumpCnt + 1)) // 정확하게 점프로 골인하기 위해 마지막에 점프 한 번 더 함
+
+	_, _ = fmt.Fprintf(writer, "%0.9f", math.Min(walkTime, math.Min(jumAndWalkTime, jumpOnlyTime)))
+}
