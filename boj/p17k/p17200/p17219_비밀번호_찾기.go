86a287f55e521952001995e980545f1ec96591fa
zrma <bulbitain@gmail.com>
2022-12-07 21:01:55 +0900

    boj: solve 17219 비밀번호 찾기
    - https://www.acmicpc.net/problem/17219

---
+++ boj/p17k/p17200/p17219_비밀번호_찾기.go
@@ -0,0 +1,21 @@
+package p17200
+
+import "fmt"
+
+func Solve17219(reader Reader, writer Writer) {
+	var n, m int
+	_, _ = fmt.Fscan(reader, &n, &m)
+
+	passwords := make(map[string]string)
+	for i := 0; i < n; i++ {
+		var site, password string
+		_, _ = fmt.Fscan(reader, &site, &password)
+		passwords[site] = password
+	}
+
+	for i := 0; i < m; i++ {
+		var site string
+		_, _ = fmt.Fscan(reader, &site)
+		_, _ = fmt.Fprintln(writer, passwords[site])
+	}
+}
