2cba3ecbd65e3990b244df44e28b153c4ffff0e7
zrma <bulbitain@gmail.com>
2022-12-08 23:22:44 +0900

    boj: solve 15654 N과 M (5)
    - https://www.acmicpc.net/problem/15654

---
+++ boj/p15k/p15600/p15654_N과_M_(5)_test.go
@@ -0,0 +1,88 @@
+package p15600_test
+
+import (
+	"1d1go/boj/p15k/p15600"
+	"1d1go/utils"
+	"bufio"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestSolve15654(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/15654")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`3 1
+4 5 2`,
+			`2
+4
+5
+`,
+		},
+		{
+			`4 2
+9 8 7 1`,
+			`1 7
+1 8
+1 9
+7 1
+7 8
+7 9
+8 1
+8 7
+8 9
+9 1
+9 7
+9 8
+`,
+		},
+		{
+			`4 4
+1231 1232 1233 1234`,
+			`1231 1232 1233 1234
+1231 1232 1234 1233
+1231 1233 1232 1234
+1231 1233 1234 1232
+1231 1234 1232 1233
+1231 1234 1233 1232
+1232 1231 1233 1234
+1232 1231 1234 1233
+1232 1233 1231 1234
+1232 1233 1234 1231
+1232 1234 1231 1233
+1232 1234 1233 1231
+1233 1231 1232 1234
+1233 1231 1234 1232
+1233 1232 1231 1234
+1233 1232 1234 1231
+1233 1234 1231 1232
+1233 1234 1232 1231
+1234 1231 1232 1233
+1234 1231 1233 1232
+1234 1232 1231 1233
+1234 1232 1233 1231
+1234 1233 1231 1232
+1234 1233 1232 1231
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p15600.Solve15654(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
