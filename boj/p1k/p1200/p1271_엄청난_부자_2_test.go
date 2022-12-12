d13af5aca28578c7ae056d6a4141e9c7c66a0e19
zrma <bulbitain@gmail.com>
2022-12-12 22:38:34 +0900

    boj: solve 1271 엄청난 부자 2
    - https://www.acmicpc.net/problem/1271

---
+++ boj/p1k/p1200/p1271_엄청난_부자_2_test.go
@@ -0,0 +1,54 @@
+package p1200_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1200"
+	"1d1go/utils"
+)
+
+func TestSolve1271(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1271")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`1000 100`,
+			`10
+0
+`,
+		},
+		{
+			`1000000000000000000 1000000000000000000`,
+			`1
+0
+`,
+		},
+		{
+			`1000000000000000000000000000000000000000000000000000000 1000000000000000000000000000000000000000000000000000000`,
+			`1
+0
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1200.Solve1271(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
