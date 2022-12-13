f6859801be9195ab8de0fd30f91786818a0559af
zrma <bulbitain@gmail.com>
2022-12-13 22:34:31 +0900

    boj: solve 1238 파티
    - https://www.acmicpc.net/problem/1238

---
+++ boj/p1k/p1200/p1238_파티_test.go
@@ -0,0 +1,48 @@
+package p1200_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1200"
+	"1d1go/utils"
+)
+
+func TestSolve1238(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1238")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`4 8 2
+1 2 4
+1 3 2
+1 4 7
+2 1 1
+2 3 5
+3 1 2
+3 4 4
+4 2 3`,
+			"10",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			writer := utils.NewStringWriter()
+
+			p1200.Solve1238(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := writer.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
