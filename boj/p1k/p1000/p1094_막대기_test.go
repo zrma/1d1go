040bf56da44ecd4d748df0c77d388ba7747314f2
zrma <bulbitain@gmail.com>
2022-12-18 00:34:24 +0900

    boj: solve 1094 막대기
    - https://www.acmicpc.net/problem/1094

---
+++ boj/p1k/p1000/p1094_막대기_test.go
@@ -0,0 +1,54 @@
+package p1000_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1000"
+)
+
+func TestSolve1094(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1094")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{"23", "4"},
+		{"32", "1"},
+		{"64", "1"},
+		{"48", "2"},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			p1000.Solve1094(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+
+		t.Run(fmt.Sprintf("%d/BitCount", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			p1000.Solve1094WithBitCount(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
