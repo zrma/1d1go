c0f53ded0602eec83ef78c2afdbfeb33a0c8aa9b
zrma <bulbitain@gmail.com>
2022-12-19 21:01:29 +0900

    boj: solve 6497 전력난
    - https://www.acmicpc.net/problem/6497

---
+++ boj/p6k/p6400/p6497_전력난_test.go
@@ -0,0 +1,79 @@
+package p6400_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p6k/p6400"
+)
+
+func TestSolve6497(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/6497")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`7 11
+0 1 7
+0 3 5
+1 2 8
+1 3 9
+1 4 7
+2 4 5
+3 4 15
+3 5 6
+4 5 8
+4 6 9
+5 6 11
+7 11
+0 1 7
+0 3 5
+1 2 8
+1 3 9
+1 4 7
+2 4 5
+3 4 15
+3 5 6
+4 5 8
+4 6 9
+5 6 11
+7 11
+0 1 7
+0 3 5
+1 2 8
+1 3 9
+1 4 7
+2 4 5
+3 4 15
+3 5 6
+4 5 8
+4 6 9
+5 6 11
+0 0`,
+			`51
+51
+51
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			p6400.Solve6497(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
