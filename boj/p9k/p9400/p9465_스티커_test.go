1800ddf0b8a497d3e7bf6582d3e9c82b65385664
zrma <bulbitain@gmail.com>
2022-12-20 23:02:30 +0900

    boj: solve 9465 스티커
    - https://www.acmicpc.net/problem/9465

---
+++ boj/p9k/p9400/p9465_스티커_test.go
@@ -0,0 +1,48 @@
+package p9400_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p9k/p9400"
+)
+
+func TestSolve9465(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/9465")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`2
+5
+50 10 100 20 40
+30 50 70 10 60
+7
+10 30 10 50 100 20 40
+20 40 30 50 60 20 80`,
+			`260
+290
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			p9400.Solve9465(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
