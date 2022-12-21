6ec4008fd117edbe7f57efd1e02042402985d645
zrma <bulbitain@gmail.com>
2022-12-21 21:34:51 +0900

    boj: solve 1918 후위 표기식
    - https://www.acmicpc.net/problem/1918

---
+++ boj/p1k/p1900/p1918_후위_표기식.go
@@ -0,0 +1,50 @@
+package p1900
+
+import (
+	"fmt"
+)
+
+func Solve1918(reader Reader, writer Writer) {
+	var s string
+	_, _ = fmt.Fscan(reader, &s)
+
+	// infix to postfix
+	stack := make([]byte, 0, len(s))
+	res := make([]byte, 0, len(s))
+	for _, c := range s {
+		switch c {
+		case '(':
+			stack = append(stack, '(')
+		case ')':
+			for len(stack) > 0 && stack[len(stack)-1] != '(' {
+				res = append(res, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			stack = stack[:len(stack)-1]
+		case '+', '-', '*', '/':
+			for len(stack) > 0 && priority(stack[len(stack)-1]) >= priority(byte(c)) {
+				res = append(res, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			stack = append(stack, byte(c))
+		default:
+			res = append(res, byte(c))
+		}
+	}
+	for len(stack) > 0 {
+		res = append(res, stack[len(stack)-1])
+		stack = stack[:len(stack)-1]
+	}
+	_, _ = fmt.Fprint(writer, string(res))
+}
+
+func priority(c byte) int {
+	switch c {
+	case '+', '-':
+		return 1
+	case '*', '/':
+		return 2
+	default:
+		return 0
+	}
+}
