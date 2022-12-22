9469aea904ab1ef337e717e2485a46d780f5055d
zrma <bulbitain@gmail.com>
2022-12-22 23:02:51 +0900

    boj: solve 1865 웜홀
    - https://www.acmicpc.net/problem/1865

---
+++ boj/p1k/p1800/p1865_웜홀.go
@@ -0,0 +1,65 @@
+package p1800
+
+import (
+	"fmt"
+)
+
+func Solve1865(reader Reader, writer Writer) {
+	var tc int
+	_, _ = fmt.Fscan(reader, &tc)
+
+	for ; tc > 0; tc-- {
+		var n, m, w int
+		_, _ = fmt.Fscan(reader, &n, &m, &w)
+
+		edges := make([]edge, 0, 2*m+w)
+		for i := 0; i < m; i++ {
+			var u, v, c int64
+			_, _ = fmt.Fscan(reader, &u, &v, &c)
+			edges = append(edges, edge{u: u, v: v, w: c})
+			edges = append(edges, edge{u: v, v: u, w: c})
+		}
+
+		for i := 0; i < w; i++ {
+			var u, v, c int64
+			_, _ = fmt.Fscan(reader, &u, &v, &c)
+			edges = append(edges, edge{u: u, v: v, w: -c})
+		}
+
+		if hasCycle(n, edges) {
+			_, _ = fmt.Fprintln(writer, "YES")
+		} else {
+			_, _ = fmt.Fprintln(writer, "NO")
+		}
+	}
+}
+
+type edge struct {
+	u, v, w int64
+}
+
+const inf = 10_001
+
+func hasCycle(n int, edges []edge) bool {
+	dist := make([]int64, n+1)
+	for i := range dist {
+		dist[i] = inf
+	}
+	dist[1] = 0
+
+	for i := 0; i < n; i++ {
+		for _, e := range edges {
+			from := e.u
+			to := e.v
+			cost := e.w
+			if dist[to] > dist[from]+cost {
+				dist[to] = dist[from] + cost
+
+				if i == n-1 {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
---
+++ boj/p1k/p1800/p1865_웜홀_test.go
@@ -0,0 +1,79 @@
+package p1800_test
+
+import (
+	"bufio"
+	_ "embed"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1800"
+)
+
+//go:embed test_data/p1865_give.txt
+var p1865give string
+
+func TestSolve1865(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1865")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`2
+3 3 1
+1 2 2
+1 3 4
+2 3 1
+3 1 3
+3 2 1
+1 2 3
+2 3 4
+3 1 8`,
+			`NO
+YES
+`,
+		},
+		{
+			`1
+500 1 1
+333 444 1
+333 444 2`,
+			`YES
+`,
+		},
+		{
+			`1
+4 3 1
+1 2 1
+2 1 1
+3 4 2
+4 3 1`,
+			`NO
+`,
+		},
+		{
+			p1865give,
+			`NO
+NO
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			p1800.Solve1865(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
