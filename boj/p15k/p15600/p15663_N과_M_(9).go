35b04a023308a4e18a8ec2a5003f9962c28d8262
zrma <bulbitain@gmail.com>
2022-12-25 23:40:01 +0900

    boj: solve 15663 N과 M (9)
    - https://www.acmicpc.net/problem/15663

---
+++ boj/p15k/p15600/p15663_N과_M_(9).go
@@ -0,0 +1,49 @@
+package p15600
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve15663(reader Reader, writer Writer) {
+	var n, m int
+	_, _ = fmt.Fscan(reader, &n, &m)
+
+	arr := make([]int, n)
+	for i := range arr {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+
+	sort.Ints(arr)
+
+	visited := make([]bool, n)
+	res := make([]int, m)
+
+	permutationDFS15663(arr, visited, res, 0, n, m, writer)
+}
+
+func permutationDFS15663(arr []int, visited []bool, res []int, depth, n, m int, writer Writer) {
+	if depth == m {
+		for i, v := range res {
+			_, _ = fmt.Fprint(writer, v)
+			if i != m-1 {
+				_, _ = fmt.Fprint(writer, " ")
+			}
+		}
+		_, _ = fmt.Fprintln(writer)
+		return
+	}
+
+	prev := -1
+	for i := 0; i < n; i++ {
+		if visited[i] || prev == arr[i] {
+			continue
+		}
+
+		visited[i] = true
+		res[depth] = arr[i]
+		permutationDFS15663(arr, visited, res, depth+1, n, m, writer)
+		visited[i] = false
+		prev = arr[i]
+	}
+}
