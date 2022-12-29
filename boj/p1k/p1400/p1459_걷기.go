3069f46d12afe79110717565ba7016aef92fb8c5
zrma <bulbitain@gmail.com>
2022-12-29 23:37:30 +0900

    boj: solve 1459 걷기
    - https://www.acmicpc.net/problem/1459

---
+++ boj/p1k/p1400/p1459_걷기.go
@@ -0,0 +1,32 @@
+package p1400
+
+import "fmt"
+
+func Solve1459(reader Reader, writer Writer) {
+	var x, y, w, s int
+	_, _ = fmt.Fscan(reader, &x, &y, &w, &s)
+
+	// 수직, 수평으로만 이동
+	if w*2 < s {
+		_, _ = fmt.Fprint(writer, (x+y)*w)
+		return
+	}
+
+	if y < x {
+		x, y = y, x
+	}
+
+	// 최대한 수직/수평 이동 + 남는 거리 대각선
+	if w < s {
+		_, _ = fmt.Fprint(writer, x*s+(y-x)*w)
+		return
+	}
+
+	if (x+y)%2 == 0 {
+		// 대각선으로만 이동
+		_, _ = fmt.Fprint(writer, y*s)
+	} else {
+		// 대각선으로만 이동하면 수직/수평 1개 남음
+		_, _ = fmt.Fprint(writer, (y-1)*s+w)
+	}
+}
