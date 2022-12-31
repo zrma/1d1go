8be31a2ee41b8ee4cbc6cfd7700b73385e43de0c
zrma <bulbitain@gmail.com>
2022-12-31 09:25:38 +0900

    boj: solve 1802 종이 접기
    - https://www.acmicpc.net/problem/1802

---
+++ boj/p1k/p1800/p1802_종이_접기.go
@@ -0,0 +1,40 @@
+package p1800
+
+import (
+	"fmt"
+)
+
+func Solve1802(reader Reader, writer Writer) {
+	var t int
+	_, _ = fmt.Fscan(reader, &t)
+
+	for i := 0; i < t; i++ {
+		var s string
+		_, _ = fmt.Fscan(reader, &s)
+
+		if solve1802(s, 0, len(s)-1) {
+			_, _ = fmt.Fprintln(writer, "YES")
+		} else {
+			_, _ = fmt.Fprintln(writer, "NO")
+		}
+	}
+}
+
+func solve1802(s string, start, end int) bool {
+	if start >= end {
+		return true
+	}
+
+	lo := start
+	hi := end
+	for lo < hi {
+		if s[lo] == s[hi] {
+			return false
+		}
+		lo++
+		hi--
+	}
+
+	mid := (start + end) / 2
+	return solve1802(s, start, mid-1) && solve1802(s, mid+1, end)
+}
