263e627348b1580a55c8fd3608a4a391d9a1a310
zrma <bulbitain@gmail.com>
2023-01-02 22:05:46 +0900

    boj: solve 12018 Yonsei TOTO
    - https://www.acmicpc.net/problem/12018

---
+++ boj/p12k/p12000/p12018_Yonsei_TOTO.go
@@ -0,0 +1,41 @@
+package p12000
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve12018(reader Reader, writer Writer) {
+	var n, m int
+	_, _ = fmt.Fscan(reader, &n, &m)
+
+	credits := make([]int, n)
+	for i := range credits {
+		var p, l int
+		_, _ = fmt.Fscan(reader, &p, &l)
+
+		person := make([]int, p)
+		for j := range person {
+			_, _ = fmt.Fscan(reader, &person[j])
+		}
+		sort.Ints(person)
+
+		if p < l {
+			credits[i] = 1
+		} else {
+			credits[i] = person[p-l]
+		}
+	}
+	sort.Ints(credits)
+
+	res := 0
+	for i := range credits {
+		if credits[i] <= m {
+			res++
+			m -= credits[i]
+		} else {
+			break
+		}
+	}
+	_, _ = fmt.Fprint(writer, res)
+}
