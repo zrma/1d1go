6d23be7b49f9c89a4893d5779b12bf8cf7d56303
zrma <bulbitain@gmail.com>
2023-01-02 22:05:46 +0900

    boj: solve 12018 Yonsei TOTO
    - https://www.acmicpc.net/problem/12018

---
+++ boj/p12k/p12000/p12018_Yonsei_TOTO_test.go
@@ -0,0 +1,64 @@
+package p12000_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p12k/p12000"
+)
+
+func TestSolve12018(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/12018")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`5 76
+5 4 
+36 25 1 36 36
+4 4
+30 24 25 20
+6 4
+36 36 36 36 36 36
+2 4
+3 7
+5 4
+27 15 26 8 14`,
+			"4",
+		},
+		{
+			`5 999999
+5 4 
+36 25 1 36 36
+4 4
+30 24 25 20
+6 4
+36 36 36 36 36 36
+2 4
+3 7
+5 4
+27 15 26 8 14`,
+			"5",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			p12000.Solve12018(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
