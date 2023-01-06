7623009d330dc710f3127ccb1ae4485e3cb7d6be
zrma <bulbitain@gmail.com>
2023-01-06 22:30:34 +0900

    boj: solve 3109 빵집
    - https://www.acmicpc.net/problem/3109

---
+++ boj/p3k/p3100/p3109_빵집.go
@@ -0,0 +1,72 @@
+package p3100
+
+import (
+	"fmt"
+)
+
+type gridState int
+
+const (
+	empty gridState = iota
+	wall
+	visited
+)
+
+func Solve3109(reader Reader, writer Writer) {
+	var r, c int
+	_, _ = fmt.Fscan(reader, &r, &c)
+
+	grid := make([][]gridState, r)
+	for row := range grid {
+		var s string
+		_, _ = fmt.Fscan(reader, &s)
+		grid[row] = make([]gridState, c)
+		for col := range grid[row] {
+			c := s[col]
+			if c == 'x' {
+				grid[row][col] = wall
+			}
+		}
+	}
+
+	_, _ = fmt.Fprint(writer, solve3109(grid))
+}
+
+func solve3109(grid [][]gridState) int {
+	res := 0
+	for row := range grid {
+		if dfs3109(grid, row, 0) {
+			res++
+		}
+	}
+	return res
+}
+
+func dfs3109(grid [][]gridState, row int, col int) bool {
+	if col == len(grid[row])-1 {
+		return true
+	}
+
+	if row > 0 && grid[row-1][col+1] == empty {
+		grid[row-1][col+1] = visited
+		if dfs3109(grid, row-1, col+1) {
+			return true
+		}
+	}
+
+	if grid[row][col+1] == empty {
+		grid[row][col+1] = visited
+		if dfs3109(grid, row, col+1) {
+			return true
+		}
+	}
+
+	if row < len(grid)-1 && grid[row+1][col+1] == empty {
+		grid[row+1][col+1] = visited
+		if dfs3109(grid, row+1, col+1) {
+			return true
+		}
+	}
+
+	return false
+}
