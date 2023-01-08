bcf44b01ae2d84cc14ca96388d4c16ea5eb84a2f
zrma <bulbitain@gmail.com>
2023-01-09 08:20:14 +0900

    boj: solve 1015 수열 정렬
    - https://www.acmicpc.net/problem/1015

---
+++ boj/p1k/p1000/p1015_수열_정렬.go
@@ -0,0 +1,35 @@
+package p1000
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve1015(reader Reader, writer Writer) {
+	var n int
+	_, _ = fmt.Fscan(reader, &n)
+
+	arr := make([]int, n)
+	for i := range arr {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+
+	sortedArr := make([]int, n)
+	copy(sortedArr, arr)
+	sort.Ints(sortedArr)
+
+	visited := make([]bool, n)
+	for i, before := range arr {
+		for j, after := range sortedArr {
+			if !visited[j] && before == after {
+				if i < n-1 {
+					_, _ = fmt.Fprint(writer, j, " ")
+				} else {
+					_, _ = fmt.Fprint(writer, j)
+				}
+				visited[j] = true
+				break
+			}
+		}
+	}
+}
