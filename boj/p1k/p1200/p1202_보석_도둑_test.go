ea440b64755a0b32b479d32620448edff1abecdd
zrma <bulbitain@gmail.com>
2023-01-08 18:12:11 +0900

    boj: solve 1202 보석 도둑
    - https://www.acmicpc.net/problem/1202

---
+++ boj/p1k/p1200/p1202_보석_도둑_test.go
@@ -0,0 +1,79 @@
+package p1200_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1200"
+)
+
+func TestSolve1202(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1202")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`2 1
+5 10
+100 100
+11`,
+			"10",
+		},
+		{
+			`3 2
+1 65
+5 23
+2 99
+10
+2`,
+			"164",
+		},
+		{
+			`2 3
+1 1
+2 2
+2 2
+3 3
+4 4`,
+			"3",
+		},
+		{
+			`3 2
+10 65
+5 23
+2 99
+10
+2`,
+			"164",
+		},
+		{
+			`3 2
+10 65
+5 99
+2 23
+10
+2`,
+			"122",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			p1200.Solve1202(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
