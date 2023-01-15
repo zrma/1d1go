141353b7db9af0904b76ac95e4fa6fe0fcfb1f42
zrma <bulbitain@gmail.com>
2023-01-15 21:52:17 +0900

    boj: solve 1700 멀티탭 스케줄링
    - https://www.acmicpc.net/problem/1700

---
+++ boj/p1k/p1700/p1700_멀티탭_스케줄링_test.go
@@ -0,0 +1,67 @@
+package p1700_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1700"
+)
+
+func TestSolve1700(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1700")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`2 7
+2 3 2 3 1 2 7`,
+			"2",
+		},
+		{
+			`3 13
+
+2 3 4 2 3 4 1 5 5 5 2 3 2`,
+			"2",
+		},
+		{
+			`3 5
+1 1 1 1 2`,
+			"0",
+		},
+		{
+			`3 5
+1 1 1 1 2`,
+			"0",
+		},
+		{
+			`1 5
+1 2 3 4 1`,
+			"4",
+		},
+		{
+			`3 14
+1 4 3 2 5 4 3 2 5 3 4 2 3 4`,
+			"4",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			p1700.Solve1700(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
