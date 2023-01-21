353bd26dff894811b9dc3eafd30f1d711f81a793
zrma <bulbitain@gmail.com>
2023-01-21 22:20:27 +0900

    boj: solve 1114 통나무 자르기
    - https://www.acmicpc.net/problem/1114

---
+++ boj/p1k/p1100/p1114_통나무_자르기.go
@@ -0,0 +1,77 @@
+package p1100
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve1114(reader Reader, writer Writer) {
+	var l, k, c int
+	_, _ = fmt.Fscan(reader, &l, &k, &c)
+
+	arr := make([]int, k)
+	for i := range arr {
+		_, _ = fmt.Fscan(reader, &arr[i])
+	}
+
+	// 처음과 끝 추가
+	arr = append(arr, 0, l)
+
+	sort.Ints(arr)
+
+	dists := make([]int, len(arr)-1)
+	for i := 1; i < len(arr); i++ {
+		dists[i-1] = arr[i] - arr[i-1]
+	}
+	sort.Ints(dists)
+
+	lo := dists[0]
+	hi := l
+
+	for lo < hi {
+		dist := lo + (hi-lo)/2
+		if cut1114(arr, dist, c) {
+			// 더 작은 간격으로 잘라봄
+			hi = dist
+		} else {
+			// 자를 수 있는 횟수가 모자라다.
+			// 더 큰 간격으로 잘라야 함
+			lo = dist + 1
+		}
+	}
+	longest := hi
+
+	curr := len(arr) - 1
+	firstCut := arr[curr]
+	cutCnt := 0
+	for i := curr - 2; i >= 0; i-- {
+		if dist := arr[curr] - arr[i]; dist > longest {
+			cutCnt++
+			i++
+			firstCut = arr[i]
+			curr = i
+		}
+	}
+	if cutCnt < c {
+		firstCut = arr[1]
+	}
+	_, _ = fmt.Fprint(writer, longest, firstCut)
+}
+
+func cut1114(points []int, dist, cntLimit int) bool {
+	cnt := 0
+	prev := 0
+	for i := 1; i < len(points); i++ {
+		if cnt > cntLimit {
+			return false
+		}
+
+		currDist := points[i] - points[prev]
+		if currDist > dist {
+			cnt++
+			prev = i - 1
+			i--
+		}
+	}
+	return true
+}
---
+++ boj/p1k/p1100/p1114_통나무_자르기_test.go
@@ -0,0 +1,71 @@
+package p1100_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p1k/p1100"
+)
+
+func TestSolve1114(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1114")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`9 2 1
+4 5`,
+			"5 4",
+		},
+		{
+			`5 1 2
+3`,
+			"3 3",
+		},
+		{
+			`5 5 3
+4 2 5 3 1`,
+			"2 1",
+		},
+		{
+			`5 10 10
+4 3 2 1 4 3 2 1 4 3`,
+			"1 1",
+		},
+		{
+			`20 6 5
+3 6 9 12 15 18`,
+			"5 3",
+		},
+		{
+			`10 4 2
+1 4 5 10`,
+			"5 1",
+		},
+		{
+			`9 9 2
+1 2 3 4 5 6 7 8 9`,
+			"3 3",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			p1100.Solve1114(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
