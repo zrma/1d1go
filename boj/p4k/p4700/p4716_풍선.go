2b3f9dce99df2217bc4314592693830745c8f22f
zrma <bulbitain@gmail.com>
2023-01-23 19:33:43 +0900

    boj: solve 4716 풍선
    - https://www.acmicpc.net/problem/4716

---
+++ boj/p4k/p4700/p4716_풍선.go
@@ -0,0 +1,67 @@
+package p4700
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Solve4716(reader Reader, writer Writer) {
+	for {
+		var n, a, b int
+		_, _ = fmt.Fscan(reader, &n, &a, &b)
+		if n == 0 {
+			break
+		}
+
+		type balloon struct {
+			k, distA, distB int
+		}
+		balloons := make([]balloon, n)
+		for i := range balloons {
+			_, _ = fmt.Fscan(reader, &balloons[i].k, &balloons[i].distA, &balloons[i].distB)
+		}
+
+		diff := func(v balloon) int {
+			dist := v.distA - v.distB
+			if dist < 0 {
+				return -dist
+			}
+			return dist
+		}
+
+		sort.Slice(balloons, func(i, j int) bool {
+			return diff(balloons[i]) > diff(balloons[j])
+		})
+
+		var totDist int
+		for _, v := range balloons {
+			if v.distA < v.distB {
+				if a < v.k {
+					v.k -= a
+					totDist += a * v.distA
+					a = 0
+
+					totDist += v.k * v.distB
+					b -= v.k
+				} else {
+					a -= v.k
+					totDist += v.k * v.distA
+				}
+			} else {
+				if b < v.k {
+					v.k -= b
+					totDist += b * v.distB
+					b = 0
+
+					totDist += v.k * v.distA
+					a -= v.k
+				} else {
+					b -= v.k
+					totDist += v.k * v.distB
+				}
+			}
+		}
+
+		_, _ = fmt.Fprintln(writer, totDist)
+	}
+}
---
+++ boj/p4k/p4700/p4716_풍선_test.go
@@ -0,0 +1,64 @@
+package p4700_test
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"1d1go/boj/p4k/p4700"
+)
+
+func TestSolve4716(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/4716")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`3 15 35
+10 20 10
+10 10 30
+10 40 10
+3 15 35
+10 20 10
+10 10 30
+10 40 10
+0 0 0`,
+			`300
+300
+`,
+		},
+		{
+			`3 10 20
+10 0 10
+10 0 10
+10 0 10
+3 20 10
+10 10 0
+10 10 0
+10 10 0
+0 0 0`,
+			`200
+200
+`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			p4700.Solve4716(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			git := buf.String()
+			assert.Equal(t, tt.want, git)
+		})
+	}
+}
