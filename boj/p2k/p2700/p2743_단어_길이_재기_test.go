6e76fd2c62d590d50aff5222eeb38c8a74078bd5
zrma <bulbitain@gmail.com>
2023-01-25 23:09:07 +0900

    boj: solve 2743 단어 길이 재기
    - https://www.acmicpc.net/problem/2743

---
+++ boj/p2k/p2700/p2743_단어_길이_재기_test.go
@@ -0,0 +1,36 @@
+package p2700
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve2743(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2743")
+
+	//goland:noinspection SpellCheckingInspection
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{"pulljima", "8"},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			Solve2743(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
