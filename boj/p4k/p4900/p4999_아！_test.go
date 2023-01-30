14dc2dd09f43fa449b290e11fcf4dcac8c62c9de
zrma <bulbitain@gmail.com>
2023-01-30 21:52:05 +0900

    boj: solve 4999 아!
    - https://www.acmicpc.net/problem/4999

---
+++ boj/p4k/p4900/p4999_아！_test.go
@@ -0,0 +1,45 @@
+package p4900
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve4999(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/4999")
+
+	//goland:noinspection SpellCheckingInspection
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`aaah
+aaaaah`,
+			"no",
+		},
+		{
+			`aaah
+ah`,
+			"go",
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			Solve4999(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
