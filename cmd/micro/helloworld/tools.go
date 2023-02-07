caef89816835ac416cc645cc253897cf9bcd6502
zrma <bulbitain@gmail.com>
2023-02-07 21:31:37 +0900

    cmd: remove micro example

--- cmd/micro/helloworld/tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-
-package main
-
-import (
-	_ "go-micro.dev/v4/cmd/protoc-gen-micro"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
-)
