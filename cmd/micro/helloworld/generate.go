425ae427eb1fec5014ec2bed21e805b427ed9026
zrma <bulbitain@gmail.com>
2023-02-07 21:31:37 +0900

    cmd: remove micro example

--- cmd/micro/helloworld/generate.go
+++
@@ -1,6 +0,0 @@
-package main
-
-//go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
-//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
-//go:generate go install go-micro.dev/v4/cmd/protoc-gen-micro@v4
-//go:generate make proto
