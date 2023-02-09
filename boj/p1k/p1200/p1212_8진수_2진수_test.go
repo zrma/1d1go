d74a5e2a2f946ff5855b29623afe77a5301a8826
zrma <bulbitain@gmail.com>
2023-02-09 21:58:14 +0900

    boj: solve 1212 8진수 2진수
    - https://www.acmicpc.net/problem/1212

---
+++ boj/p1k/p1200/p1212_8진수_2진수_test.go
@@ -0,0 +1,64 @@
+package p1200
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve1212(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1212")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{"314", "11001100"},
+		{"7", "111"},
+		{"66", "110110"},
+		{"234", "10011100"},
+		{"5", "101"},
+		{"4", "100"},
+		{"1", "1"},
+		{"0", "0"},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			Solve1212(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestToBinary(t *testing.T) {
+	for i, tt := range []struct {
+		give byte
+		want string
+	}{
+		{'0', "000"},
+		{'1', "001"},
+		{'2', "010"},
+		{'3', "011"},
+		{'4', "100"},
+		{'5', "101"},
+		{'6', "110"},
+		{'7', "111"},
+		{'8', ""},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			got := toBinary(tt.give)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
