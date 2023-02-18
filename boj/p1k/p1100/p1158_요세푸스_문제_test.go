af58bed6e26eefc96bb7d8d4b9cdd149e5b4f6cd
zrma <bulbitain@gmail.com>
2023-02-18 21:51:27 +0900

    boj: solve 1158 요세푸스 문제
    - https://www.acmicpc.net/problem/1158

---
+++ boj/p1k/p1100/p1158_요세푸스_문제_test.go
@@ -0,0 +1,35 @@
+package p1100
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve1158(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1158")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{"7 3", "<3, 6, 2, 7, 5, 1, 4>"},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			Solve1158(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
