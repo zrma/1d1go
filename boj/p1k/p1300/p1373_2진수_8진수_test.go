cfb8fb1125f6db73c2b55360e9006ca0ef406190
zrma <bulbitain@gmail.com>
2023-02-19 22:03:19 +0900

    boj: solve 1373 2진수 8진수
    - https://www.acmicpc.net/problem/1373

---
+++ boj/p1k/p1300/p1373_2진수_8진수_test.go
@@ -0,0 +1,39 @@
+package p1300
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve1373(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1373")
+
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{"11001100", "314"},
+		{"11011", "33"},
+		{"11111111", "377"},
+		{"11", "3"},
+		{"1", "1"},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			Solve1373(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
