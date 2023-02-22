c4e0ff58846cd8f51d842b10a91ddd6726f095ae
zrma <bulbitain@gmail.com>
2023-02-22 21:38:07 +0900

    boj: solve 1302 베스트셀러
    - https://www.acmicpc.net/problem/1302

---
+++ boj/p1k/p1300/p1302_베스트셀러_test.go
@@ -0,0 +1,84 @@
+package p1300
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve1302(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1302")
+
+	//goland:noinspection SpellCheckingInspection
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{
+			`5
+top
+top
+top
+top
+kimtop`,
+			`top`,
+		},
+		{
+			`9
+table
+chair
+table
+table
+lamp
+door
+lamp
+table
+chair`,
+			`table`,
+		},
+		{
+			`6
+a
+a
+a
+b
+b
+b`,
+			`a`,
+		},
+		{
+			`8
+icecream
+peanuts
+peanuts
+chocolate
+candy
+chocolate
+icecream
+apple`,
+			`chocolate`,
+		},
+		{
+			`1
+soul`,
+			`soul`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			Solve1302(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
