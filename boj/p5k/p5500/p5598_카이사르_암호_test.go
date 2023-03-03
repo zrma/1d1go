c67d89ef59c25c843ce7a8e72facfa0f622f6dd1
zrma <bulbitain@gmail.com>
2023-03-03 22:42:15 +0900

    boj: solve 5598 카이사르 암호
    - https://www.acmicpc.net/problem/5598

---
+++ boj/p5k/p5500/p5598_카이사르_암호_test.go
@@ -0,0 +1,37 @@
+package p5500
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve5598(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/5598")
+
+	//goland:noinspection SpellCheckingInspection
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{"MRL", "JOI"},
+		{"FURDWLD", "CROATIA"},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			Solve5598(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
