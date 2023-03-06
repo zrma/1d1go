5a8539d8f492c740ec9c745d45f935c37c6f8527
zrma <bulbitain@gmail.com>
2023-03-06 22:06:47 +0900

    boj: solve 2789 유학 금지
    - https://www.acmicpc.net/problem/2789

---
+++ boj/p2k/p2700/p2789_유학_금지_test.go
@@ -0,0 +1,37 @@
+package p2700
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve2789(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/2789")
+
+	//goland:noinspection SpellCheckingInspection
+	for i, tt := range []struct {
+		give string
+		want string
+	}{
+		{"LOVA", "LOV"},
+		{"KARIJERA", "KJ"},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			Solve2789(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
