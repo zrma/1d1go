4c7ea82bb0d20107ed9cbb25b375e6b93f89db1f
zrma <bulbitain@gmail.com>
2023-03-09 22:12:44 +0900

    boj: solve 1919 애너그램 만들기
    - https://www.acmicpc.net/problem/1919

---
+++ boj/p1k/p1900/p1919_애너그램_만들기_test.go
@@ -0,0 +1,42 @@
+package p1900
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve1919(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1919")
+
+	//goland:noinspection SpellCheckingInspection
+	tests := []struct {
+		give string
+		want string
+	}{
+		{
+			`aabbcc
+xxyybb`,
+			"8",
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(bytes.Buffer)
+			writer := bufio.NewWriter(buf)
+
+			Solve1919(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
