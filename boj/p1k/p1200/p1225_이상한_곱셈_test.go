a9ae49db2d0aac4cc4782bd5a1fbf499ad20def0
zrma <bulbitain@gmail.com>
2023-03-10 20:11:55 +0900

    boj: solve 1225 이상한 곱셈
    - https://www.acmicpc.net/problem/1225

---
+++ boj/p1k/p1200/p1225_이상한_곱셈_test.go
@@ -0,0 +1,37 @@
+package p1200
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve1225(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/1225")
+
+	tests := []struct {
+		give string
+		want string
+	}{
+		{"123 45", "54"},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			Solve1225(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
