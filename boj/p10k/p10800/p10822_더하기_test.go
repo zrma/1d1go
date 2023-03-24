43920cee5036e2dfa59a32d4129b7f8f9aac4203
zrma <bulbitain@gmail.com>
2023-03-24 23:39:19 +0900

    boj: solve 10822 더하기
    - https://www.acmicpc.net/problem/10822

---
+++ boj/p10k/p10800/p10822_더하기_test.go
@@ -0,0 +1,37 @@
+package p10800
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve10822(t *testing.T) {
+	t.Log("https://www.acmicpc.net/problem/10822")
+
+	tests := []struct {
+		give string
+		want string
+	}{
+		{"10,20,30,50,100", "210"},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+
+			Solve10822(reader, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
