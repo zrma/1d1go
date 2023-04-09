c4ec8f09ddf4c8125bb74a8e148ff8630b3ad8a1
zrma <bulbitain@gmail.com>
2023-04-09 18:22:35 +0900

    hyperskill: add Work on project. Stage 4/7: A good stack

--- cmd/flash_cards/main.go
+++ cmd/flash_cards/main.go
@@ -29,28 +29,59 @@ func FlashCards(scanner *bufio.Scanner, writer io.Writer) {
 	}
 
 	cards := make([]card, n)
+	labels := make(map[string]int)
+	answers := make(map[string]int)
 
-	for i := 1; i <= n; i++ {
-		_, _ = fmt.Fprintf(writer, "The term for card #%d:\n", i)
-		scanner.Scan()
-		label := scanner.Text()
+	for i := 0; i < n; i++ {
+		_, _ = fmt.Fprintf(writer, "The term for card #%d:\n", i+1)
 
-		_, _ = fmt.Fprintf(writer, "The definition for card #%d:\n", i)
-		scanner.Scan()
-		answer := scanner.Text()
+		var label string
+		for scanner.Scan() {
+			label = scanner.Text()
 
-		cards[i-1] = card{label, answer}
+			if _, ok := labels[label]; !ok {
+				break
+			}
+
+			_, _ = fmt.Fprintf(writer, "The term \"%s\" already exists. Try again:\n", label)
+		}
+
+		_, _ = fmt.Fprintf(writer, "The definition for card #%d:\n", i+1)
+
+		var answer string
+
+		for scanner.Scan() {
+			answer = scanner.Text()
+
+			if _, ok := answers[answer]; !ok {
+				break
+			}
+
+			_, _ = fmt.Fprintf(writer, "The definition \"%s\" already exists. Try again:\n", answer)
+		}
+
+		cards[i] = card{label, answer}
+		labels[label] = i
+		answers[answer] = i
 	}
 
-	for i := 1; i <= n; i++ {
-		_, _ = fmt.Fprintf(writer, "Print the definition of \"%s\":\n", cards[i-1].label)
+	for i := 0; i < n; i++ {
 		scanner.Scan()
+		_, _ = fmt.Fprintf(writer, "Print the definition of \"%s\":\n", cards[i].label)
+
 		answer := scanner.Text()
 
-		if answer == cards[i-1].answer {
+		if answer == cards[i].answer {
 			_, _ = fmt.Fprintln(writer, "Correct!")
-		} else {
-			_, _ = fmt.Fprintf(writer, "Wrong. The right answer is \"%s\".\n", cards[i-1].answer)
+			continue
 		}
+
+		_, ok := answers[answer]
+		if !ok {
+			_, _ = fmt.Fprintf(writer, "Wrong. The right answer is \"%s\".\n", cards[i].answer)
+			continue
+		}
+
+		_, _ = fmt.Fprintf(writer, "Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".\n", cards[i].answer, cards[answers[answer]].label)
 	}
 }
--- cmd/flash_cards/main_test.go
+++ cmd/flash_cards/main_test.go
@@ -20,19 +20,42 @@ func TestPlay(t *testing.T) {
 			`2
 print()
 outputs text
+print()
 str()
+outputs text
 converts to a string
 outputs text
-outputs text`,
+converts to a string`,
+			`Input the number of cards:
+The term for card #1:
+The definition for card #1:
+The term for card #2:
+The term "print()" already exists. Try again:
+The definition for card #2:
+The definition "outputs text" already exists. Try again:
+Print the definition of "print()":
+Correct!
+Print the definition of "str()":
+Correct!
+`,
+		},
+		{
+			`2
+uncle
+a brother of one's parent
+ankle
+a part of the body where the foot and the leg meet
+a part of the body where the foot and the leg meet
+???`,
 			`Input the number of cards:
 The term for card #1:
 The definition for card #1:
 The term for card #2:
 The definition for card #2:
-Print the definition of "print()":
-Correct!
-Print the definition of "str()":
-Wrong. The right answer is "converts to a string".
+Print the definition of "uncle":
+Wrong. The right answer is "a brother of one's parent", but your definition is correct for "ankle".
+Print the definition of "ankle":
+Wrong. The right answer is "a part of the body where the foot and the leg meet".
 `,
 		},
 	}
