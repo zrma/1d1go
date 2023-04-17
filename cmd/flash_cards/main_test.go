be49fb6f8db05e4fe5d17426099409ef803ddfd1
zrma <bulbitain@gmail.com>
2023-04-17 21:55:45 +0900

    hyperskill: add Work on project. Stage 6/7: Statistics

--- cmd/flash_cards/main_test.go
+++ cmd/flash_cards/main_test.go
@@ -30,27 +30,27 @@ France
 remove
 Wakanda
 exit`,
-			`Input the action (add, remove, import, export, ask, exit):
+			`Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 The card:
 The definition of the card:
 The pair ("France":"Paris") has been added.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 The card:
 The card "France" already exists. Try again:
 The definition of the card:
 The definition "Paris" already exists. Try again:
 The pair ("Great Britain":"London") has been added.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 Which card?
 The card has been removed.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 Which card?
 Can't remove "Wakanda": there is no such card.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 Bye bye!
 `,
 		},
@@ -72,36 +72,36 @@ Moscow
 export
 capitalsNew.txt
 exit`,
-			`Input the action (add, remove, import, export, ask, exit):
+			`Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 File name:
 File not found.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 The card:
 The definition of the card:
 The pair ("Japan":"Tokyo") has been added.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 The card:
 The definition of the card:
 The pair ("Russia":"UpdateMeFromFile") has been added.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 File name:
 28 cards have been loaded.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 How many times to ask?
 Print the definition of "Japan":
 Correct!
 Print the definition of "Russia":
 Correct!
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 File name:
 29 cards have been saved.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 Bye bye!
 `,
 		},
@@ -121,20 +121,20 @@ ankle
 ??
 uncle
 exit`,
-			`Input the action (add, remove, import, export, ask, exit):
+			`Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 The card:
 The definition of the card:
 The pair ("a brother of one's parent":"uncle") has been added.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 The card:
 The definition of the card:
 The pair ("a part of the body where the foot and the leg meet":"ankle") has been added.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 How many times to ask?
 Print the definition of "a brother of one's parent":
-Wrong. The right answer is "uncle", but your definition is correct for "a part of the body where the foot and the leg meet".
+Wrong. The right answer is "uncle", but your definition is correct for "a part of the body where the foot and the leg meet" card.
 Print the definition of "a part of the body where the foot and the leg meet":
 Wrong. The right answer is "ankle".
 Print the definition of "a brother of one's parent":
@@ -144,9 +144,55 @@ Correct!
 Print the definition of "a brother of one's parent":
 Wrong. The right answer is "uncle".
 Print the definition of "a part of the body where the foot and the leg meet":
-Wrong. The right answer is "ankle", but your definition is correct for "a brother of one's parent".
+Wrong. The right answer is "ankle", but your definition is correct for "a brother of one's parent" card.
 
-Input the action (add, remove, import, export, ask, exit):
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
+Bye bye!
+`,
+		},
+		{
+			`hardest card
+import
+capitals.txt
+hardest card
+ask
+1
+Paris
+hardest card
+reset stats
+hardest card
+log
+todayLog.txt
+exit`,
+			`Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
+There are no cards with errors.
+
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
+File name:
+28 cards have been loaded.
+
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
+The hardest card is "France". You have 10 errors answering it.
+
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
+How many times to ask?
+Print the definition of "Russia":
+Wrong. The right answer is "Moscow", but your definition is correct for "France" card.
+
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
+The hardest cards are "Russia", "France". You have 10 errors answering them.
+
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
+Card statistics have been reset.
+
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
+There are no cards with errors.
+
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
+File name:
+The log has been saved.
+
+Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):
 Bye bye!
 `,
 		},
