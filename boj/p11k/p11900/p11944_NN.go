975a1a1265fe4cdaac121b59f47ae24160283cd6
zrma <bulbitain@gmail.com>
2023-04-21 22:48:14 +0900

    boj: solve 11944 NN
    - https://www.acmicpc.net/problem/11944

---
+++ boj/p11k/p11900/p11944_NN.go
@@ -0,0 +1,28 @@
+package p11900
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func Solve11944(reader io.Reader, writer io.Writer) {
+	var n, m int
+	_, _ = fmt.Fscanln(reader, &n, &m)
+
+	buf := new(strings.Builder)
+	buf.Grow(m)
+
+	for i := 0; i < n; i++ {
+		buf.WriteString(fmt.Sprintf("%d", n))
+		if buf.Len() >= m {
+			break
+		}
+	}
+
+	res := buf.String()
+	if len(res) > m {
+		res = res[:m]
+	}
+	_, _ = fmt.Fprint(writer, res)
+}
