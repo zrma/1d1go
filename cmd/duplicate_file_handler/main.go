7504990963df02b020e2126a235b3e4b66a13db7
zrma <bulbitain@gmail.com>
2023-04-23 18:58:56 +0900

    hyperskill: add Work on project. Stage 2/4: How much does it weigh?

--- cmd/duplicate_file_handler/main.go
+++ cmd/duplicate_file_handler/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 
 	"github.com/spf13/afero"
 )
@@ -18,7 +21,9 @@ func main() {
 
 	targetPath := args[1]
 
-	duplicateFileHandler(targetPath, os.Stdout)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	duplicateFileHandler(targetPath, scanner, os.Stdout)
 }
 
 var fileSystem = afero.NewMemMapFs()
@@ -31,7 +36,34 @@ func walkFunc(path string, f filepath.WalkFunc) error {
 	return afero.Walk(fileSystem, path, f)
 }
 
-func duplicateFileHandler(targetPath string, writer io.Writer) {
+func duplicateFileHandler(targetPath string, scanner *bufio.Scanner, writer io.Writer) {
+	scanner.Split(bufio.ScanLines)
+
+	_, _ = fmt.Fprintln(writer, "Enter file format:")
+
+	scanner.Scan()
+	fileFormat := scanner.Text()
+
+	_, _ = fmt.Fprintln(writer, "Size sorting options:")
+	_, _ = fmt.Fprintln(writer, "1. Descending")
+	_, _ = fmt.Fprintln(writer, "2. Ascending")
+	_, _ = fmt.Fprintln(writer)
+
+	var sortingOption string
+	for {
+		_, _ = fmt.Fprintln(writer, "Enter a sorting option:")
+
+		scanner.Scan()
+		sortingOption = scanner.Text()
+		if sortingOption == "1" || sortingOption == "2" {
+			break
+		}
+		_, _ = fmt.Fprintln(writer, "Wrong option")
+		_, _ = fmt.Fprintln(writer)
+	}
+
+	files := make(map[int64][]string)
+
 	err := filepathWalk(targetPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -39,10 +71,49 @@ func duplicateFileHandler(targetPath string, writer io.Writer) {
 		if info.IsDir() {
 			return nil
 		}
-		_, _ = fmt.Fprintln(writer, filepath.ToSlash(path))
+		if fileFormat != "" && !strings.HasSuffix(info.Name(), fileFormat) {
+			return nil
+		}
+
+		files[info.Size()] = append(files[info.Size()], filepath.ToSlash(path))
 		return nil
 	})
 	if err != nil {
 		fmt.Println(err)
+	}
+
+	var res []struct {
+		size  int64
+		files []string
+	}
+	for size, files := range files {
+		if len(files) < 2 {
+			continue
+		}
+
+		sort.Strings(files)
+
+		res = append(res, struct {
+			size  int64
+			files []string
+		}{size, files})
+	}
+
+	if sortingOption == "1" {
+		sort.Slice(res, func(i, j int) bool {
+			return res[i].size > res[j].size
+		})
+	} else {
+		sort.Slice(res, func(i, j int) bool {
+			return res[i].size < res[j].size
+		})
+	}
+
+	for _, r := range res {
+		_, _ = fmt.Fprintf(writer, "%d bytes\n", r.size)
+		for _, file := range r.files {
+			_, _ = fmt.Fprintf(writer, "%s\n", file)
+		}
+		_, _ = fmt.Fprintln(writer)
 	}
 }
--- cmd/duplicate_file_handler/main_test.go
+++ cmd/duplicate_file_handler/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -14,60 +15,98 @@ import (
 func TestDuplicateFileHandler(t *testing.T) {
 	t.Log("https://hyperskill.org/projects/240")
 
+	//chopin_waltz7_op64_no2.mp3 / 9765504 bytes
+	//vivaldi_four_seasons_winter.mp3 / 9158144 bytes
+	//sia_snowman.mp3 / 4590560 bytes
+	//gordon_ramsay_chicken_breast.avi / 4590560 bytes
+	//smells_like_teen_spirit.mp3 / 4590560 bytes
+	//numb.mp3 / 5786312 bytes
+	//rick_astley_never_gonna_give_you_up.mp3 / 3422208 bytes
+	//unknown.mp3 / 3422208 bytes
+	//nea_some_say.mp3 / 3232056 bytes
+	//voice.mp3 / 2319746 bytes
+
 	// +---[root_folder]
-	//    |
-	//    +---wall.png
-	//    +---pass.txt
-	//    +---[docs]
+	//    +---gordon_ramsay_chicken_breast.avi /4590560 bytes
+	//    +---[audio]
 	//    |   |
-	//    |   +---project.py
-	//    |   +---calc.xls
-	//    |   +---tutorial.mp4
-	//    |   +---[res]
+	//    |   +---voice.mp3 /2319746 bytes
+	//    |   +---sia_snowman.mp3 /4590560 bytes
+	//    |   +---nea_some_say.mp3 /3232056 bytes
+	//    |   +---[classic]
+	//    |   |   |
+	//    |   |   +---unknown.mp3 /3422208 bytes
+	//    |   |   +---vivaldi_four_seasons_winter.mp3 /9158144 bytes
+	//    |   |   +---chopin_waltz7_op64_no2.mp3 /9765504 bytes
+	//    |   +---[rock]
 	//    |       |
-	//    |       +---data.json
-	//    |   +---[output]
-	//    |       |
-	//    |       +---result.json
+	//    |       +---smells_like_teen_spirit.mp3 /4590560 bytes
+	//    |       +---numb.mp3 /5786312 bytes
 	//    +---[masterpiece]
 	//        |
-	//        +---rick_astley_never_gonna_give_you_up.mp3
+	//        +---rick_astley_never_gonna_give_you_up.mp3 /3422208 bytes
 
 	assert.NoError(t, fileSystem.Mkdir("root_folder", 0755))
-	assert.NoError(t, fileSystem.Mkdir("root_folder/docs", 0755))
-	assert.NoError(t, fileSystem.Mkdir("root_folder/docs/res", 0755))
-	assert.NoError(t, fileSystem.Mkdir("root_folder/docs/output", 0755))
+	assert.NoError(t, fileSystem.Mkdir("root_folder/audio", 0755))
+	assert.NoError(t, fileSystem.Mkdir("root_folder/audio/classic", 0755))
+	assert.NoError(t, fileSystem.Mkdir("root_folder/audio/rock", 0755))
 	assert.NoError(t, fileSystem.Mkdir("root_folder/masterpiece", 0755))
 
-	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/wall.png", []byte("wall.png"), 0644))
-	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/pass.txt", []byte("pass.txt"), 0644))
-	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/docs/project.py", []byte("project.py"), 0644))
-	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/docs/calc.xls", []byte("calc.xls"), 0644))
-	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/docs/tutorial.mp4", []byte("tutorial.mp4"), 0644))
-	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/docs/res/data.json", []byte("data.json"), 0644))
-	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/docs/output/result.json", []byte("result.json"), 0644))
-	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/masterpiece/rick_astley_never_gonna_give_you_up.mp3", []byte("rick_astley_never_gonna_give_you_up.mp3"), 0644))
+	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/audio/classic/chopin_waltz7_op64_no2.mp3", []byte("7777777"), 0644))
+	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/audio/classic/vivaldi_four_seasons_winter.mp3", []byte("666666"), 0644))
+	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/audio/sia_snowman.mp3", []byte("55555"), 0644))
+	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/gordon_ramsay_chicken_breast.avi", []byte("55555"), 0644))
+	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/audio/rock/smells_like_teen_spirit.mp3", []byte("55555"), 0644))
+	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/audio/rock/numb.mp3", []byte("4444"), 0644))
+	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/masterpiece/rick_astley_never_gonna_give_you_up.mp3", []byte("333"), 0644))
+	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/audio/classic/unknown.mp3", []byte("333"), 0644))
+	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/audio/nea_some_say.mp3", []byte("22"), 0644))
+	assert.NoError(t, afero.WriteFile(fileSystem, "root_folder/audio/voice.mp3", []byte("1"), 0644))
 
-	const want = `root_folder/wall.png
-root_folder/pass.txt
-root_folder/docs/project.py
-root_folder/docs/calc.xls
-root_folder/docs/tutorial.mp4
-root_folder/docs/res/data.json
-root_folder/docs/output/result.json
+	tests := []struct {
+		give string
+		want string
+	}{
+		{
+			`mp3
+3
+2`,
+			`Enter file format:
+Size sorting options:
+1. Descending
+2. Ascending
+
+Enter a sorting option:
+Wrong option
+
+Enter a sorting option:
+3 bytes
+root_folder/audio/classic/unknown.mp3
 root_folder/masterpiece/rick_astley_never_gonna_give_you_up.mp3
-`
 
-	filepathWalk = walkFunc
-	defer func() { filepathWalk = filepath.Walk }()
+5 bytes
+root_folder/audio/rock/smells_like_teen_spirit.mp3
+root_folder/audio/sia_snowman.mp3
 
-	buf := new(strings.Builder)
-	writer := bufio.NewWriter(buf)
-	duplicateFileHandler("root_folder", writer)
+`,
+		},
+	}
 
-	err := writer.Flush()
-	assert.NoError(t, err)
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			filepathWalk = walkFunc
+			defer func() { filepathWalk = filepath.Walk }()
 
-	got := buf.String()
-	assert.ElementsMatch(t, strings.Split(want, "\n"), strings.Split(got, "\n"))
+			scanner := bufio.NewScanner(strings.NewReader(tt.give))
+			buf := new(strings.Builder)
+			writer := bufio.NewWriter(buf)
+			duplicateFileHandler("root_folder", scanner, writer)
+
+			err := writer.Flush()
+			assert.NoError(t, err)
+
+			got := buf.String()
+			assert.Equal(t, tt.want, got)
+		})
+	}
 }
