74a0fd4749db9406f1a177e2b89b617f34911910
zrma <bulbitain@gmail.com>
2025-01-11 01:22:43 +0900

    boj: improve CCW algorithm and line intersection logic

--- boj/p17k/p17300/p17386_선분_교차_1.go
+++ boj/p17k/p17300/p17386_선분_교차_1.go
@@ -50,6 +50,35 @@ func MaxDistance(p0, p1, p2, p3 Point) float64 {
 	return math.Max(math.Max(dist0, dist1), math.Max(dist2, dist3))
 }
 
+// IsIntersect returns whether two lines intersect
+//
+// | Step 1. 왼쪽에서 오른쪽 아래로 내려가며 곱해서 더하기
+// |
+// |                    x1     y1
+// |                        ↘
+// |                    x2     y2  +x1y2
+// |                        ↘
+// |                    x3     y3  +x2y3
+// |                        ↘
+// |                    x1     y1  +x3y1
+// |                ---------------------
+// |                               +x1y2 +x2y3 +x3y1
+// |
+// |  Step 2. 오른쪽에서 왼쪽 아래로 내려가며 곱해서 빼기
+// |
+// |                    x1     y1
+// |                        ↙
+// |             -y1x2  x2     y2
+// |                        ↙
+// |             -y2x3  x3     y3
+// |                        ↙
+// |             -y3x1  x1     y1
+// |                 ---------------------
+// |  -y1x2 -y2x3 -y3x1
+// |
+// |  (x1y2 + x2y3 + x3y1 - y1x2 - y2x3 - y3x1)
+//
+// 두 선분 모두, 다른 선분의 두 점에 대해 두 CCW 값의 부호가 서로 반대라면 두 선분은 교차한다.
 func IsIntersect(p0, p1, p2, p3 Point) (bool, bool) {
 	abc := integer.CCW(p0.X, p0.Y, p1.X, p1.Y, p2.X, p2.Y)
 	abd := integer.CCW(p0.X, p0.Y, p1.X, p1.Y, p3.X, p3.Y)
--- utils/integer/ccw.go
+++ utils/integer/ccw.go
@@ -1,5 +1,16 @@
 package integer
 
+// CCW returns the direction of the cross product of the vectors (x1, y1) -> (x2, y2) and (x2, y2) -> (x3, y3)
+// 1: counter-clockwise
+// -1: clockwise
+// 0: collinear
+//
+// Shoelace formula(신발끈 공식, 가우스의 면적 공식, 측량사의 공식)
+// https://en.wikipedia.org/wiki/Shoelace_formula
+//
+// 외적의 수직벡터 방향에 따라 반시계방향의 삼각형의 면적은 양수, 시계방향의 경우는 음수
+//
+// 오른손 법칙을 기억하자
 func CCW(x1, y1, x2, y2, x3, y3 int) int {
 	result := x1*y2 + x2*y3 + x3*y1 - y1*x2 - y2*x3 - y3*x1
 	if result > 0 {
