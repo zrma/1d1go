b0cf1f9aa759871194f14f0e423f46caf79ae8ee
zrma <bulbitain@gmail.com>
2025-03-16 21:41:15 +0900

    refactor: update go version, pre-commit, fix benchmark

--- go.mod
+++ go.mod
@@ -1,22 +1,22 @@
 module 1d1go
 
-go 1.23
+go 1.24
 
-toolchain go1.23.4
+toolchain go1.24.1
 
 require (
-	github.com/PuerkitoBio/goquery v1.10.1
+	github.com/PuerkitoBio/goquery v1.10.2
 	github.com/alicebob/miniredis/v2 v2.34.0
 	github.com/eclipse/paho.mqtt.golang v1.5.0
 	github.com/golang/mock v1.6.0
 	github.com/oddg/hungarian-algorithm v0.0.0-20170809162819-9567cbc363de
-	github.com/panjf2000/ants/v2 v2.11.0
-	github.com/spf13/afero v1.12.0
+	github.com/panjf2000/ants/v2 v2.11.2
+	github.com/spf13/afero v1.14.0
 	github.com/stretchr/testify v1.10.0
 	go.uber.org/zap v1.27.0
-	google.golang.org/grpc v1.69.4
+	google.golang.org/grpc v1.71.0
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1
-	google.golang.org/protobuf v1.36.2
+	google.golang.org/protobuf v1.36.5
 )
 
 require (
@@ -29,13 +29,13 @@ require (
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/yuin/gopher-lua v1.1.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/mod v0.22.0 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	golang.org/x/tools v0.29.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250106144421-5f5ef82da422 // indirect
+	golang.org/x/mod v0.24.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/tools v0.31.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250313205543-e70fdf4c4cb4 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- practice/benchmarks/slice_test.go
+++ practice/benchmarks/slice_test.go
@@ -1,45 +1,39 @@
 package benchmarks
 
 import (
+	"fmt"
 	"testing"
 )
 
-func benchmarkSliceConstruction(i int64, b *testing.B) {
-	b.Run("make", func(b *testing.B) {
-		benchmarkMakeSlice(i, b)
-	})
-
-	b.Run("append", func(b *testing.B) {
-		benchmarkAppendSlice(i, b)
-	})
-}
-
-func benchmarkMakeSlice(i int64, b *testing.B) {
-	cnt := i * int64(b.N)
-	var arr []int64
-	for i = 0; i < cnt; i++ {
-		arr = append(arr, i)
+func benchmarkSliceMake(size int, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		arr := make([]int64, size)
+		for j := 0; j < size; j++ {
+			arr[j] = int64(j)
+		}
+		_ = arr
 	}
-	_ = arr
 }
 
-func benchmarkAppendSlice(i int64, b *testing.B) {
-	cnt := i * int64(b.N)
-	arr := make([]int64, cnt)
-	for i = 0; i < cnt; i++ {
-		arr[i] = i
+func benchmarkSliceAppend(size int, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		arr := make([]int64, 0, size)
+		for j := 0; j < size; j++ {
+			arr = append(arr, int64(j))
+		}
+		_ = arr
 	}
-	_ = arr
 }
 
-func BenchmarkSliceConstruction1(b *testing.B)    { benchmarkSliceConstruction(1, b) }
-func BenchmarkSliceConstruction2(b *testing.B)    { benchmarkSliceConstruction(2, b) }
-func BenchmarkSliceConstruction4(b *testing.B)    { benchmarkSliceConstruction(4, b) }
-func BenchmarkSliceConstruction8(b *testing.B)    { benchmarkSliceConstruction(8, b) }
-func BenchmarkSliceConstruction16(b *testing.B)   { benchmarkSliceConstruction(16, b) }
-func BenchmarkSliceConstruction32(b *testing.B)   { benchmarkSliceConstruction(32, b) }
-func BenchmarkSliceConstruction64(b *testing.B)   { benchmarkSliceConstruction(64, b) }
-func BenchmarkSliceConstruction128(b *testing.B)  { benchmarkSliceConstruction(128, b) }
-func BenchmarkSliceConstruction256(b *testing.B)  { benchmarkSliceConstruction(256, b) }
-func BenchmarkSliceConstruction512(b *testing.B)  { benchmarkSliceConstruction(512, b) }
-func BenchmarkSliceConstruction1024(b *testing.B) { benchmarkSliceConstruction(1024, b) }
+func BenchmarkSliceConstruction(b *testing.B) {
+	sizes := []int{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024}
+
+	for _, size := range sizes {
+		b.Run(fmt.Sprintf("size=%d/make", size), func(b *testing.B) {
+			benchmarkSliceMake(size, b)
+		})
+		b.Run(fmt.Sprintf("size=%d/append", size), func(b *testing.B) {
+			benchmarkSliceAppend(size, b)
+		})
+	}
+}
